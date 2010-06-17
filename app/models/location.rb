5e4816374ba387635304586d26649722b45a3000
xyctka@gmail.com <xyctka@gmail.com>
2010-06-18 00:10:47 +0300

    single table inheritance and belongs_to associacion added to Locus

---
+++ app/models/location.rb
@@ -0,0 +1,8 @@
+class Country < Locus
+end
+
+class Region < Locus
+end
+
+class Location < Locus
+end
--- app/models/locus.rb
+++ app/models/locus.rb
@@ -2,6 +2,8 @@ class Locus < ActiveRecord::Base
 
   validates :code, :format => /^[a-z_]+$/
 
+  belongs_to :parent, :class => Locus
+
   def to_param
     code
   end
