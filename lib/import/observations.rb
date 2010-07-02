bc122256eeb5932363723bb99cb2860411708ac2
xyctka@gmail.com <xyctka@gmail.com>
2010-07-02 08:25:00 +0300

    observations import

--- lib/import/observations.rb
+++ lib/import/observations.rb
@@ -7,14 +7,13 @@ module Import
     def self.get_legacy
       
       require 'app/models/observation'
+      require 'app/models/species'
 
       init_legacy
 
-      species_map = YAML.load_file('lib/import/legacy/species_map.yml')
-
       Legacy::Observation.where("sp_id <> 'mulspp'").each do |ob|
         new_ob = Observation.new({
-                :species_id => species_map[ob[:sp_id]][:id],
+                :species_id => ob[:sp_id] == 'incogt' ? nil : Species.find_by_code(ob[:sp_id]).id,
                 :locus_id => Locus.find_by_code(ob[:loc_id].gsub('-', '_')).id,
                 :observ_date => ob[:ob_date],
                 :biotope => ob[:bt_id],
