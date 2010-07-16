26090f5a929f3c6575791627d25fe7561fdca1c0
xyctka@gmail.com <xyctka@gmail.com>
2010-07-16 14:30:21 +0300

    order locations only when necessary (lists in admin)

--- app/models/locus.rb
+++ app/models/locus.rb
@@ -5,7 +5,7 @@ class Locus < ActiveRecord::Base
   validates :code, :format => /^[a-z_]+$/i
   validates :loc_type, :inclusion => ['Country', 'Region', 'Location'] 
 
-  default_scope :order => [:loc_type, :parent_id, :code]
+  scope :ordered_for_list, order(:loc_type, :parent_id, :code)
 
   belongs_to :parent, :class_name => 'Locus'
 
@@ -14,7 +14,7 @@ class Locus < ActiveRecord::Base
   end
 
   def self.all_ordered
-    all = Locus.all
+    all = Locus.ordered_for_list.all
     all.select { |loc| loc.loc_type == 'Country' } +
             all.select { |loc| loc.loc_type == 'Region' } +
             all.select { |loc| loc.loc_type == 'Location' }
