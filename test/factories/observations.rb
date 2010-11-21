c6213e3394531f5aec23bf90a8ec7c1a05898757
xyctka@gmail.com <xyctka@gmail.com>
2010-11-20 20:47:11 -0500

    added unit tests for species scopes reordering

--- test/factories/observations.rb
+++ test/factories/observations.rb
@@ -1,8 +1,8 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 Factory.define :observation do |f|
-  f.species_id Species.find_by_code!('pasdom').id
-  f.locus_id Locus.find_by_code!('brovary').id
+  f.species Species.find_by_code!('pasdom')
+  f.locus Locus.find_by_code!('brovary')
   f.observ_date "2010-06-18"
   f.quantity "several"
   f.biotope "park"
--- test/unit/species_test.rb
+++ test/unit/species_test.rb
@@ -4,4 +4,23 @@ class SpeciesTest < ActiveSupport::TestCase
 #  should "be valid" do
 #    assert Species.new.valid?
 #  end
+
+  should 'be reordered alphabetically' do
+    # TODO: Limited to 100 because of postgres strange ordering (LC_COLLATE must be set to "C")
+    expected = Species.all.map {|sp| sp.name_sci }.sort[0..99]
+    got = Species.limit(100).alphabetic.map {|sp| sp.name_sci }
+    assert_equal expected, got
+  end
+
+  should 'order lifelist correctly' do
+    Factory.create(:observation, :species => Species.find_by_code!('pasdom'), :observ_date => "2010-06-20")
+    Factory.create(:observation, :species => Species.find_by_code!('melgal'), :observ_date => "2010-06-18")
+    Factory.create(:observation, :species => Species.find_by_code!('anapla'), :observ_date => "2009-06-18")
+    Factory.create(:observation, :species => Species.find_by_code!('anacly'), :observ_date => "2007-07-18")
+    Factory.create(:observation, :species => Species.find_by_code!('embcit'), :observ_date => "2009-08-09")
+    got = Species.lifelist.map {|sp| sp.mind }
+    expected = got.sort.reverse
+    assert_equal expected, got
+  end
+
 end
