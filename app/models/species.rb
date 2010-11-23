f03fc92275c7356b40e0cebb7da9fc43e5a28497
xyctka@gmail.com <xyctka@gmail.com>
2010-11-22 21:49:31 -0500

    new species tests

--- app/models/species.rb
+++ app/models/species.rb
@@ -3,7 +3,7 @@ class Species < ActiveRecord::Base
 #  validates :order, :presence => true, :allow_blank => true
   validates :family, :presence => true
   validates :name_sci, :format => /^[A-Z][a-z]+( \(?[a-z]+\)?)+$/
-  validates :code, :format => /^[a-z]{6}$/, :allow_blank => true
+  validates :code, :format => /^[a-z]{6}$/
 
   default_scope order(:index_num)
   scope :alphabetic, reorder(:name_sci)
--- test/unit/species_test.rb
+++ test/unit/species_test.rb
@@ -5,10 +5,42 @@ class SpeciesTest < ActiveSupport::TestCase
 #    assert Species.new.valid?
 #  end
 
+  should 'not be saved with empty Latin name' do
+    sp          = Species.find_by_code!('parcae')
+    sp.name_sci = ''
+    assert_raise(ActiveRecord::RecordInvalid) do
+      sp.save!
+    end
+  end
+
+  should 'not be saved with empty code' do
+    sp          = Species.find_by_code!('parcae')
+    sp.code     = ''
+    assert_raise(ActiveRecord::RecordInvalid) do
+      sp.save!
+    end
+  end
+
+  should 'not be saved with existing Latin name' do
+    sp          = Species.find_by_code!('parcae')
+    sp.name_sci = 'Parus major'
+    assert_raise(ActiveRecord::RecordInvalid) do
+      sp.save!
+    end
+  end
+
+  should 'not be saved with existing code' do
+    sp          = Species.find_by_code!('parcae')
+    sp.code     = 'parmaj'
+    assert_raise(ActiveRecord::RecordInvalid) do
+      sp.save!
+    end
+  end
+
   should 'be reordered alphabetically' do
     # TODO: Limited to 100 because of postgres strange ordering (LC_COLLATE must be set to "C")
-    expected = Species.all.map {|sp| sp.name_sci }.sort[0..99]
-    got = Species.limit(100).alphabetic.map {|sp| sp.name_sci }
+    expected = Species.all.map { |sp| sp.name_sci }.sort[0..99]
+    got      = Species.limit(100).alphabetic.map { |sp| sp.name_sci }
     assert_equal expected, got
   end
 
@@ -18,7 +50,7 @@ class SpeciesTest < ActiveSupport::TestCase
     Factory.create(:observation, :species => Species.find_by_code!('anapla'), :observ_date => "2009-06-18")
     Factory.create(:observation, :species => Species.find_by_code!('anacly'), :observ_date => "2007-07-18")
     Factory.create(:observation, :species => Species.find_by_code!('embcit'), :observ_date => "2009-08-09")
-    got = Species.lifelist.map {|sp| sp.mind }
+    got      = Species.lifelist.map { |sp| sp.mind }
     expected = got.sort.reverse
     assert_equal expected, got
   end
