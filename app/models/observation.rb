b588eff1748f8eb9919d437c88d62027b062212d
xyctka@gmail.com <xyctka@gmail.com>
2010-12-04 13:20:34 -0500

    gave up to 3.0.3 (select arguments fix)

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -11,7 +11,7 @@ class Observation < ActiveRecord::Base
 
   scope :species_first_met_dates, lambda {|*args|
     options = args.extract_options!
-    rel = mine.select(:species_id, 'MIN(observ_date) AS mind').group(:species_id)
+    rel = mine.select('species_id, MIN(observ_date) AS mind').group(:species_id)
     rel = rel.where('EXTRACT(year from observ_date) = ?', options[:year]) unless options[:year].blank?
     rel = rel.where('EXTRACT(month from observ_date) = ?', options[:month]) unless options[:month].blank?
     rel = rel.where('locus_id' => options[:loc_ids]) unless options[:locus].blank?
