132e56c333245b1d17ef2e589597f8e8933d1168
xyctka@gmail.com <xyctka@gmail.com>
2010-12-06 14:54:39 -0500

    posts tests fail, as well as creating post via form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   validates :code, :uniqueness => true, :presence => true
   validates :title, :topic, :status, :presence => true
+  validates :lj_url_id, :lj_post_id, :numericality => true
 
   has_many :observations
   has_many :species, :through => :observations
