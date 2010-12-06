427200d5452a8d9313bff46c7668e99c4a47ea96
xyctka@gmail.com <xyctka@gmail.com>
2010-12-05 19:53:45 -0500

    removed unnecessary paths helpers

--- app/helpers/paths_helper.rb
+++ app/helpers/paths_helper.rb
@@ -1,24 +1,4 @@
 module PathsHelper
-  def new_path_helper
-    url_for({:action => :new})
-  end
-
-  def show_path_helper(obj = nil)
-    url_for({:action => :show, :id => obj || instance_variable_get("@#{controller_name.singularize}".to_sym)})
-  end
-
-  def edit_path_helper(obj = nil)
-    url_for({:action => :edit, :id => obj || instance_variable_get("@#{controller_name.singularize}".to_sym)})
-  end
-
-  def destroy_path_helper(obj = nil)
-    url_for({:action => :destroy, :id => obj || instance_variable_get("@#{controller_name.singularize}".to_sym)})
-  end
-
-  def index_path_helper(options = {})
-    url_for(options.merge(:action => :index))
-  end
-
   def blog_path
     root_path
   end
