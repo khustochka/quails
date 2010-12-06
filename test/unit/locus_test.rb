a73c8342761d4a5e502196b1da4225bfe2fce77e
xyctka@gmail.com <xyctka@gmail.com>
2010-12-06 10:39:48 -0500

    fixed typo in locus test

--- test/unit/locus_test.rb
+++ test/unit/locus_test.rb
@@ -21,7 +21,7 @@ class LocusTest < ActiveSupport::TestCase
   should 'properly find all subregions' do
     loc = Locus.find_by_code!('ukraine')
     actual = loc.get_subregions.map {|l| l.id}.sort
-    expected = Locus.where(:code => ['ukraine', 'kiev_obl', 'khreson_obl', 'khreson', 'chernihiv']).map {|l| l.id}.sort
+    expected = Locus.where(:code => ['ukraine', 'kiev_obl', 'kherson_obl', 'kherson', 'chernihiv']).map {|l| l.id}.sort
     not_expected = Locus.where(:code => ['usa', 'new_york', 'brooklyn', 'hoboken']).map {|l| l.id}.sort
     assert_equal [], expected - actual, 'Some expected values are not included'
     assert_equal [], not_expected & actual, 'Some unexpected values are included'
