c6e36248e2b020cddcf581dc489bab1618a815d1
xyctka@gmail.com <xyctka@gmail.com>
2010-12-15 22:07:41 -0500

    minor styling for post

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+  # TODO: implement correct body parsing and transform
+  def post_body(post)
+    content_tag(:p, post.text.gsub(/(\r?\n){2}/,"\n</p>\n<p>\n").html_safe)
+  end
 end
