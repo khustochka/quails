7e7b37fb6b6b5e0d0e5f35a951a9275b91e56a00
xyctka@gmail.com <xyctka@gmail.com>
2011-01-12 16:39:05 -0500

    addition to comment

--- app/controllers/mixins/layout_controller.rb
+++ app/controllers/mixins/layout_controller.rb
@@ -12,7 +12,7 @@ module LayoutController
   module ClassMethods
 
     # TODO: if layout is inherited from ApplicationController and then redefined
-    # the parent's unnecessary before_filter remains in the chain
+    # the parent's unnecessary before_filter remains in the chain (possibly not?)
     def layout(layout, conditions = {})
       before_filter conditions do
         @layout = layout
