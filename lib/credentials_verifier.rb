6eef279da041ae7e5f69897934a656b2f9227e5f
xyctka@gmail.com <xyctka@gmail.com>
2011-01-19 22:33:37 -0500

    store and verify encoded password in dev and prod environments but plain text in test

--- lib/credentials_verifier.rb
+++ lib/credentials_verifier.rb
@@ -3,7 +3,8 @@ module CredentialsVerifier
   def self.init(options)
     const_set(:AUTH_PROC,
               lambda { authenticate_or_request_with_http_basic do |user_name, password|
-                user_name == options.username && password == options.password
+                expected_passwd = Rails.env.test? ? password : Digest::SHA1.hexdigest(password)
+                user_name == options.username && expected_passwd == options.password
               end
               })
   end
