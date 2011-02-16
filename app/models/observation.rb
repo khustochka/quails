3ddb7ec666ee96e2068950018cacb328a1847ca4
xyctka@gmail.com <xyctka@gmail.com>
2011-02-16 18:03:56 -0500

    fall back to join on MIN(date) which is 100x faster. Added tests for most common errors, one is not yet fixed.

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -19,19 +19,25 @@ class Observation < ActiveRecord::Base
     [nil] + rel.map { |ob| ob[:year] }
   end
 
-  def self.first_met_observations(*args)
+  def self.lifers_dates(*args)
     options = args.extract_options!
-    rel2 = mine.identified.select('id').from('observations AS ob2').order('observ_date').limit(1)
-    rel2 = rel2.where('species_id = observations.species_id')
-    rel2 = rel2.where('EXTRACT(year from observ_date) = ?', options[:year]) unless options[:year].blank?
-    rel2 = rel2.where('EXTRACT(month from observ_date) = ?', options[:month]) unless options[:month].blank?
-    rel2 = rel2.where('locus_id' => options[:loc_ids]) unless options[:locus].blank?
+    rel     = mine.identified.select('species_id, MIN(observ_date) AS first_date').group(:species_id)
+    rel     = rel.where('EXTRACT(year from observ_date) = ?', options[:year]) unless options[:year].blank?
+    rel     = rel.where('EXTRACT(month from observ_date) = ?', options[:month]) unless options[:month].blank?
+    rel     = rel.where('locus_id' => options[:loc_ids]) unless options[:locus].blank?
+    rel
+  end
 
-    where("observations.id = (#{rel2.to_sql})")
+  def self.lifers_observations(*args)
+    options = args.extract_options!
+
+    # Using join on earliest date is the fastest
+    select('DISTINCT ON (observations.species_id, observ_date) observations.*').joins("INNER JOIN (#{lifers_dates(options).to_sql}) AS dts
+          ON observations.species_id=dts.species_id AND observations.observ_date=dts.first_date")
   end
 
   def self.lifelist(*args)
-    first_met_observations(*args).includes(:species, :post).order('observ_date DESC')
+    lifers_observations(*args).includes(:species, :post).order('observ_date DESC')
   end
 
 end
--- test/unit/observation_test.rb
+++ test/unit/observation_test.rb
@@ -13,4 +13,33 @@ class ObservationTest < ActiveSupport::TestCase
     assert_equal expected, got
   end
 
+  should 'not miss species from lifelist if it was first seen not by me' do
+    Factory.create(:observation, :species => Species.find_by_code!('pasdom'), :observ_date => "2008-06-20")
+    Factory.create(:observation, :species => Species.find_by_code!('melgal'), :observ_date => "2009-06-18", :mine => false)
+    observ   = Factory.create(:observation, :species => Species.find_by_code!('melgal'), :observ_date => "2010-06-18")
+    got      = Observation.lifers_observations.map { |ob| [ob.species_id, ob.observ_date] }
+    expected = [observ.species_id, observ.observ_date]
+    assert_contains got, expected
+  end
+
+  should 'not duplicate species in lifelist if it was seen twice on its first date' do
+    Factory.create(:observation, :species => Species.find_by_code!('pasdom'), :observ_date => "2009-06-18")
+    Factory.create(:observation, :species => Species.find_by_code!('melgal'), :observ_date => "2009-06-18", :locus => Locus.find_by_code!('brovary'))
+    Factory.create(:observation, :species => Species.find_by_code!('melgal'), :observ_date => "2009-06-18", :locus => Locus.find_by_code!('kiev'))
+    got = Observation.lifers_observations.all
+    assert_equal 2, got.size
+    got2 = Observation.lifelist.all
+    assert_equal 2, got2.size
+  end
+
+  should 'not miss species from lifelist if its first observation has no post' do
+    blogpost = Factory.create(:post)
+    Factory.create(:observation, :species => Species.find_by_code!('anapla'), :observ_date => "2009-08-09", :post => blogpost)
+    observ = Factory.create(:observation, :species => Species.find_by_code!('anacly'), :observ_date => "2007-07-18")
+    Factory.create(:observation, :species => Species.find_by_code!('anacly'), :observ_date => "2009-08-09", :post => blogpost)
+    got      = Observation.lifelist.map { |ob| [ob.species_id, ob.observ_date] }
+    expected = [observ.species_id, observ.observ_date]
+    assert_contains got, expected
+  end
+
 end
