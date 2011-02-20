e9ef241cf0cbe3ea9db75907c66b3e406b2fd128
xyctka@gmail.com <xyctka@gmail.com>
2011-02-19 21:37:14 -0500

    added tests for hiding 'private' posts links in lifelist for public

--- test/functional/lifelist_controller_test.rb
+++ test/functional/lifelist_controller_test.rb
@@ -59,4 +59,23 @@ class LifelistControllerTest < ActionController::TestCase
       assert_select 'a', false
     end
   end
+
+  should 'not show hidden post link to common visitor' do
+    @obs[1].post = Factory.create(:post, :status => 'PRIV')
+    @obs[1].save!
+    get :lifelist
+    assert_response :success
+    assert_select 'td', I18n::l(@obs[1].observ_date, :format => :long) do
+      assert_select 'a', false
+    end
+  end
+
+  should 'show hidden post link to administrator' do
+    @obs[1].post = Factory.create(:post, :status => 'PRIV')
+    @obs[1].save!
+    login_as_admin
+    get :lifelist
+    assert_response :success
+    assert_select "a[href=#{public_post_path(@obs[1].post)}]", I18n::l(@obs[1].observ_date, :format => :long)
+  end
 end
