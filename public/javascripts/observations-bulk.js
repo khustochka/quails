867391404a68cee0e3f29152703483ff698eaae1
xyctka@gmail.com <xyctka@gmail.com>
2011-05-20 17:17:34 +0300

    first draft of bulk save frontend (add check sign and id) - success only, no verification for already existing

--- public/javascripts/observations-bulk.js
+++ public/javascripts/observations-bulk.js
@@ -1,10 +1,22 @@
 $(function() {
     var form = $('form#new_observation[data-remote]');
+    var check_img = $( "<img>" ).attr('src', '/images/check_alt_16x16.png');
+
     form.attr('action', form.data('action'));
-    
+    form.bind('ajax:success', function(e, data) {
+        if (data.result == "OK") {
+           $(".obs-row").each( function(index) {
+               var val = data.data[index];
+               if (val.id) $(this).prepend(
+                       check_img.clone(), $("<input type='hidden' name='o[][id]'>").attr('value', val.id)
+                       );
+           })
+        }
+    });
+
     var sample_row = $('.obs-row').detach();
     var cnt = 1;
-    
+
     $('#add-row').click(function() {
         var row = sample_row.clone(true);
         $(':submit').before(row);
