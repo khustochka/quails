97340bc77932e285957eed48d8b86c5e3d15f003
xyctka@gmail.com <xyctka@gmail.com>
2011-05-30 16:16:42 +0300

    remove unnecessary line

--- config/initializers/security.rb
+++ config/initializers/security.rb
@@ -1,7 +1,5 @@
 require 'credentials_verifier'
 
-file = 'config/security.yml'
-
 options = YAML.load(ERB.new(File.read('config/security.yml')).result)[Rails.env]
 
 CredentialsVerifier.init Hashie::Mash.new(options)
