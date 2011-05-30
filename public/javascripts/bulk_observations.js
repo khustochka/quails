283425c77f3f8aab9b3807fbbae447f0c731f725
xyctka@gmail.com <xyctka@gmail.com>
2011-05-30 17:07:54 +0300

    observations#bulksave js: quick select (unfinished): UI

--- public/javascripts/bulk_observations.js
+++ public/javascripts/bulk_observations.js
@@ -1,6 +1,18 @@
 $(function() {
     var form = $('form#new_observation[data-remote]');
 
+    var options_list = $('.obs-row select.sp-suggest').children( "option" );
+
+    function addNewRow() {
+        var row = sample_row.clone(true);
+        $(':submit').before(row);
+        row.find('label:contains("Species:")').attr('for', 'observation_species_id' + cnt);
+        row.find('.sp-suggest').attr('id', 'observation_species_id' + cnt).combobox();
+        cnt++;
+        return false;
+    }
+
+    /* On form submit */
     form.attr('action', form.data('action'));
     form.bind('ajax:success', function(e, data) {
         $(".error_list").remove();
@@ -27,22 +39,42 @@ $(function() {
         }
     });
 
-    $('.species-quick-add').combobox();
-    $('input#species_species_id').bind( "autocompleteselect", function(event, ui) {
-        alert('xx')
-    });
+    $('#species-quick-add').autocomplete({
+        delay: 0,
+        minLength: 3,
+        autoFocus: true,
+        source: function( request, response ) {
+            var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
+            response( options_list.map(function() {
+                var text = $( this ).text();
+                if ( ( this.value != null ) && ( !request.term || matcher.test(text) ) )
+                    return {
+                        label: text.replace('Avis incognita', '<i>Avis incognita</i>').replace(
+                            new RegExp(
+                                "(?![^&;]+;)(?!<[^<>]*)(" +
+                                $.ui.autocomplete.escapeRegex(request.term) +
+                                ")(?![^<>]*>)(?![^&;]+;)", "gi"
+                            ), "<strong>$1</strong>" ),
+                        value: text
+                    };
+            }) );
+        },
+        select: function() {
+            addNewRow( $( this ).val() );
+            $( this ).val("");
+            return false;
+        }
+    }).data( "autocomplete" )._renderItem = function( ul, item ) {
+        return $( "<li></li>" )
+            .data( "item.autocomplete", item )
+            .append( "<a>" + item.label + "</a>" )
+            .appendTo( ul );
+    };
 
     var sample_row = $('.obs-row').detach();
     var cnt = 1;
 
-    $('#add-row').click(function() {
-        var row = sample_row.clone(true);
-        $(':submit').before(row);
-        row.find('label:contains("Species:")').attr('for', 'observation_species_id' + cnt);
-        row.find('.sp-suggest').attr('id', 'observation_species_id' + cnt).combobox();
-        cnt++;
-        return false;
-    });
+    $('#add-row').click(addNewRow);
 
     $('.remove').live('click', function() {
         $(this).parent().remove();
