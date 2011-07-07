75d90e6efbfcc249282a171023b7a2c474a91af4
xyctka@gmail.com <xyctka@gmail.com>
2011-07-07 10:09:30 +0300

    use minified jquery even in dev/test mode

--- config/initializers/jquery_versions.rb
+++ config/initializers/jquery_versions.rb
@@ -2,7 +2,7 @@ JQUERY_VERSION = '1.6.2'
 
 JQUERY_URL = Rails.env.production? ?
     "http://ajax.googleapis.com/ajax/libs/jquery/#{JQUERY_VERSION}/jquery.min.js" :
-    "jquery-#{JQUERY_VERSION}"
+    "jquery-#{JQUERY_VERSION}.min"
 
 Dir['public/javascripts/*.js'].each do |path|
   if File.basename(path).match(/jquery-ui*/)
