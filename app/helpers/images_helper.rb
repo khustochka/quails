70948dcf64055276e6e91530547748cb40fac537
xyctka@gmail.com <xyctka@gmail.com>
2011-07-11 11:00:11 +0300

    refactored image sources redirect

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,2 +1,11 @@
 module ImagesHelper
+
+  def jpg_url(img, thumbnail = false)
+    "http://birdwatch.org.ua/photos/#{'tn_' if thumbnail}#{img.code}.jpg"
+  end
+
+  def image_thumbnail(img)
+    link_to image_tag(jpg_url(img, true)), public_image_path(img)
+  end
+
 end
