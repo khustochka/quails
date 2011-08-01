decc829c12bca956ae06b42e395f61a6fd4da8ea
xyctka@gmail.com <xyctka@gmail.com>
2011-08-01 21:22:19 +0300

    show span if no link in main menu (will be used for styling)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,12 @@ module ApplicationHelper
     Rails.env.development? ? '!!! - Set the window caption / quails3' : request.host
   end
 
+  def link_to_or_span(name, options = {}, html_options = {})
+    link_to_unless_current(name, options, html_options) do
+      content_tag(:span, name, html_options)
+    end
+  end
+
   def include_stylesheets
     stylesheet_link_tag @stylesheets.uniq
   end
