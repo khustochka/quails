c65e3acd3aec4913eb895a3a1507ddbc02908400
xyctka@gmail.com <xyctka@gmail.com>
2011-08-09 21:28:15 +0300

    flickr settings are now in security.yml

--- config/initializers/flickraw.rb
+++
@@ -1,6 +0,0 @@
-FlickRawOptions = {
-    "api_key" => "... Your API key ...",
-    "shared_secret" => "... Your shared secret ...",
-    "auth_token" => "... Your saved token...",
-    "lazyload" => true
-}
--- config/initializers/security.rb
+++ config/initializers/security.rb
@@ -4,4 +4,6 @@ options = YAML.load(ERB.new(File.read('config/security.yml')).result)[Rails.env]
 
 IMAGES_HOST = options['images_host']
 
+FlickRawOptions = options['flickr'].merge({"lazyload" => true})
+
 CredentialsVerifier.init Hashie::Mash.new(options)
