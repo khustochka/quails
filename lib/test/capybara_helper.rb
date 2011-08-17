12783020db2b05e3f51be6e8c0a26463b9eae028
xyctka@gmail.com <xyctka@gmail.com>
2011-08-17 15:49:48 +0300

    Shiver, Cucumber, your end is coming

---
+++ lib/test/capybara_helper.rb
@@ -0,0 +1,40 @@
+require 'capybara/rails'
+require 'rspec-expectations'
+
+module CapybaraTestCase
+  include Capybara::DSL
+
+  def self.included(klass)
+    klass.teardown do
+      Capybara.reset_sessions!
+    end
+  end
+
+  def login_as_admin
+    if page.driver.try(:browser).respond_to?(:basic_authorize)
+      page.driver.browser.basic_authorize(ActiveSupport::TestCase::TEST_CREDENTIALS.username, ActiveSupport::TestCase::TEST_CREDENTIALS.password_plain)
+      # To set cookies
+      visit '/login'
+    else
+      # FIXME for this to work you need to add pref("network.http.phishy-userpass-length", 255); to /Applications/Firefox.app/Contents/MacOS/defaults/pref/firefox.js
+      str = "http://%s:%s@%s:%s/login"
+      visit str % [TEST_CREDENTIALS.username, TEST_CREDENTIALS.password_plain, page.driver.rack_server.host, page.driver.rack_server.port]
+    end
+  end
+end
+
+module JavaScriptTestCase
+
+  def self.included(klass)
+    klass.include CapybaraTestCase
+
+    klass.setup do
+      Capybara.current_driver = Capybara.javascript_driver
+    end
+
+    klass.teardown do
+      Capybara.use_default_driver
+    end
+
+  end
+end
--- test/integration/ui_posts_test.rb
+++ test/integration/ui_posts_test.rb
@@ -1,21 +1,45 @@
-require "capybara_test_helper"
+require 'test_helper'
+require 'test/capybara_helper'
 
-class UIPostsTest < CapybaraTestCase
+class UIPostsTest < ActionDispatch::IntegrationTest
 
-  setup do
-    @blogpost = Factory.create(:post)
-  end
+  include CapybaraTestCase
 
   test "Adding post" do
     login_as_admin
-
     visit new_post_path
-
     fill_in('Code', :with => 'new-post')
     fill_in('Title', :with => 'Test post')
     fill_in('Text', :with => 'Post text.')
     select('OPEN', :from => 'Status')
     select('OBSR', :from => 'Topic')
+    click_button('Save')
+    blogpost = Post.find_by_code('new-post')
+    current_path.should == show_post_path(blogpost.to_url_params)
+  end
+
+  test "Editing post" do
+    blogpost = Factory.create(:post)
+    login_as_admin
+    visit edit_post_path(blogpost)
+    fill_in('Code', :with => 'changed-post')
+    fill_in('Title', :with => 'Test post edited')
+    fill_in('Text', :with => 'Post text edited.')
+    fill_in('Post date', :with => '2009-07-27')
+    click_button('Save')
+    blogpost = Post.find_by_code('changed-post')
+    current_path.should == show_post_path(blogpost.to_url_params)
+  end
+
+  test "Navigation via Edit this and Show this links" do
+    blogpost = Factory.create(:post)
+    login_as_admin
+    visit show_post_path(blogpost.to_url_params)
+    click_link('Edit this one')
+    current_path.should == edit_post_path(blogpost)
+    click_link('Show this one')
+    current_path.should == show_post_path(blogpost.to_url_params)
 
   end
+
 end
