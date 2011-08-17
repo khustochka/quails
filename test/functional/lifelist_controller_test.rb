cb4ed87434eca7b087439bcf1255704c983c5544
xyctka@gmail.com <xyctka@gmail.com>
2011-08-17 12:51:12 +0300

    lifelist tests update

--- test/functional/lifelist_controller_test.rb
+++ test/functional/lifelist_controller_test.rb
@@ -41,41 +41,4 @@ class LifelistControllerTest < ActionController::TestCase
     assert_response :success
     assert_select 'a.sp_link'
   end
-
-  should 'show post link if post is associated' do
-    @obs[1].post = Factory.create(:post)
-    @obs[1].save!
-    get :lifelist
-    assert_response :success
-    assert_select "a[href=#{public_post_path(@obs[1].post)}]", I18n::l(@obs[1].observ_date, :format => :long)
-  end
-
-  should 'not show post link if no post is associated' do
-    @obs[1].post = Factory.create(:post)
-    @obs[1].save!
-    get :lifelist
-    assert_response :success
-    assert_select 'td', I18n::l(@obs[2].observ_date, :format => :long) do
-      assert_select 'a', false
-    end
-  end
-
-  should 'not show hidden post link to common visitor' do
-    @obs[1].post = Factory.create(:post, :status => 'PRIV')
-    @obs[1].save!
-    get :lifelist
-    assert_response :success
-    assert_select 'td', I18n::l(@obs[1].observ_date, :format => :long) do
-      assert_select 'a', false
-    end
-  end
-
-  should 'show hidden post link to administrator' do
-    @obs[1].post = Factory.create(:post, :status => 'PRIV')
-    @obs[1].save!
-    login_as_admin
-    get :lifelist
-    assert_response :success
-    assert_select "a[href=#{public_post_path(@obs[1].post)}]", I18n::l(@obs[1].observ_date, :format => :long)
-  end
 end
---
+++ test/functional/lifelist_posts_test.rb
@@ -0,0 +1,52 @@
+require 'test_helper'
+
+class LifelistControllerTest < ActionController::TestCase
+  tests LifelistController
+
+  setup do
+    @obs = [
+        Factory.create(:observation, :species => Species.find_by_code!('pasdom'), :observ_date => "2010-06-20", :locus => Locus.find_by_code!('new_york')),
+        Factory.create(:observation, :species => Species.find_by_code!('melgal'), :observ_date => "2010-06-18"),
+        Factory.create(:observation, :species => Species.find_by_code!('anapla'), :observ_date => "2009-06-18"),
+        Factory.create(:observation, :species => Species.find_by_code!('anacly'), :observ_date => "2007-07-18", :locus => Locus.find_by_code!('brovary')),
+        Factory.create(:observation, :species => Species.find_by_code!('embcit'), :observ_date => "2009-08-09", :locus => Locus.find_by_code!('kherson'))
+    ]
+  end
+
+  should 'show post link if post is associated' do
+    @obs[1].post = Factory.create(:post)
+    @obs[1].save!
+    get :lifelist
+    assert_response :success
+    assert_select "a[href=#{public_post_path(@obs[1].post)}]", I18n::l(@obs[1].observ_date, :format => :long)
+  end
+
+  should 'not show post link if no post is associated' do
+    @obs[1].post = Factory.create(:post)
+    @obs[1].save!
+    get :lifelist
+    assert_response :success
+    assert_select 'time', I18n::l(@obs[2].observ_date, :format => :long) do
+      assert_select 'a', false
+    end
+  end
+
+  should 'not show hidden post link to common visitor' do
+    @obs[1].post = Factory.create(:post, :status => 'PRIV')
+    @obs[1].save!
+    get :lifelist
+    assert_response :success
+    assert_select 'time', I18n::l(@obs[1].observ_date, :format => :long) do
+      assert_select 'a', false
+    end
+  end
+
+  should 'show hidden post link to administrator' do
+    @obs[1].post = Factory.create(:post, :status => 'PRIV')
+    @obs[1].save!
+    login_as_admin
+    get :lifelist
+    assert_response :success
+    assert_select "a[href=#{public_post_path(@obs[1].post)}]", I18n::l(@obs[1].observ_date, :format => :long)
+  end
+end
