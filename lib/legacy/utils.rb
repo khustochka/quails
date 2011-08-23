8d7b65e3268560c208594b8cb5b632f7f3e7964c
xyctka@gmail.com <xyctka@gmail.com>
2011-08-23 07:44:05 +0300

    only read database.yml when necessary (now fails on heroku when loading rake tasks)

--- lib/legacy/utils.rb
+++ lib/legacy/utils.rb
@@ -3,13 +3,6 @@ require 'iconv'
 module Legacy
   class Utils
 
-    @@legacy_db_spec = YAML.load_file('config/database.yml')['legacy'] || {}
-    @@rails_db_spec = YAML.load_file('config/database.yml')[Rails.env || 'development']
-
-    # Iconv does not understand 'unicode'
-    @@legacy_db_spec['encoding'] = 'utf-8' if @@legacy_db_spec['encoding'] == 'unicode'
-    @@rails_db_spec['encoding'] = 'utf-8' if @@rails_db_spec['encoding'] == 'unicode'
-
     def self.prepare_table(raw)
       cols = raw['columns']
       raw['records'].map do |el|
@@ -28,10 +21,18 @@ module Legacy
     def self.conv_to_old(str)
       @@legacy_db_spec["encoding"] != @@rails_db_spec["encoding"] ?
               Iconv.iconv(@@legacy_db_spec["encoding"], @@rails_db_spec["encoding"], str).to_s :
-      str
+              str
     end
 
     def self.db_connect
+
+      @@legacy_db_spec = YAML.load_file('config/database.yml')['legacy'] || {}
+      @@rails_db_spec = YAML.load_file('config/database.yml')[Rails.env || 'development']
+
+      # Iconv does not understand 'unicode'
+      @@legacy_db_spec['encoding'] = 'utf-8' if @@legacy_db_spec['encoding'] == 'unicode'
+      @@rails_db_spec['encoding'] = 'utf-8' if @@rails_db_spec['encoding'] == 'unicode'
+
       Legacy::Models::Base.establish_connection(@@legacy_db_spec)
     end
 
