afd99ac1ad267b263c0602e4593cdacab9f2dcf2
xyctka@gmail.com <xyctka@gmail.com>
2011-09-09 21:40:10 +0300

    Implement simple console progress bar for observations import

--- lib/legacy/import/observations.rb
+++ lib/legacy/import/observations.rb
@@ -12,25 +12,35 @@ module Legacy
 
         puts 'Importing observations...'
 
-        observations.reject {|el| el[:sp_id] == 'mulspp'}.each do |ob|
+        observs = observations.reject { |el| el[:sp_id] == 'mulspp' }
+
+        chunk = observs.size / 49
+
+        observs.each_with_index do |ob, i|
           new_ob = Observation.new({
-                                       :species_id => Legacy::Mapping.species[ob[:sp_id]],
-                                       :locus_id => Legacy::Mapping.locations[ob[:loc_id].gsub('-', '_')],
-                                       :observ_date => ob[:ob_date],
-                                       :biotope => ob[:bt_id],
-                                       :quantity => ob[:quantity],
-                                       :place => ob[:place],
-                                       :notes => ob[:notes],
-                                       :post_id => ob[:post_id].blank? ? nil : Legacy::Mapping.posts[ob[:post_id]],
-                                       :mine => ob[:mine]
+                                           :species_id => Legacy::Mapping.species[ob[:sp_id]],
+                                           :locus_id => Legacy::Mapping.locations[ob[:loc_id].gsub('-', '_')],
+                                           :observ_date => ob[:ob_date],
+                                           :biotope => ob[:bt_id],
+                                           :quantity => ob[:quantity],
+                                           :place => ob[:place],
+                                           :notes => ob[:notes],
+                                           :post_id => ob[:post_id].blank? ? nil : Legacy::Mapping.posts[ob[:post_id]],
+                                           :mine => ob[:mine]
                                    })
           new_ob.id = ob[:observ_id]
           new_ob.save!
+          if (i + 1) % chunk == 0
+            part = ((i + 1) / chunk)
+            $stderr.write(('=' * part) + " #{part*2}%\r")
+          end
         end
 
         ActiveRecord::Base.connection.execute("ALTER SEQUENCE observations_id_seq RESTART #{Observation.maximum(:id)+1}")
 
-        puts 'Done.'
+
+        $stderr.write(('=' * 50) + " 100%")
+        puts "Done."
       end
     end
   end
