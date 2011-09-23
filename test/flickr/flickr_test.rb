b3695649a621f968a0b0b0560969ab6307e440fa
xyctka@gmail.com <xyctka@gmail.com>
2011-09-23 23:33:10 +0300

    flickr test added

--- test/flickr/flickr_test.rb
+++ test/flickr/flickr_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class FlickrTest < ActiveSupport::TestCase
 
-  test 'dummy' do
-    fail("Not implemented")
+  test 'can find private flickr photos (auth success)' do
+    flickr.photos.search(:user_id => '8289389@N04', :privacy_filter => 5).should_not be_blank
   end
 
 end
