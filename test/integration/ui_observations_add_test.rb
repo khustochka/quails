0b92a21a2fb46490ebf7346a0d25492f87f50cd6
xyctka@gmail.com <xyctka@gmail.com>
2011-09-23 10:28:16 +0100

    Timeout is sometimes necessary for the test to pass

--- test/integration/ui_observations_add_test.rb
+++ test/integration/ui_observations_add_test.rb
@@ -36,8 +36,8 @@ class UIObservationsAddTest < ActionDispatch::IntegrationTest
     select_suggestion('Brovary', :from => 'Location')
     fill_in('Date', :with => '2011-04-08')
     select_suggestion('- Avis incognita', :from => 'Species')
-    lambda { click_button('Save') }.should change(Observation, :count).by(1)
-    Observation.order('id DESC').limit(1).last.species_id.should == 0
+    lambda { click_button('Save'); sleep 0.5 }.should change(Observation, :count).by(1)
+    Observation.order('id DESC').limit(1).first.species_id.should == 0
   end
 
   test "Adding several observations" do
