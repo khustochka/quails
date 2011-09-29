41a3f5cfe8c738262e503c735ea9f40ec206794e
xyctka@gmail.com <xyctka@gmail.com>
2011-09-29 14:58:07 +0100

    Recreate database.yml from sample if it is missing

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+begin
 # Initialize the rails application
-Quails3::Application.initialize!
+  Quails3::Application.initialize!
+rescue Errno::ENOENT
+  require 'fileutils'
+  FileUtils.cp 'config/database.sample.yml', 'config/database.yml'
+  raise 'Created config/database.yml. Please edit it as appropriate'
+end
