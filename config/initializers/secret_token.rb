150c3715823fe8d46cefcd080bd76c170bc8acf9
xyctka@gmail.com <xyctka@gmail.com>
2011-10-08 21:04:03 +0300

    Secret token for cookies signing is now configurable
    and not in git

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Quails3::Application.config.secret_token = '668718a0533a20f98e79a9545609f6d65aa3ddf8410ce4f5667c80422fa6c67e8a13ee21c8dd34b1c030d578c8ef8b59d769e597c436c01f0d01d8d2d4c73bb0'
