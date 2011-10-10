9d4192f49b4b5a81294416d9b9f09b70c64c9c7b
xyctka@gmail.com <xyctka@gmail.com>
2011-10-10 14:09:11 +0100

    Security config cleanup

--- config/initializers/security.rb
+++ config/initializers/security.rb
@@ -1,19 +1,25 @@
-require 'credentials_verifier'
+filename = Rails.env.production? ? 'config/security.yml' : 'config/security_devtest.yml'
 
-file = Rails.env.production? ? 'config/security.yml' : 'config/security_devtest.yml'
-
-options = YAML.load(ERB.new(File.read(file)).result)[Rails.env]
+options = YAML.load(ERB.new(File.read(filename)).result)[Rails.env]
 
 IMAGES_HOST = options['images_host']
 
 FlickRawOptions = options['flickr'].merge({"lazyload" => true})
-
 require 'flickraw'
 
+require 'credentials_verifier'
 CredentialsVerifier.init Hashie::Mash.new(options)
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Quails3::Application.config.secret_token = options['secret_token']
+secret = options['secret_token']
+if secret.blank? || secret.length < 30
+  raise ArgumentError, "A secret is required to generate an " +
+    "integrity hash for cookie session data. Use " +
+    "SecureRandom.hex(30) to generate a secret " +
+    "of at least 30 characters and store it " +
+    "in config/security.yml"
+end
+Quails3::Application.config.secret_token = secret
