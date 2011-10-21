58314e958da76e5c1f82795faa1c1d0a4314a14a
xyctka@gmail.com <xyctka@gmail.com>
2011-10-21 10:36:43 +0100

    Tests for adding image (with observations drag and drop)

--- test/integration/ui_images_test.rb
+++ test/integration/ui_images_test.rb
@@ -18,7 +18,50 @@ class UIImagesTest < ActionDispatch::IntegrationTest
     img.observations.size.should == 2
   end
 
-  test "Remove an observation" do
+  test "Adding new image" do
+    FactoryGirl.create(:observation, :observ_date => '2008-07-01')
+    login_as_admin
+    visit new_image_path
+    
+    fill_in('Code', :with => 'test-img-capybara')
+    fill_in('Title', :with => 'Capybara test image')
+    
+    within('.observation_search') do
+      fill_in('Date', :with => '2008-07-01')
+      select_suggestion('Brovary', :from => 'Location')
+      click_button 'Search'
+    end
+        
+    find(:xpath, "//ul[contains(@class,'found-obs')]/li[1]").drag_to find('.observation_list')
+    
+    lambda { click_button('Save') }.should change(Image, :count).by(1)
+    img = Image.find_by_code('test-img-capybara')
+    current_path.should == show_image_path(img.to_url_params)
+  end
+
+  test "Image save should not use all found observations" do
+    FactoryGirl.create(:observation, :species => seed(:merser))
+    FactoryGirl.create(:observation, :species => seed(:anapla))
+    login_as_admin
+    visit new_image_path
+    
+    fill_in('Code', :with => 'test-img-capybara')
+    fill_in('Title', :with => 'Capybara test image')
+    
+    within('.observation_search') do
+      fill_in('Date', :with => '')
+      click_button 'Search'
+    end
+        
+    find(:xpath, "//ul[contains(@class,'found-obs')]/li[div/span[contains(text(),'Mergus serrator')]]").drag_to find('.observation_list')
+    
+    lambda { click_button('Save') }.should change(Image, :count).by(1)
+    img = Image.find_by_code('test-img-capybara')
+    
+    img.species.map(&:name_sci).should == ['Mergus serrator']
+  end
+
+  test "Remove an observation from image" do
     img = FactoryGirl.create(:image)
     FactoryGirl.create(:observation, :species => seed(:lancol), :images => [img])
     FactoryGirl.create(:observation, :species => seed(:denmaj), :images => [img])
@@ -33,22 +76,5 @@ class UIImagesTest < ActionDispatch::IntegrationTest
     img.reload
     img.observations.size.should == 2
   end
-
-  # test "Adding new image" do
-  # login_as_admin
-  # visit new_image_path
-  #
-  # fill_in('Code', :with => 'test-img-capybara')
-  # fill_in('Title', :with => 'Capybara test image')
-  #
-  # within('.observations_search') do
-  # fill_in('Date', :with => '2008-07-01')
-  # select_suggestion('Brovary', :from => 'Location')
-  # end
-  #
-  # lambda { click_button('Save') }.should change(Image, :count).by(1)
-  # img = Image.find_by_code('test-img-capybara')
-  # current_path.should == show_image_path(img.to_url_params)
-  # end
 
 end
