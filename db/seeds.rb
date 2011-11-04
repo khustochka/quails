4e030fbfb0c8aba7ab7ee4cce11582acb8ef91d1
xyctka@gmail.com <xyctka@gmail.com>
2011-11-04 07:54:58 +0000

    remove explicitly setting YAML engine to psych

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,6 @@
 
 require 'bunch_db/table'
 
-YAML::ENGINE.yamler = 'psych' # required for parsing db dump yaml in debugging, or on heroku
-
 local_opts = YAML.load_file('config/local.yml')
 dirname = File.join(local_opts['repo'], 'seed')
 
