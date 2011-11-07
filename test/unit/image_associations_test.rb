fe077f9149e1bca28e1051b5c36afd24c6c69682
xyctka@gmail.com <xyctka@gmail.com>
2011-11-07 10:47:23 +0000

    Use factories in image test

--- test/unit/image_associations_test.rb
+++ test/unit/image_associations_test.rb
@@ -2,13 +2,16 @@ require 'test_helper'
 
 class ImagesAssociationsTest < ActiveSupport::TestCase
 
-  test 'properly link images with one species' do
-    sp = seed(:lancol)
-    obs = FactoryGirl.create(:observation, :species => sp)
-    img = FactoryGirl.create(:image, :code => 'picture-of-the-shrike', :observations => [obs])
+  setup do
+    @obs = FactoryGirl.create(:observation)
+    @image = FactoryGirl.create(:image, :observations => [@obs])
+  end
 
-    assert_equal [img.code], sp.images.map(&:code)
-    assert_equal [sp.code], img.species.map(&:code)
+  test 'properly link images with one species' do
+    sp = @obs.species
+
+    assert_equal [@image.code], sp.images.map(&:code)
+    assert_equal [sp.code], @image.species.map(&:code)
   end
 
   test 'properly link images with several species' do
@@ -18,29 +21,24 @@ class ImagesAssociationsTest < ActiveSupport::TestCase
     obs2 = FactoryGirl.create(:observation, :species => sp2, :observ_date => "2008-07-01")
     img = FactoryGirl.create(:image, :code => 'picture-of-the-shrike-and-the-wryneck', :observations => [obs1, obs2])
 
-    assert_equal [img.code], sp1.images.reload.map(&:code)
-    assert_equal [img.code], sp2.images.reload.map(&:code)
+    assert_equal [img.code], sp1.images.map(&:code)
+    assert_equal [img.code], sp2.images.map(&:code)
     ([sp1.code, sp2.code] - img.species.map(&:code)).should be_empty
   end
 
   test 'properly link image and post' do
-    sp = seed(:lancol)
-    obs = FactoryGirl.create(:observation, :species => sp)
-    blogpost = FactoryGirl.create(:post, :observations => [obs])
-    img = FactoryGirl.create(:image, :code => 'picture-of-the-shrike', :observations => [obs])
+    blogpost = FactoryGirl.create(:post, :observations => [@obs])
 
-    assert_equal [img.code], blogpost.images.map(&:code)
-    assert_equal blogpost.code, img.post.code
+    assert_equal [@image.code], blogpost.images.map(&:code)
+    assert_equal blogpost.code, @image.post.code
   end
 
   test 'properly unlink observations when image destroyed' do
-    observation = FactoryGirl.create(:observation)
-    img = FactoryGirl.create(:image, :code => 'picture-of-the-shrike', :observations => [observation])
     assert_nothing_raised do
-      img.destroy
+      @image.destroy
     end
-    assert observation.reload
-    assert_equal 0, observation.images.size
+    assert @obs.reload
+    assert_equal 0, @obs.images.size
   end
 
 
