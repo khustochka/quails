802a200afa9c97fa598f0af43a3a05c4bef36223
xyctka@gmail.com <xyctka@gmail.com>
2012-02-10 10:58:05 +0000

    Fix edit map styling

--- public/javascripts/map.edit.js
+++ public/javascripts/map.edit.js
@@ -2,7 +2,7 @@ $(function () {
 
     function adjustSizes() {
       var clientHeight = $(window).height(),
-          upper = $('#header').outerHeight(true) + $('#new_search').outerHeight();
+          upper = $('#header').outerHeight(true) + $('#new_q').outerHeight();
       $('ul.obs-list').height(clientHeight - upper - 1);
       $('div#googleMap').height(clientHeight - upper);
       $('div#googleMap').width($(window).width() - 381);
