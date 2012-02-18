e6a6398c2302a52f341bdb007c28b3fd62896190
xyctka@gmail.com <xyctka@gmail.com>
2012-02-18 22:25:21 +0200

    Fix setting EN locale for administrative actions

--- app/controllers/mixins/admin_controller.rb
+++ app/controllers/mixins/admin_controller.rb
@@ -10,7 +10,9 @@ module AdminController
       requires_admin_authorized options
       layout 'admin', options
       skip_before_filter :set_locale, options
-      I18n.locale = :en
+      before_filter options do
+        I18n.locale = :en
+      end
     end
 
   end
