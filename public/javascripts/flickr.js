f7d66de5ba9648ba104f5046958472c1a1ccf7d8
xyctka@gmail.com <xyctka@gmail.com>
2012-03-02 10:14:58 +0000

    Better serialize search form inputs

--- public/javascripts/flickr.js
+++ public/javascripts/flickr.js
@@ -3,7 +3,7 @@ $(function () {
     function searchOnFlickr() {
         //found_obs.empty();
         //$('.observation_options').addClass('loading');
-        var data = $("input#flickr_text,input#flickr_date").serializeArray();
+        var data = $(".flickr_search :input").serializeArray();
         $.get("/images/flickr_search", data, function (data) {
             $(data).each(function () {
                 $("<img>", { "src":this.url_s }).appendTo('.flickr_search');
