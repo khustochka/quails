405abbf18ff3b61a1f953879ea957ff53101e751
xyctka@gmail.com <xyctka@gmail.com>
2012-03-29 16:13:42 +0100

    Stricter spots search on map/edit

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -12,10 +12,9 @@ class SpotsController < ApplicationController
   def search
     spots =
         if params[:q] && params[:q].values.uniq != ['']
-          # Though you may think of Spots.search(params[:q])... it only works if query is like
-          # {observation_observ_date: '2011-01-01'} which is problematic for reusing observattions/search partial.
-          # And benchmarking shows it is just a small bit slower.
-          Observation.search(params[:q]).result.joins(:spots).select('lat, lng')
+          Spot.
+              joins("JOIN (#{Observation.search(params[:q]).result.to_sql}) as obs ON observation_id=obs.id").
+              select('lat, lng')
         else
           []
         end
