cacb985cf387f42585564a175138a26a15d24947
xyctka@gmail.com <xyctka@gmail.com>
2012-04-05 11:19:58 +0100

    Fix standalone month names

---
+++ test/integration/i18n_test.rb
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+
+require 'test_helper'
+require 'capybara_helper'
+
+class UIPostsTest < ActionDispatch::IntegrationTest
+
+  include CapybaraTestCase
+
+  test 'Use standalone month names in Russian' do
+    create(:post, face_date: '2011-01-09')
+    visit month_path(year: '2011', month: '01', hl: :ru)
+    find('h1').text.should == 'Январь 2011'
+  end
+
+  test 'Use standalone month names in English' do
+    create(:post, face_date: '2011-01-09')
+    visit month_path(year: '2011', month: '01', hl: :en)
+    find('h1').text.should == 'January 2011'
+  end
+
+  test 'No leading zero in post pubdate' do
+    blogpost = create(:post, face_date: '2011-01-09')
+    visit show_post_path(blogpost.to_url_params.merge(hl: :en))
+    find('time').text.should match(/January 9, 2011/)
+  end
+
+end
