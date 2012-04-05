62305191563c6b2e2e9986c17706ad1ae83b9d81
xyctka@gmail.com <xyctka@gmail.com>
2012-04-05 09:49:40 +0100

    Raise MissingTranslation exception (except for plural rule)

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,4 +1,23 @@
 ALL_LOCALES = [:en, :ru, :uk]
 DEFAULT_PUBLIC_LOCALE = :ru
 
-I18n::Backend::Simple.send(:include, I18n::Backend::Pluralization)
+I18n.available_locales = ALL_LOCALES
+
+if Rails.env.development? || Rails.env.test?
+
+  # raises exception when translation is missing
+  module I18n
+    class JustRaiseHandler < ExceptionHandler
+      def call(exception, locale, key, options)
+        if exception.is_a?(MissingTranslation) && key.to_s != 'i18n.plural.rule'
+          raise exception.to_exception
+        else
+          super
+        end
+      end
+    end
+  end
+
+ I18n.exception_handler = I18n::JustRaiseHandler.new
+
+end
