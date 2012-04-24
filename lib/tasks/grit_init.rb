2217fffb4693de902855dd290e7f8407ceb7332a
xyctka@gmail.com <xyctka@gmail.com>
2012-04-24 14:57:46 +0100

    Just comment

--- lib/tasks/grit_init.rb
+++ lib/tasks/grit_init.rb
@@ -1,5 +1,6 @@
 require 'grit'
 
+# Necessary on Windows in order to find git binary
 Grit::Git.git_binary ||= ENV['PATH'].split(";").
     map { |p| File.join(p, 'git.exe') }.
     find { |p| File.exist?(p) }
