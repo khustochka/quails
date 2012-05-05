e7d7719fddb6ce9d2bc1240a8f04973905d8c031
xyctka@gmail.com <xyctka@gmail.com>
2012-05-05 21:08:49 +0300

    Observations should be sorted for research task

--- app/controllers/research_controller.rb
+++ app/controllers/research_controller.rb
@@ -17,7 +17,7 @@ class ResearchController < ApplicationController
   def more_than_year
     sort_col = params[:sort].try(:to_sym) || :date2
     period = params[:period].try(:to_i) || 365
-    @list = MyObservation.preload(:species).group_by(&:species).inject([]) do |collection, obsdata|
+    @list = MyObservation.order(:observ_date).preload(:species).group_by(&:species).inject([]) do |collection, obsdata|
       sp, obss = obsdata
       obs = obss.each_cons(2).select do |ob1, ob2|
         (ob2.observ_date - ob1.observ_date) >= period
