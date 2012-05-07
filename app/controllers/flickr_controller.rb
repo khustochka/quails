bb20d9285959aa71d5e0e517ea9da859dff637b4
xyctka@gmail.com <xyctka@gmail.com>
2012-05-07 11:48:02 +0100

    Use each_with_object

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -10,15 +10,15 @@ class FlickrController < ApplicationController
 
     @images = Image.preload(:observations).page(params[:page]).per(10)
 
-    @flickr_imgs = @images.inject({}) do |memo, img|
-      memo.merge(
-          img.id => flickr.photos.search(:user_id => '8289389@N04',
-                                         :extras => 'original_format,date_taken',
-                                         :text => img.species[0].name_sci,
-                                         :min_taken_date => img.observ_date - 1,
-                                         :max_taken_date => img.observ_date + 1
+    @flickr_imgs = @images.each_with_object({}) do |img, memo|
+      memo[img.id] =
+          flickr.photos.search(
+              :user_id => '8289389@N04',
+              :extras => 'original_format,date_taken',
+              :text => img.species[0].name_sci,
+              :min_taken_date => img.observ_date - 1,
+              :max_taken_date => img.observ_date + 1
           )
-      )
     end
 
   end
--- app/controllers/research_controller.rb
+++ app/controllers/research_controller.rb
@@ -17,7 +17,7 @@ class ResearchController < ApplicationController
   def more_than_year
     sort_col = params[:sort].try(:to_sym) || :date2
     period = params[:period].try(:to_i) || 365
-    @list = MyObservation.order(:observ_date).preload(:species).group_by(&:species).inject([]) do |collection, obsdata|
+    @list = MyObservation.order(:observ_date).preload(:species).group_by(&:species).each_with_object([]) do |obsdata, collection|
       sp, obss = obsdata
       obs = obss.each_cons(2).select do |ob1, ob2|
         (ob2.observ_date - ob1.observ_date) >= period
