fd88a6871a48f16606600c33e77fd5b0524d9a38
xyctka@gmail.com <xyctka@gmail.com>
2012-05-08 16:30:53 +0100

    Dummy map on image edit

---
+++ app/assets/javascripts/map.image.js
@@ -0,0 +1,43 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require map.init
+
+$(function () {
+
+    var marks,
+        theMap = $('#googleMap');
+
+    theMap.width('800px');
+    theMap.height('600px');
+
+    theMap.gmap3('init');
+
+//    $.get('/map/spots', function (data, textStatus, jqXHR) {
+//        marks = data;
+//        theMap.gmap3(
+//            { action:'addMarkers',
+//                markers:marks,
+//                radius:40,
+//                clusters:{
+//                    // This style will be used for clusters with more than 0 markers
+//                    0:{
+//                        content:'<div class="cluster">CLUSTER_COUNT</div>',
+//                        width:46,
+//                        height:28
+//                    }
+//                }
+//            }
+//        );
+//    });
+
+});
