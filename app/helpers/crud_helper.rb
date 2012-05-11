cf6cafe7c4f183ebd71f94e7a6e0dd66d02e7a2e
xyctka@gmail.com <xyctka@gmail.com>
2012-05-11 14:04:13 +0100

    New/Edit label helper

---
+++ app/helpers/crud_helper.rb
@@ -0,0 +1,14 @@
+module CrudHelper
+
+  def new_or_edit_label(obj)
+    obj.persisted? ? 'Edit' : 'New'
+  end
+
+  def default_submit_button(form)
+    form.button :submit, :value => t('.save_button', :default => 'Save'), 'data-disable-with' => "Saving...", :id => 'save_button'
+  end
+
+  def default_destroy_link(rec)
+    link_to image_tag('/img/x_alt_16x16.png', :title => 'Destroy'), rec, :confirm => 'Are you sure?', :method => :delete, :class => 'destroy'
+  end
+end
