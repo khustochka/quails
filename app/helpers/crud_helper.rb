956ddef6350cb56b0d4d2d8cbf3994251dff7d4e
xyctka@gmail.com <xyctka@gmail.com>
2012-06-13 08:47:23 +0100

    Refactor deprecated feature

--- app/helpers/crud_helper.rb
+++ app/helpers/crud_helper.rb
@@ -9,6 +9,6 @@ module CrudHelper
   end
 
   def default_destroy_link(rec)
-    link_to image_tag('/img/x_alt_16x16.png', :title => 'Destroy'), rec, :confirm => 'Are you sure?', :method => :delete, :class => 'destroy'
+    link_to image_tag('/img/x_alt_16x16.png', title: 'Destroy'), rec, data: {confirm: 'Are you sure?'}, method: :delete, class: 'destroy'
   end
 end
