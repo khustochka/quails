78232fbe880fff2b39ca4e6d50bb8caae85a08c6
xyctka@gmail.com <xyctka@gmail.com>
2012-08-14 14:02:17 +0100

    lifelist.js cleanup

--- app/assets/javascripts/lifelist.js
+++ app/assets/javascripts/lifelist.js
@@ -6,14 +6,9 @@ $(function() {
     // default timeout is causing page reload on heavy pages, like lifelist
     $('.filter-list a, table#lifelist th a').pjax('.main', {timeout: false});
 
-    $(document)
-        .on('pjax:start', '.main', showSpinner)
-        .on('pjax:end', '.main', hideSpinner);
+    $('.main').on('pjax:start', showSpinner);
 
     function showSpinner() {
       $("h1", this).append("<img src='/img/loading.gif'>");
-    }
-
-    function hideSpinner() {
     }
 });
