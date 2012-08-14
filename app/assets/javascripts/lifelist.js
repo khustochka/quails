5a01f5fdd366665f1f61735b4f4460629f7f607f
xyctka@gmail.com <xyctka@gmail.com>
2012-08-14 13:43:23 +0100

    No need in explicit pjax container

--- app/assets/javascripts/lifelist.js
+++ app/assets/javascripts/lifelist.js
@@ -4,7 +4,7 @@
 
 $(function() {
     // default timeout is causing page reload on heavy pages, like lifelist
-    $('.filter-list a, table#lifelist th a').pjax('[data-pjax-container]', {timeout: false});
+    $('.filter-list a, table#lifelist th a').pjax('.main', {timeout: false});
 
     $(document)
         .on('pjax:start', '.main', showSpinner)
