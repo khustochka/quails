dbfd0062709045f21373a5265b1f010c5dae3af8
xyctka@gmail.com <xyctka@gmail.com>
2012-08-14 14:16:04 +0100

    Add pjax for paginated pages

---
+++ app/assets/javascripts/pjax_pagination.js
@@ -0,0 +1,14 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery_pjax
+
+$(function() {
+    // default timeout is causing page reload on heavy pages, like lifelist
+    $('nav.pagination a').pjax('.main', {timeout: false});
+
+    $('.main').on('pjax:start', showSpinner);
+
+    function showSpinner() {
+      $("nav.pagination", this).append("<img src='/img/loading.gif'>");
+    }
+});
