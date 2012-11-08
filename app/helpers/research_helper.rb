117da61cb75ed9d4f34b982f1aa184bb4edefc12
Vitalii Khustochka <xyctka@gmail.com>
2012-11-08 08:54:10 +0000

    extract lambda to helper

--- app/helpers/research_helper.rb
+++ app/helpers/research_helper.rb
@@ -2,4 +2,8 @@ module ResearchHelper
   def research_page_header
     [link_to('Research', research_path), @page_title].join(' : ').html_safe
   end
+
+  def locus_check(loc)
+    image_tag('/img/check_16x13.png', title: loc, alt: loc)
+  end
 end
