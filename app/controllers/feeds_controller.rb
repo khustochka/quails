4c3a46aef049e94672ba43a891638d2a09cbfa07
Vitalii Khustochka <xyctka@gmail.com>
2012-11-12 10:11:28 +0000

    Cleaner sitemap query

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -14,8 +14,7 @@ class FeedsController < ApplicationController
   def sitemap
     @posts = Post.public.select("slug, face_date, updated_at")
     @images = Image.preload(:species).select("id, slug, created_at")
-    @species = Species.joins(:observations).where('species.id != 0').
-        uniq.select('legacy_slug')
+    @species = Species.where(id: Observation.select(:species_id))
 
     # TODO: take into account only the posts shown on home page
     @root_lastmod = Post.public.order('updated_at DESC').limit(1).first.updated_at.iso8601 rescue nil
