0157df04f4c41cf9c36344937b0fbbe9c53aab9d
Vitalii Khustochka <xyctka@gmail.com>
2012-11-28 10:30:28 +0000

    Post's images: the same query can be produced
    with AR associations (uniq is somehow automatic)

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,12 +16,8 @@ class Post < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
   has_many :observations, :dependent => :nullify
   has_many :species, :through => :observations, :order => [:index_num], :uniq => true
-#  has_many :images, :through => :observations, :include => :species, :uniq => true
-
-  def images
-    Image.joins(:observations).where('observations.post_id' => id).includes(:species).
-        order('observations.observ_date, observations.locus_id, images.index_num, species.index_num')
-  end
+  has_many :images, :through => :observations, :include => [:species],
+           :order => 'observations.observ_date, observations.locus_id, images.index_num, species.index_num'
 
   # Convert "timezone-less" face_date to local time zone because AR treats it as UTC (especially necessary for feed updated time)
   def face_date
