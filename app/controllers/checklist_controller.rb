444898f7cf47454260b5421fa44764dc02dbe277
Vitalii Khustochka <xyctka@gmail.com>
2012-12-06 09:58:50 +0000

    Edit also status sign

--- app/controllers/checklist_controller.rb
+++ app/controllers/checklist_controller.rb
@@ -15,7 +15,7 @@ class ChecklistController < ApplicationController
   def save
     params[:s].each do |sp|
       item = LocalSpecies.find(sp[:id])
-      item.update_attributes!(notes_ru: sp[:n])
+      item.update_attributes!(notes_ru: sp[:n], status: sp[:s])
     end
     redirect_to action: :edit
   end
