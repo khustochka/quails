ccbe74e512d5b821a9ce36422d7382d49c6b999a
Vitalii Khustochka <xyctka@gmail.com>
2012-12-12 14:39:38 +0000

    Pass status notes via textile

--- app/helpers/checklists_helper.rb
+++ app/helpers/checklists_helper.rb
@@ -1,9 +1,6 @@
 module ChecklistsHelper
 
-  def convert_statuses(st_string)
-    str = st_string.split.map do |st|
-      translate(st, scope: 'countries.checklist.status')
-    end.join(', ')
+  def convert_status(str)
     RedCloth.new(str, [:no_span_caps, :lite_mode]).to_html.html_safe
   end
 
