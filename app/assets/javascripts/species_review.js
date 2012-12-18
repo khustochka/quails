bcb95890fdabc8ca72437bb084efca8faaae3b73
Vitalii Khustochka <xyctka@gmail.com>
2012-12-18 15:21:18 +0000

    Species review: update species instantly
    Also reborder via JS

--- app/assets/javascripts/species_review.js
+++ app/assets/javascripts/species_review.js
@@ -2,17 +2,37 @@
 
 $(function () {
 
-  var bringUp = $("<span>", {html: "&#8682;", class: "bringUp"});
-  $("form:not('.edit_species') div.input").append(bringUp);
+  var spForm = $('form.edit_species'),
+      bringUp = $("<span>", {html: "&#8682;", class: "bringUp"}),
+      variantInputs = $("form:not('.edit_species') div.input input");
 
   function reborder() {
-    // should remove or add the red border
+    variantInputs.each(
+        function () {
+          var inp = $(this);
+          if (inp.val() === $("#" + inp.data("target")).val())
+            inp.removeClass("mismatch");
+          else
+            inp.addClass("mismatch");
+        }
+    );
   }
 
-  $(".bringUp").on("click", function() {
-    var inp = $(this).prev('input');
+  reborder();
+  variantInputs.after(bringUp);
+
+  $(".bringUp").on("click", function () {
+    var inp = $(this).prev("input");
     $("#" + inp.data("target")).val(inp.val());
+    spForm.submit();
+  });
+
+  spForm.on("ajax:success", function (e, data) {
     reborder();
+  });
+
+  spForm.on("ajax:error", function (e, data) {
+    alert("Error saving species");
   });
 
 });
