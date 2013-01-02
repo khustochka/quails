88efbbbc306a64a1f4b1e63987e7a771513e10dd
Vitalii Khustochka <xyctka@gmail.com>
2013-01-02 08:42:27 +0000

    Update current year in stats

--- app/controllers/my_stats_controller.rb
+++ app/controllers/my_stats_controller.rb
@@ -6,11 +6,11 @@ class MyStatsController < ApplicationController
     ukraine_subregions = Locus.find_by_slug('ukraine').subregion_ids
     @species_ukraine = MyObservation.distinct_species.filter(locus: ukraine_subregions).count
 
-    @species_this_year = MyObservation.distinct_species.filter(year: 2012).count
+    @species_this_year = MyObservation.distinct_species.filter(year: 2013).count
 
     # Count over subquery is faster than Array#size
     rel = MyObservation.aggregate_by_species(:min).
-                    having("EXTRACT(year FROM MIN(observ_date)) = 2012")
+                    having("EXTRACT(year FROM MIN(observ_date)) = 2013")
     @new_this_year = Observation.from("(#{rel.to_sql}) AS obs").count
   end
 
