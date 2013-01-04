4f17c11c99ee88aaf0026977efe14849d8790daf
Vitalii Khustochka <xyctka@gmail.com>
2013-01-04 08:47:15 +0000

    Replace conditions with hash lookups
    Must be a trifle faster

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -11,6 +11,24 @@ module ImagesHelper
       "Original" => 'o'
   }
 
+  LIST_FOR_PICTURED_SPECIES = {
+      true => :ol,
+      false => :ul
+  }
+
+  SENTENCE_SEPARATOR_DEPENDING_ON_POST = {
+      true =>  ';',
+      false => '.'
+  }
+
+  def list_type_for_image_species(image)
+    LIST_FOR_PICTURED_SPECIES[image.multi?]
+  end
+
+  def sentence_separator_depending_on_post(post)
+    SENTENCE_SEPARATOR_DEPENDING_ON_POST[post.present?]
+  end
+
   def jpg_url(img)
     (img.on_flickr? && img.flickr_data['Original']['source']) ||
         legacy_image_url("#{img.slug}.jpg")
