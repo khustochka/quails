04d41521cf5507921290e2c03f2288ed09b38a4b
Vitalii Khustochka <xyctka@gmail.com>
2013-01-04 10:55:48 +0000

    Do not set admin cookie if it is not configured

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,13 @@ class User
   end
 
   def has_admin_cookie?
-    @cookies.signed[User.cookie_name] == User.cookie_value
+    User.cookie_value && @cookies.signed[User.cookie_name] == User.cookie_value
   end
 
   def set_admin_cookie
-    @cookies.signed[User.cookie_name] = {value: User.cookie_value, expires: 1.month.from_now}
+    if User.cookie_value
+      @cookies.signed[User.cookie_name] = {value: User.cookie_value, expires: 1.month.from_now}
+    end
   end
 
   def available_posts
