fc3a9e86326e4003fd4ecf50111b66c5b7482f41
Vitalii Khustochka <xyctka@gmail.com>
2013-01-23 16:02:10 +0000

    A bit more stats

--- app/controllers/my_stats_controller.rb
+++ app/controllers/my_stats_controller.rb
@@ -10,8 +10,29 @@ class MyStatsController < ApplicationController
 
     # Count over subquery is faster than Array#size
     rel = MyObservation.aggregate_by_species(:min).
-                    having("EXTRACT(year FROM MIN(observ_date)) = 2013")
+        having("EXTRACT(year FROM MIN(observ_date)) = 2013")
     @new_this_year = Observation.from("(#{rel.to_sql}) AS obs").count
+
+    # Year with max species
+    @year_with_max_species = MyObservation.distinct_species.
+        group("EXTRACT(YEAR from observ_date)").
+        order("COUNT(DISTINCT species_id) DESC").
+        limit(1).
+        count("DISTINCT species_id").to_a.first
+
+    # Month+year with max species
+    @month_year_with_max_species = MyObservation.distinct_species.
+        group("EXTRACT(YEAR from observ_date), EXTRACT(month from observ_date)").
+        order("COUNT(DISTINCT species_id) DESC").
+        limit(1).
+        count("DISTINCT species_id")
+
+    # Month with max species
+    @month_with_max_species = MyObservation.distinct_species.
+        group("EXTRACT(month from observ_date)").
+        order("COUNT(DISTINCT species_id) DESC").
+        limit(1).
+        count("DISTINCT species_id").to_a.first
   end
 
 end
