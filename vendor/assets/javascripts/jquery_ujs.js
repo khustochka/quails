f17d77834c5002e0315da8e7d1a2d313145f05e7
Vitalii Khustochka <xyctka@gmail.com>
2013-02-19 12:47:12 +0000

    Update assets

--- vendor/assets/javascripts/jquery_ujs.js
+++ vendor/assets/javascripts/jquery_ujs.js
@@ -12,12 +12,7 @@
 
   // Cut down on the number if issues from people inadvertently including jquery_ujs twice
   // by detecting and raising an error when it happens.
-  var alreadyInitialized = function() {
-    var events = $._data(document, 'events');
-    return events && events.click && $.grep(events.click, function(e) { return e.namespace === 'rails'; }).length;
-  }
-
-  if ( alreadyInitialized() ) {
+  if ( $.rails !== undefined ) {
     $.error('jquery-ujs has already been loaded!');
   }
 
@@ -281,9 +276,7 @@
     enableElement: function(element) {
       if (element.data('ujs:enable-with') !== undefined) {
         element.html(element.data('ujs:enable-with')); // set to old enabled state
-        // this should be element.removeData('ujs:enable-with')
-        // but, there is currently a bug in jquery which makes hyphenated data attributes not get removed
-        element.data('ujs:enable-with', false); // clean up cache
+        element.removeData('ujs:enable-with'); // clean up cache
       }
       element.unbind('click.railsDisable'); // enable element
     }
