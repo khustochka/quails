72f0b3009f54bc635f338c53d49a000d9dd9590a
Vitalii Khustochka <xyctka@gmail.com>
2013-03-01 21:35:28 +0200

    Make the search component cache itself

--- lib/service_code/google_search.rb
+++ lib/service_code/google_search.rb
@@ -1,9 +1,28 @@
 class GoogleSearch
 
   def self.render(view)
-    unless Rails.env.test? || view.admin_layout?
-      view.render partial: 'partials/search'
+    cache view, [:google_cse, admin: view.admin_layout?] do
+      unless Rails.env.test? || view.admin_layout?
+        view.render partial: 'partials/search'
+      end
     end
   end
 
+  private
+  def self.cache(view, name = {}, options = nil, &block)
+    if view.controller.perform_caching
+      fragment_for(view, name, options, &block).html_safe
+    else
+      yield
+    end
+  end
+
+  def self.fragment_for(view, name = {}, options = nil, &block)
+    if fragment = view.controller.read_fragment(name, options)
+      fragment
+    else
+      fragment = block.call || ""
+      view.controller.write_fragment(name, fragment, options)
+    end
+  end
 end
