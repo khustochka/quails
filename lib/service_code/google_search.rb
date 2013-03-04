9d3ed4b3bb999965d72b1d75dbbaa20a57273bf9
Vitalii Khustochka <xyctka@gmail.com>
2013-03-04 08:55:35 +0000

    Refactor to a class

--- lib/service_code/google_search.rb
+++ lib/service_code/google_search.rb
@@ -1,32 +1,41 @@
-module GoogleSearch
+class GoogleSearch
 
   def self.render(view)
-    cache view, [:google_cse, admin: view.admin_layout?] do
-      unless Rails.env.test? || view.admin_layout? || @code.blank?
-        view.render partial: 'partials/search', object: @code, as: :code
+    new(view).render
+  end
+
+  def self.configure(code)
+    @@code = code
+  end
+
+  def initialize(view)
+    @view = view
+  end
+
+  def render
+    cache [:google_cse, admin: @view.admin_layout?] do
+      unless Rails.env.test? || @view.admin_layout? || @code.blank?
+        @view.render partial: 'partials/search', object: @@code, as: :code
       end
     end
   end
 
-  def self.configure(code)
-    @code = code
-  end
-
   private
-  def self.cache(view, name = {}, options = nil, &block)
-    if view.controller.perform_caching
-      fragment_for(view, name, options, &block).html_safe
+
+  def cache(name = {}, options = nil, &block)
+    if @view.controller.perform_caching
+      fragment_for(name, options, &block).html_safe
     else
       yield
     end
   end
 
-  def self.fragment_for(view, name = {}, options = nil, &block)
-    if fragment = view.controller.read_fragment(name, options)
+  def fragment_for(name = {}, options = nil, &block)
+    if fragment = @view.controller.read_fragment(name, options)
       fragment
     else
       fragment = block.call || ""
-      view.controller.write_fragment(name, fragment, options)
+      @view.controller.write_fragment(name, fragment, options)
     end
   end
 end
