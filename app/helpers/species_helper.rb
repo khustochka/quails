2ae751017e46118003929eea1f78057b94d8d24e
Vitalii Khustochka <xyctka@gmail.com>
2013-03-05 15:21:22 +0000

    Cache species list

--- app/helpers/species_helper.rb
+++ app/helpers/species_helper.rb
@@ -5,7 +5,7 @@ module SpeciesHelper
   end
 
   def species_for_select
-    [Species::AVIS_INCOGNITA] + Species.alphabetic.all
+    @species_for_select ||= [Species::AVIS_INCOGNITA] + Species.alphabetic.all
   end
 
   def species_link(sp_obj, string = nil)
