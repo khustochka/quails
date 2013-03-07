6bf599da1d74eda7ed2181bad3f9e2e5ab841576
Vitalii Khustochka <xyctka@gmail.com>
2013-03-07 12:46:03 +0000

    Do not load GA code for user with admin cookie

--- lib/service_code/google_analytics.rb
+++ lib/service_code/google_analytics.rb
@@ -3,11 +3,16 @@ require 'service_code/service_code'
 class GoogleAnalytics < ServiceCode
 
   def render
-    cache [:ga_code, admin: @view.admin_layout?] do
-      unless !(Rails.env.production? && Quails.env.real_prod?) || @view.admin_layout? || @@code.blank?
+    cache [:ga_code, admin: may_be_admin?] do
+      unless !(Rails.env.production? && Quails.env.real_prod?) || may_be_admin? || @@code.blank?
         @view.render partial: 'partials/ga_code', object: @@code, as: :code
       end
     end
+  end
+
+  private
+  def may_be_admin?
+    @view.current_user.has_admin_cookie? || @view.admin_layout?
   end
 
 end
