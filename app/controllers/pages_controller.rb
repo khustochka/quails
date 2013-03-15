359f11e331bf89d068033e2c6123baa6dd0df95b
Vitalii Khustochka <xyctka@gmail.com>
2013-03-15 13:32:33 +0000

    Cache only non-admin view of pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
-  caches_page :show, :error, gzip: true
+  caches_page :show, :error, gzip: true, unless: -> { current_user.admin? || current_user.has_admin_cookie? }
 
   def show
     render params[:id]
