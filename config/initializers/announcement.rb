aef784ab7b0e411d3a0ea1ddb3e70d8d713d23a3
Vitalii Khustochka <xyctka@gmail.com>
2013-03-18 10:18:16 +0000

    Announcement styling

--- config/initializers/announcement.rb
+++ config/initializers/announcement.rb
@@ -1,5 +1,9 @@
 require "quails/env"
 
-if Rails.env.production? && Quails.env.vps?
-  $announcement = "This is a test site."
+if Rails.env.production?
+  $announcement = <<-CDATA
+    Это тестовая версия сайта. Оставленные здесь комментарии будут удалены!
+    Оставить пожелания и сообщения об ошибках можно
+    <a href="http://birdwatch.org.ua/2013/03/new-site/">на старом сайте</a>.
+  CDATA
 end
