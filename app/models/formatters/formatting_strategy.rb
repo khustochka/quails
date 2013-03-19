9af0f213706101c5414979341c5d940589a01010
Vitalii Khustochka <xyctka@gmail.com>
2013-03-19 07:56:16 +0000

    More robust links

--- app/models/formatters/formatting_strategy.rb
+++ app/models/formatters/formatting_strategy.rb
@@ -16,7 +16,7 @@ class FormattingStrategy
           if term == "lj"
             lj_user(word)
           else
-            %Q("#{word || term}":#{term})
+            %Q(["#{word || term}":#{term}])
           end
         when '#' then
           post_link(word, term)
