1b8b5df20772d4092ea67294aab4cbc689971b8e
Vitalii Khustochka <xyctka@gmail.com>
2013-03-20 12:40:53 +0000

    Failing test

--- test/unit/image_test.rb
+++ test/unit/image_test.rb
@@ -40,4 +40,16 @@ class ImageTest < ActiveSupport::TestCase
     assert_equal im2, im1.next_by_species(s)
   end
 
+  test "prev and next image by species should be correct for reversed created_at" do
+    o1 = create(:observation, observ_date: "2013-01-01")
+    o2 = create(:observation, observ_date: "2013-02-01")
+    o3 = create(:observation, observ_date: "2013-03-01")
+    s = o1.species
+    im3 = create(:image, observations: [o3])
+    im2 = create(:image, observations: [o2])
+    im1 = create(:image, observations: [o1])
+    assert_equal im1, im2.prev_by_species(s)
+    assert_equal im3, im2.next_by_species(s)
+  end
+
 end
