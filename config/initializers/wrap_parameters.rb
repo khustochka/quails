33d4e45535936eadbfc22dec3c543c32e3cf490b
Vitalii Khustochka <xyctka@gmail.com>
2013-03-22 14:17:22 +0000

    Rollback beta workarounds

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -8,9 +8,7 @@ ActiveSupport.on_load(:action_controller) do
   wrap_parameters format: [:json] if respond_to?(:wrap_parameters)
 end
 
-# FIXME: should be false by default in edge rails
-# To disable root element in JSON for ActiveRecord objects.
-ActiveSupport.on_load(:active_record) do
-  self.include_root_in_json = false
-end
-
+# To enable root element in JSON for ActiveRecord objects.
+#ActiveSupport.on_load(:active_record) do
+#  self.include_root_in_json = true
+#end
