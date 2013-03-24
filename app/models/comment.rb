be5d9c57068b4142eb0bc21ea0ef760faa3d8fa9
Vitalii Khustochka <xyctka@gmail.com>
2013-03-24 17:53:59 +0200

    block comments with too many hrefs in the body

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
 
-  STOP_WORDS = %w( replica vuitton generic zithromax cheap cialis payday loans pharmacy url=http link=http viagra tricor
-                  accutane seroquel retin lasix )
+  STOP_WORDS = %w( replica vuitton generic zithromax cheap cialis payday loans pharmacy url=http link=http
+                    viagra tricor accutane seroquel retin lasix )
 
   ALLOWED_PARAMETERS = [:name, :text, :parent_id]
 
@@ -9,19 +9,24 @@ class Comment < ActiveRecord::Base
 
   validates :text, :name, :post_id, :presence => true
 
+  before_create do
+    self.approved = !self.like_spam?
+    true
+  end
+
   belongs_to :post
   has_many :subcomments, :class_name => 'Comment', :foreign_key => :parent_id, :dependent => :destroy
   belongs_to :parent_comment, :class_name => 'Comment', :foreign_key => :parent_id
-
-  before_create do
-    self.approved = self.text !~ /#{STOP_WORDS.join('|')}/i
-    true
-  end
 
   default_scope { order(:created_at) }
 
   scope :approved, lambda { where(approved: true) }
 
   scope :unapproved, lambda { where(approved: false) }
+
+  def like_spam?
+    self.text =~ /#{STOP_WORDS.join('|')}/i ||
+        self.text.scan(/href=/i).size > 4
+  end
 
 end
