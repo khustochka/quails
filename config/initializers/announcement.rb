7e26d85069e1e27694c0961da0f012695e5f7fc7
Vitalii Khustochka <xyctka@gmail.com>
2013-03-25 10:21:56 +0000

    Show revision in env page

--- config/initializers/announcement.rb
+++ config/initializers/announcement.rb
@@ -7,3 +7,8 @@
 #    <a href="http://birdwatch.org.ua/2013/03/new-site/">на старом сайте</a>.
 #  CDATA
 #end
+
+begin
+  $revision, $commit = `git log -n 1 --format="%H%n%n%B"`.split("\n\n", 2)
+rescue
+end
