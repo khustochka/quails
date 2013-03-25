bd4cbcbf41513cda5034ebe56543d3a71aff637e
Vitalii Khustochka <xyctka@gmail.com>
2013-03-25 14:57:26 +0000

    PJax in image listing

---
+++ app/assets/javascripts/image_show.js
@@ -0,0 +1,19 @@
+//= require jquery_ujs
+//= require jquery_pjax
+//= require pjax_spinner
+
+$(function () {
+
+  // Not used for now
+  function resizeTable() {
+    $('.image_nav').width($('.image_canvas').width());
+  }
+
+  // default timeout is causing page reload on heavy pages, like lifelist
+  $('.main').pjax('a.img_prev_next');
+
+  showSpinner(function (event) {
+    var link = $(event.relatedTarget);
+    link.replaceWith("<img src='/img/loading_small.gif'>");
+  });
+});
