baab3ed148b376893e76dbd1522395eafb1d3072
Vitalii Khustochka <xyctka@gmail.com>
2013-03-27 09:50:05 +0000

    Enable caching debug in dev mode (with env var)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Quails::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = ENV['DEV_CACHING'] || false
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
