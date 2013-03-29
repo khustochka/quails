aae4d8f1feb4a7c1a2b87267c08075bf635bb59b
Vitalii Khustochka <xyctka@gmail.com>
2013-03-29 12:34:21 +0000

    Add spinner for LJ posting

--- app/assets/javascripts/post_form.js
+++ app/assets/javascripts/post_form.js
@@ -32,7 +32,12 @@ $(function () {
   lj_post_form.data('remote', true);
   lj_post_form.attr('action', lj_post_form.attr('action') + '.json');
 
+  lj_post_form.bind('ajax:beforeSend', function () {
+    $("li#lj_url_li").append("<img src='/img/loading_small.gif' id='spinner'>");
+  });
+
   lj_post_form.bind("ajax:error", function (e, xhr) {
+    $('#spinner').remove();
     alert($.parseJSON(xhr.responseText).alert);
   });
 
