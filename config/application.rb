6605c2a0fccb40fb7ce1ddbf9e08b9854aebbb85
Vitalii Khustochka <xyctka@gmail.com>
2013-04-02 12:31:08 +0100

    Update bundle require

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 
 
 # Assets should be precompiled for production (so we don't need the gems loaded then)
-Bundler.require(*Rails.groups(assets: %w(development test)))
+Bundler.require(:default, Rails.env)
 
 module Quails
   class Application < Rails::Application
