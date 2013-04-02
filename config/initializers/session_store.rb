9a5f24df84bea54b681d5772810e396e77d0430f
Vitalii Khustochka <xyctka@gmail.com>
2013-04-02 12:31:00 +0100

    Update cookie store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Quails::Application.config.session_store :encrypted_cookie_store, key: '_quails_session'
+Quails::Application.config.session_store :cookie_store, key: '_quails_session'
