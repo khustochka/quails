6d7c411bdeab33cf80aca542ea024f4d3b595913
Vitalii Khustochka <xyctka@gmail.com>
2013-05-24 13:00:30 +0100

    Search integration tests

--- test/integration/ui_observations_test.rb
+++ test/integration/ui_observations_test.rb
@@ -17,42 +17,4 @@ class UIObservationsTest < ActionDispatch::IntegrationTest
     assert_false observation.voice
   end
 
-  test 'Searching and showing Avis incognita observations' do
-    card = create(:card, observ_date: "2010-06-18")
-    create(:observation, species_id: 0, card: card)
-    create(:observation, species_id: 0, card: create(:card, observ_date: "2010-06-19"))
-    create(:observation, species: seed(:spinus), card: card)
-    login_as_admin
-    visit observations_path
-    select('- Avis incognita', from: 'Species')
-    click_button('Search')
-    assert_equal 200, page.driver.response.status
-    assert find('table.obs_list').has_content?('- Avis incognita')
-    assert_false find('table.obs_list').has_content?('Spinus spinus')
-  end
-
-  test 'Searching observations by species works properly' do
-    card = create(:card, observ_date: "2010-06-18")
-    create(:observation, species: seed(:pasdom), card: card)
-    create(:observation, species: seed(:fulatr), card: card)
-    login_as_admin
-    visit observations_path
-    select('Passer domesticus', from: 'Species')
-    click_button('Search')
-    assert find('table.obs_list').has_content?('Passer domesticus')
-    assert_false find('table.obs_list').has_content?('Fulica atra')
-  end
-
-  test 'Searching observations by mine/not mine works properly' do
-    card = create(:card, observ_date: "2010-06-18")
-    create(:observation, species: seed(:pasdom), card: card, mine: false)
-    create(:observation, species: seed(:fulatr), card: card)
-    login_as_admin
-    visit observations_path
-    choose('Not mine')
-    click_button('Search')
-    assert find('table.obs_list').has_content?('Passer domesticus')
-    assert_false find('table.obs_list').has_content?('Fulica atra')
-  end
-
 end
