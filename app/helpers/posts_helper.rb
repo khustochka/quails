bbcedddc258d19fb31319cf98d72bf999b383a3c
Vitalii Khustochka <xyctka@gmail.com>
2013-05-29 10:21:31 +0100

    Show post title in the link

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,7 +2,7 @@ module PostsHelper
 
   def post_link(text, blogpost, show_text_if_no_post = false)
     if blogpost
-      link_to(text, public_post_path(blogpost), class: ('grayout' unless blogpost.public?))
+      link_to(text, public_post_path(blogpost), title: blogpost.formatted.title, class: ('grayout' unless blogpost.public?))
     else
       show_text_if_no_post ? text : nil
     end
