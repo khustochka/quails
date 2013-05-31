76b3a4a9860d165f5cf4ec79f2df2dd4575d31f2
Vitalii Khustochka <xyctka@gmail.com>
2013-05-31 08:59:02 +0100

    Better format

--- app/models/image_asset_item.rb
+++ app/models/image_asset_item.rb
@@ -1,12 +1,19 @@
 class ImageAssetItem < Struct.new(:type, :width, :height, :url)
 
+  TYPE_TO_STR = {local: "l", flickr: "f"}
+  STR_TO_TYPE = TYPE_TO_STR.invert
+
   def self.dump(obj)
-    [obj.type, obj.width, obj.height, obj.url].join(",")
+    [TYPE_TO_STR[obj.type], obj.width, obj.height, obj.url].join(",")
   end
 
   def self.load(str)
     t, w, h, u = str.split(",")
-    new(t.to_sym, w.to_i, h.to_i, u)
+    new(STR_TO_TYPE[t], w.to_i, h.to_i, u)
+  end
+
+  def inspect
+    '%s: "%dx%d" %s' % [type, width, height, url.inspect]
   end
 
 end
