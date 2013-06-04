a7927e7a872c8301acc350471e66ee379fc33d69
Vitalii Khustochka <xyctka@gmail.com>
2013-06-04 18:21:59 +0100

    better max width

--- app/helpers/justify_helper.rb
+++ app/helpers/justify_helper.rb
@@ -1,26 +1,25 @@
 module JustifyHelper
   BORDER = 4
-  MAX_WIDTH = 968
 
   def justify(thumbs)
     result = []
     current_row = []
 
-    init_height = ImagesHelper::THUMBNAIL_HEIGHT
+    max_width = @wider ? 1120 : 896
 
 
     thumbs.each do |thumb|
       current_row << thumb
       sum_width = width(current_row)
-      if sum_width > MAX_WIDTH * 1.01
-        ratio = MAX_WIDTH.to_f / sum_width
+      if sum_width > max_width * 1.01
+        ratio = max_width.to_f / sum_width
         current_row.each { |th| th.force_width((th.width * ratio).to_i) }
 
         new_width = width(current_row)
-        if new_width > MAX_WIDTH
+        if new_width > max_width
           current_row.cycle do |el|
-            puts new_width - MAX_WIDTH
-            break if new_width == MAX_WIDTH
+            puts new_width - max_width
+            break if new_width == max_width
             el.force_width(el.width - 1)
             new_width -= 1
           end
