387e987b757bbf5acac54df9100f7e1bb070eda8
Vitalii Khustochka <xyctka@gmail.com>
2013-06-05 14:03:39 +0100

    simplify justify algorithm

--- app/helpers/justify_helper.rb
+++ app/helpers/justify_helper.rb
@@ -11,25 +11,35 @@ module JustifyHelper
 
     max_width = @wider ? 1120 : 896
 
+    sum_width = 0
 
     thumbs.each do |thumb|
       current_row << thumb
-      sum_width = width(current_row)
-      if sum_width > max_width * 1.01
+      sum_width += thumb.width + (BORDER * 2)
+      if sum_width > max_width * 1.1
         ratio = max_width.to_f / sum_width
-        current_row.each { |th| th.force_width((th.width * ratio).to_i) }
-
-        new_width = width(current_row)
+        new_width = current_row.inject(0) do |nw, th|
+          w = (th.width * ratio).to_i
+          th.force_width(w)
+          nw + w + (BORDER * 2)
+        end
+        #p "new_width=#{new_width}"
         if new_width > max_width
           current_row.cycle do |el|
             break if new_width == max_width
+            #p el.width
             el.force_width(el.width - 1)
             new_width -= 1
+            #p el.width
           end
         end
 
+
+        #p "adjusted_width=#{width(current_row)}"
+
         result << current_row
         current_row = []
+        sum_width = 0
 
       end
     end
