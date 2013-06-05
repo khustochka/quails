01082f80744187dc6c7d3df850d94837aa256952
Vitalii Khustochka <xyctka@gmail.com>
2013-06-05 16:29:30 +0100

    adjustments to implement increased height

--- app/helpers/justify_helper.rb
+++ app/helpers/justify_helper.rb
@@ -1,5 +1,6 @@
 module JustifyHelper
   BORDER = 4
+  INCREASE_ENABLED = false
 
   def render_justified(array)
     render 'images/justified', array: array
@@ -12,18 +13,30 @@ module JustifyHelper
     max_width = @wider ? 1120 : 896
 
     sum_width = 0
+    temp_el = nil
 
     thumbs.each do |thumb|
       current_row << thumb
+      pre_sum_width = sum_width
       sum_width += thumb.width + (BORDER * 2)
       if sum_width > max_width * 1.1
         ratio = max_width.to_f / sum_width
+
+        if INCREASE_ENABLED
+          if ratio < 0.85
+            ratio = pre_sum_width.to_f / sum_width
+            temp_el = current_row.pop
+          end
+        end
+
         new_width = current_row.inject(0) do |nw, th|
           w = (th.width * ratio).to_i
           th.force_width(w)
           nw + w + (BORDER * 2)
         end
         #p "new_width=#{new_width}"
+
+        #Reducing
         if new_width > max_width
           current_row.cycle do |el|
             break if new_width == max_width
@@ -34,12 +47,19 @@ module JustifyHelper
           end
         end
 
+        #Increasing
+        # TODO: should force height and recalc total width
 
         #p "adjusted_width=#{width(current_row)}"
 
         result << current_row
         current_row = []
         sum_width = 0
+        if temp_el
+          current_row << temp_el
+          sum_width = temp_el.width + (BORDER * 2)
+          temp_el = nil
+        end
 
       end
     end
