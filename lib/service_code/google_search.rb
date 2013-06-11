f1e6d7bc2947b095ff8e39dcf41a5df5383c6089
Vitalii Khustochka <xyctka@gmail.com>
2013-06-11 12:46:39 +0100

    Show search to admin

--- lib/service_code/google_search.rb
+++ lib/service_code/google_search.rb
@@ -3,8 +3,8 @@ require 'service_code/service_code'
 class GoogleSearch < ServiceCode
 
   def render
-    cache [:google_cse, admin: @view.admin_layout?] do
-      unless Rails.env.test? || @view.admin_layout? || config.code.blank?
+    cache :google_cse do
+      unless Rails.env.test? || config.code.blank?
         @view.render partial: 'partials/search', object: config.code, as: :code
       end
     end
