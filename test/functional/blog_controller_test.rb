117483d64b72c8b0b7d1a44236415b05e3510fb5
Vitalii Khustochka <xyctka@gmail.com>
2013-06-11 10:26:31 +0100

    remove unnecessary tests

--- test/functional/blog_controller_test.rb
+++ test/functional/blog_controller_test.rb
@@ -55,38 +55,6 @@ class BlogControllerTest < ActionController::TestCase
     assert_includes(assigns(:posts), blogpost2)
   end
 
-  test 'show correct Earlier Posts link if limit is on the month border' do
-    blogpost1 = create(:post, face_date: '2007-12-06 13:14:15')
-    blogpost2 = create(:post, face_date: '2007-11-06 13:14:15')
-    blogpost3 = create(:post, face_date: '2007-12-05 13:14:15')
-    blogpost4 = create(:post, face_date: '2007-10-06 13:14:15')
-    blogpost5 = create(:post, face_date: '2007-10-05 13:14:15')
-    get :front_page
-    assert_response :success
-    assert_includes(assigns(:posts), blogpost1)
-    assert_includes(assigns(:posts), blogpost2)
-    assert_includes(assigns(:posts), blogpost3)
-    refute_includes(assigns(:posts), blogpost4)
-    refute_includes(assigns(:posts), blogpost5)
-    assert_equal blogpost4.to_month_url, assigns(:prev_month)
-  end
-
-  test 'show correct Earlier Posts link if limit is inside the month' do
-    blogpost1 = create(:post, face_date: '2007-12-06 13:14:15')
-    blogpost2 = create(:post, face_date: '2007-12-05 13:14:15')
-    blogpost3 = create(:post, face_date: '2007-11-06 13:14:15')
-    blogpost4 = create(:post, face_date: '2007-11-05 13:14:15')
-    blogpost5 = create(:post, face_date: '2007-11-04 13:14:15')
-    get :front_page
-    assert_response :success
-    assert_includes(assigns(:posts), blogpost1)
-    assert_includes(assigns(:posts), blogpost2)
-    assert_includes(assigns(:posts), blogpost3)
-    refute_includes(assigns(:posts), blogpost4)
-    refute_includes(assigns(:posts), blogpost5)
-    assert_equal blogpost4.to_month_url, assigns(:prev_month)
-  end
-
   test 'show full month and correct Earlier Posts link if the last month exceeds the limit' do
     blogpost1 = create(:post, face_date: '2007-11-30 13:14:15')
     blogpost2 = create(:post, face_date: '2007-11-06 13:14:15')
