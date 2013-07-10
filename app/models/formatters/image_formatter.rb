a9617544e92fd1a568e256431329c6f735002ad5
Vitalii Khustochka <xyctka@gmail.com>
2013-07-10 12:14:47 +0100

    Add description meta tag for images

--- app/models/formatters/image_formatter.rb
+++ app/models/formatters/image_formatter.rb
@@ -1,11 +1,23 @@
 class ImageFormatter < ModelFormatter
 
+  include ActionView::Helpers::SanitizeHelper
+  include ActionView::Helpers::TranslationHelper
+
   def title
     OneLineFormatter.apply(@model.public_title)
   end
 
   def description
-    WikiFormatter.new(@model.description).for_site
+    @description ||= WikiFormatter.new(@model.description).for_site
+  end
+
+  def meta_description
+    descr = "Снято #{l(@model.observ_date, :format => :long)}, #{@model.locus.name}."
+    if description.present?
+      descr << "\n"
+      descr << strip_tags(description)
+    end
+    descr
   end
 
 end
