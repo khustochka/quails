7083a4d7696a59f30bcd5221f90802c6e7a4ccba
Vitalii Khustochka <xyctka@gmail.com>
2013-07-29 05:23:37 +0300

    Justify: stretch last row

--- app/helpers/justify_helper.rb
+++ app/helpers/justify_helper.rb
@@ -24,7 +24,7 @@ module JustifyHelper
       Rails.logger.debug "sum_width=#{sum_width}"
       if sum_width > max_width
         border_sum = num_thumbs * (BORDER * 2)
-        ratio = (max_width.to_f - border_sum) / (sum_width - border_sum + 5)
+        ratio = (max_width.to_f - border_sum) / (sum_width - border_sum)
 
         if INCREASE_ENABLED
           if ratio < 0.85
@@ -36,7 +36,7 @@ module JustifyHelper
         end
 
         new_height = (ImagesHelper::THUMBNAIL_HEIGHT * ratio).to_i
-        new_widths = current_row.map {|th| (th.width * ratio).to_i}
+        new_widths = current_row.map { |th| (th.width * ratio).to_i }
 
         new_width = new_widths.sum + (BORDER * 2 * current_row.size)
 
@@ -73,6 +73,37 @@ module JustifyHelper
         end
 
       end
+    end
+
+    border_sum = num_thumbs * (BORDER * 2)
+    ratio = (max_width.to_f - border_sum) / (sum_width - border_sum)
+
+    if ratio < 1.2
+      new_height = (ImagesHelper::THUMBNAIL_HEIGHT * ratio).to_i
+      new_widths = current_row.map { |th| (th.width * ratio).to_i }
+
+      new_width = new_widths.sum + (BORDER * 2 * current_row.size)
+
+      Rails.logger.debug "new_width=#{new_width}"
+
+      # Pixel adjustment
+      if new_width != max_width
+        delta = (max_width - new_width)
+        pixel = delta / delta.abs
+
+        new_height += (delta * pixel / new_widths.size * 0.75).to_i
+        (0..new_widths.size - 1).cycle do |idx|
+          break if new_width == max_width
+          new_widths[idx] += pixel
+          new_width += pixel
+        end
+      end
+
+      current_row.each_with_index do |el, idx|
+        el.force_dimensions(width: new_widths[idx], height: new_height)
+      end
+
+      Rails.logger.debug "adjusted_width=#{width(current_row)}"
     end
 
     result << current_row
