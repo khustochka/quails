993c238694cc91565dec1d8ece5899ff6f51d8b8
Vitalii Khustochka <xyctka@gmail.com>
2013-08-13 09:52:19 +0100

    fix regex

--- app/models/formatters/feed_format_strategy.rb
+++ app/models/formatters/feed_format_strategy.rb
@@ -3,7 +3,7 @@ class FeedFormatStrategy < SiteFormatStrategy
   def initialize(text, metadata = {})
     new_text = text.
         gsub(/(href|src)=("|')\//, "\\1=\\2http://#{metadata[:host]}/").
-        gsub(/:\//, ":http://#{metadata[:host]}/")
+        gsub(/:\/(?!\/)/, ":http://#{metadata[:host]}/")
     super(new_text, metadata)
   end
 
