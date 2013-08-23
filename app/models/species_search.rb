9f8e61b51ba9c88f5824a9a9a1e5fde0799e1f6f
Vitalii Khustochka <xyctka@gmail.com>
2013-08-23 16:40:56 +0100

    Simple search by all languages

--- app/models/species_search.rb
+++ app/models/species_search.rb
@@ -1,12 +1,19 @@
 class SpeciesSearch
 
+  FILTER = lambda do |term|
+    %w(name_sci name_ru name_en name_uk).map do |field|
+      "#{field} ILIKE '#{term}%' OR #{field} ILIKE '% #{term}%' OR #{field} ILIKE '%-#{term}%'"
+    end.join(" OR ")
+  end
+
   def self.find(term)
     return [] if term.blank?
-    Species.
-        select("name_sci, name_en, name_ru, name_uk").
-        where("name_sci ILIKE '#{term}%' OR name_sci ILIKE '% #{term}%'").
-        limit(5).
-        map { |sp| SpeciesSearchResult.new(sp) }
+    rel = Species.
+        select("DISTINCT name_sci, name_en, name_ru, name_uk").
+        where(FILTER.call(term)).
+        limit(5)
+
+    rel.map { |sp| SpeciesSearchResult.new(sp) }
   end
 
 end
