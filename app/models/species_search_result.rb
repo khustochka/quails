8c5f98a9a461ed34cacfeb7f0f0ffe089a1cdf28
Vitalii Khustochka <xyctka@gmail.com>
2013-08-23 16:32:08 +0100

    Add species name to search suggest

--- app/models/species_search_result.rb
+++ app/models/species_search_result.rb
@@ -1,11 +1,11 @@
-class SpeciesSearchResult < Struct.new(:name_sci, :name_ru)
+class SpeciesSearchResult < Struct.new(:name_sci, :name)
 
   def initialize(sp)
-    super(sp.name_sci, sp.name_ru)
+    super(sp.name_sci, sp.name)
   end
 
   def as_json(*options)
-    {label: name_sci, url: "/species/#{Species.parameterize(name_sci)}"}
+    {label: name_sci, url: "/species/#{Species.parameterize(name_sci)}", name: name}
   end
 
 end
