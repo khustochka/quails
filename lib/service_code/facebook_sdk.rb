5d3a0a8838871bcd9cb8f7457183fa22640b7dc8
Vitalii Khustochka <xyctka@gmail.com>
2013-08-29 12:32:47 +0100

    Remove caches where they are unnecessary

--- lib/service_code/facebook_sdk.rb
+++ lib/service_code/facebook_sdk.rb
@@ -3,10 +3,8 @@ require 'service_code/service_code'
 class FacebookSdk < ServiceCode
 
   def render
-    cache :facebook_sdk do
-      unless Rails.env.test? || config.code.blank?
-        @view.render partial: 'social/fb_sdk', object: config.code, as: :code
-      end
+    unless Rails.env.test? || config.code.blank?
+      @view.render partial: 'social/fb_sdk', object: config.code, as: :code
     end
   end
 
