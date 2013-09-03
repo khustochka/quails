d9da7867ec466267c469082c1ac50d4253f1b3f4
Vitalii Khustochka <xyctka@gmail.com>
2013-09-03 14:54:20 +0100

    Fix the test

--- test/unit/species_search_test.rb
+++ test/unit/species_search_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SpeciesSearchTest < ActionView::TestCase
 
   test 'simple search by scientific name' do
-    result = SpeciesSearch.new(Species.all, 'garrul').find
+    result = SpeciesSearch.new(Admin.new({}).searchable_species, 'garrul').find
     assert_equal ["Garrulus glandarius", "Coracias garrulus", "Bombycilla garrulus"], result.map(&:name_sci)
   end
 
