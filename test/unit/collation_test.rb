1f3b31040d136f515899b546d1c021bdb049fad7
Vitalii Khustochka <xyctka@gmail.com>
2013-09-09 09:59:31 +0100

    Explicitly test collation instead of alphabetic order

---
+++ test/unit/collation_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CollationTest < ActiveSupport::TestCase
+
+  test 'collation should be C' do
+    assert_match /^C/, ActiveRecord::Base.connection.select_rows("SHOW LC_COLLATE")[0][0]
+  end
+
+end
