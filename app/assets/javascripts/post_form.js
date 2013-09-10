3021a5d598f07cdfeda5fac1733074a13123200e
Vitalii Khustochka <xyctka@gmail.com>
2013-09-10 17:12:58 +0100

    Extract textarea wikificator

--- app/assets/javascripts/post_form.js
+++ app/assets/javascripts/post_form.js
@@ -1,22 +1,8 @@
 //= require suggest_over_combo
 //= require post_cards
+//= require wiki_form
 
-// Tag insertion
 $(function () {
-  var textarea = $('#post_text'),
-      sp_insert_button = $("<button>", {text: "[species]", id: "insert_sp_tag", type: "button"});
-
-  // Add a button
-
-  $("<div>", {class: "edit_tools"}).insertBefore(textarea);
-
-  sp_insert_button.click(function () {
-    insertTags('{{', '|}}', 'вид');
-  });
-
-  sp_insert_button.appendTo(".edit_tools");
-
-
   // Cards search form
 
   var searchForm = $('form.search');
@@ -46,69 +32,6 @@ $(function () {
     $("li#lj_url_li").html($("<a>", {text: url, href: url}));
   });
 });
-
-function insertTags(tagOpen, tagClose, sampleText) {
-  var txtarea;
-  if (document.editform) {
-    txtarea = document.editform.wpTextbox1;
-  } else {
-    var areas = document.getElementsByTagName("textarea");
-    txtarea = areas[areas.length - 1];
-  }
-  var selText, isSample = false;
-  if (document.selection && document.selection.createRange) {
-    if (document.documentElement && document.documentElement.scrollTop) {
-      var winScroll = document.documentElement.scrollTop;
-    } else if (document.body) {
-      var winScroll = document.body.scrollTop;
-    }
-    txtarea.focus();
-    var range = document.selection.createRange();
-    selText = range.text;
-    checkSelectedText();
-    range.text = tagOpen + selText + tagClose;
-    if (isSample && range.moveStart) {
-      if (window.opera) {
-        tagClose = tagClose.replace(/\n/g, "");
-      }
-      range.moveStart("character", -tagClose.length - selText.length);
-      range.moveEnd("character", -tagClose.length);
-    }
-    range.select();
-    if (document.documentElement && document.documentElement.scrollTop) {
-      document.documentElement.scrollTop = winScroll;
-    } else if (document.body) {
-      document.body.scrollTop = winScroll;
-    }
-  } else if (txtarea.selectionStart || txtarea.selectionStart == "0") {
-    var textScroll = txtarea.scrollTop;
-    txtarea.focus();
-    var startPos = txtarea.selectionStart;
-    var endPos = txtarea.selectionEnd;
-    selText = txtarea.value.substring(startPos, endPos);
-    checkSelectedText();
-    txtarea.value = txtarea.value.substring(0, startPos) + tagOpen + selText + tagClose + txtarea.value.substring(endPos, txtarea.value.length);
-    if (isSample) {
-      txtarea.selectionStart = startPos + tagOpen.length;
-      txtarea.selectionEnd = startPos + tagOpen.length + selText.length;
-    } else {
-      txtarea.selectionStart = startPos + tagOpen.length + selText.length + tagClose.length;
-      txtarea.selectionEnd = txtarea.selectionStart;
-    }
-    txtarea.scrollTop = textScroll;
-  }
-
-  function checkSelectedText() {
-    if (!selText) {
-      selText = sampleText;
-      isSample = true;
-    } else if (selText.charAt(selText.length - 1) == " ") {
-      selText = selText.substring(0, selText.length - 1);
-      tagClose += " ";
-    }
-  }
-
-}
 
 function reloadCardsSearch() {
   $('form.search').submit();
