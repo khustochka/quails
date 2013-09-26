0ff4646e24629e0ccfc01e7d76b85ae2d3ddd184
Vitalii Khustochka <xyctka@gmail.com>
2013-09-26 12:07:57 +0100

    Update assets

--- config/initializers/jquery.rb
+++ config/initializers/jquery.rb
@@ -1,3 +1,4 @@
+# Jquery series 2.x does not support IE7 properly (e.g .the Map page)
 JQUERY_CDN = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
 JQUERYUI_CDN = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"
 
