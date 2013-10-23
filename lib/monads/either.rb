f35dc878d3bca18736a39b2359f605f105ef86c3
Vitalii Khustochka <xyctka@gmail.com>
2013-10-23 10:52:02 +0100

    Apply Either monad to flickr

---
+++ lib/monads/either.rb
@@ -0,0 +1,63 @@
+module Either
+
+  def self.value(value)
+    Value.new(value)
+  end
+
+  def self.error(msg)
+    Error.new(msg)
+  end
+
+  class Value
+    def initialize(value)
+      @value = value
+    end
+
+    def valid?
+      true
+    end
+
+    def error?
+      false
+    end
+
+    def get
+      @value
+    end
+
+    def method_missing(method, *args, &block)
+      begin
+        Value.new(@value.send(method, *args, &block))
+      rescue => e
+        Error.new(e.message)
+      end
+    end
+  end
+
+  class Error
+    attr_reader :errors
+    def initialize(msg)
+      @msg = msg
+      @errors = ActiveModel::Errors.new(self)
+      @errors.add(:base, msg)
+    end
+
+    def error?
+      true
+    end
+
+    def valid?
+      false
+    end
+
+    def get
+      raise "Do not run get on Error!"
+    end
+
+    def method_missing(method, *args, &block)
+      self
+    end
+
+  end
+
+end
