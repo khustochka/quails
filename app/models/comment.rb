9d5d4c713081f725f7496d5711bb6c0f3151ed6f
Vitalii Khustochka <xyctka@gmail.com>
2013-12-04 09:42:41 +0000

    Comment for with email: first touch

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,6 +12,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post, touch: :commented_at
   has_many :subcomments, :class_name => 'Comment', :foreign_key => :parent_id, :dependent => :destroy
   belongs_to :parent_comment, :class_name => 'Comment', :foreign_key => :parent_id
+  belongs_to :commenter
 
   default_scope { order(:created_at) }
 
---
+++ app/models/commenter.rb
@@ -0,0 +1,3 @@
+class Commenter < ActiveRecord::Base
+  has_many :comments
+end
