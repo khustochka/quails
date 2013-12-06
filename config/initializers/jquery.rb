972d37391b2c3df1dbb3df9189cb9bff2e60797a
Vitalii Khustochka <xyctka@gmail.com>
2013-12-06 10:07:55 +0000

    Protocol-dependent CDN

--- config/initializers/jquery.rb
+++ config/initializers/jquery.rb
@@ -1,5 +1,5 @@
 # Jquery series 2.x does not support IE7 properly (e.g .the Map page)
-JQUERY_CDN = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
-JQUERYUI_CDN = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"
+JQUERY_CDN = "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
+JQUERYUI_CDN = "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"
 
 JQUERY_URL = Rails.env.production? ? JQUERY_CDN : "jquery.min.js"
