3e51f71915483cdff5af378e6ced1646abeb7574
Vitalii Khustochka <xyctka@gmail.com>
2013-12-17 14:53:50 +0000

    Split observations/search in html and for map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -14,4 +14,26 @@ class MapsController < ApplicationController
     @spot = Spot.new(public: true)
   end
 
+  # GET "/map/observations.json"
+  def observations
+    preload_tables = [{:card => :locus}, :species, :spots]
+    json_methods = [:spots]
+
+    # Have to do outer join to preserve Avis incognita
+    observs =
+        params[:q] && params[:q].delete_if { |_, v| v.empty? }.present? ?
+            ObservationSearch.new(params[:q]).observations.
+                joins("LEFT OUTER JOIN species ON species_id = species.id").
+                preload(preload_tables).
+                order('cards.observ_date', 'cards.locus_id', 'species.index_num').limit(params[:limit] || 200) :
+            []
+
+    respond_to do |format|
+      format.json { render json:
+                               { json: observs.as_json(only: :id, methods: json_methods),
+                                 html: render_to_string(partial: 'maps/observation', collection: observs, formats: [:html]) }
+      }
+    end
+  end
+
 end
