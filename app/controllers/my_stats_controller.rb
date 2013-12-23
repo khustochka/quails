7201f2211f3023362efd55fa68d33e139a48757e
Vitalii Khustochka <xyctka@gmail.com>
2013-12-23 14:00:41 +0000

    Updated Stats page (1st touch)

--- app/controllers/my_stats_controller.rb
+++ app/controllers/my_stats_controller.rb
@@ -3,69 +3,18 @@ class MyStatsController < ApplicationController
   def index
     # Subquery is faster than join
     @total = MyObservation.distinct_species.count
-    ukraine_subregions = Locus.find_by_slug('ukraine').subregion_ids
-    @species_ukraine = MyObservation.distinct_species.filter(locus: ukraine_subregions).count
+    observations_filtered = Observation.joins(:card)
+    identified_observations = observations_filtered.identified
+    lifelist_filtered = Lifelist.basic.relation
 
-    @species_this_year = MyObservation.distinct_species.filter(year: 2013).count
+    @year_data = identified_observations.select('EXTRACT(year FROM observ_date)::integer as year,
+                                      COUNT(observations.id) as count_obs,
+                                      COUNT(DISTINCT observ_date) as count_days,
+                                      COUNT(DISTINCT species_id) as count_species').
+        group('EXTRACT(year FROM observ_date)').order('year')
 
-    # Count over subquery is faster than Array#size
-    rel = MyObservation.joins(:card).aggregate_by_species(:min).
-        having("EXTRACT(year FROM MIN(observ_date))::integer = 2013")
-    @new_this_year = Observation.from("(#{rel.to_sql}) AS obs").count
+    @first_sp_by_year = lifelist_filtered.group('EXTRACT(year FROM first_seen)::integer').except(:order).count(:all)
 
-    # Year with max species
-    query = MyObservation.joins(:card).
-        select("COUNT(DISTINCT species_id) as sp_count, EXTRACT(YEAR from observ_date)::integer as year").
-        group("EXTRACT(YEAR from observ_date)::integer").
-        order("sp_count DESC").
-        limit(1)
-    @year_with_max_species = Hashie::Mash.new(Observation.connection.select_one(query, "Year with max species"))
-
-    # Month+year with max species
-    query = MyObservation.joins(:card).
-        select("COUNT(DISTINCT species_id) as sp_count, EXTRACT(YEAR from observ_date)::integer as year, EXTRACT(month from observ_date)::integer as month").
-        group("EXTRACT(YEAR from observ_date)::integer, EXTRACT(month from observ_date)::integer").
-        order("sp_count DESC").
-        limit(1)
-    @month_year_with_max_species = Hashie::Mash.new(Observation.connection.select_one(query, "Month, year with max species"))
-
-    # Month with max species
-    query = MyObservation.joins(:card).
-        select("COUNT(DISTINCT species_id) as sp_count, EXTRACT(MONTH from observ_date)::integer as month").
-        group("EXTRACT(month FROM observ_date)::integer").
-        order("sp_count DESC").
-        limit(1)
-    @month_with_max_species = Hashie::Mash.new(Observation.connection.select_one(query, "Month with max species"))
-
-    # Day with max species
-    query = MyObservation.joins(:card).
-        select("COUNT(DISTINCT species_id) as sp_count, observ_date").
-        group("observ_date").
-        order("sp_count DESC").
-        limit(1)
-    @day_with_max_species = Hashie::Mash.new(Observation.connection.select_one(query, "Day with max species"))
-
-    # Day with max new species
-    new_sp_query = MyObservation.joins(:card).select("species_id, MIN(observ_date) as observed_at").group(:species_id).to_sql
-    query = Observation.
-        select("COUNT(species_id) as sp_count, observed_at").
-        from("(#{new_sp_query}) as new_sp_dates").
-        group("observed_at").
-        order("sp_count DESC").
-        limit(1)
-    @day_with_max_new_species = Hashie::Mash.new(Observation.connection.select_one(query, "Day with max new species"))
-
-    # Newest species
-    @newest_species = Lifelist.basic.relation.first
-
-    # Oldest species
-    @oldest_species = Lifelist.advanced.sort('last').relation.reorder('last_seen ASC').first
-
-    # Most often seen species
-    @most_often_species = Lifelist.advanced.sort('count').relation.first
-
-    # Species met only once # TODO: can be done via SQL
-    @sp_met_once = MyObservation.group(:species_id).having('COUNT(id) = 1').count.size
   end
 
 end
