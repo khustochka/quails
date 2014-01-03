8d5f08112a7a27c7f40339dc15357f0f3db6fdf9
Vitalii Khustochka <xyctka@gmail.com>
2014-01-03 15:56:04 +0000

    Fix contest

--- contest.rb
+++ contest.rb
@@ -30,11 +30,11 @@ end
 
 SPCS = Hash[Species.all.map { |s| [s.id, s.name_sci] }]
 
-obs = MyObservation.
+obs = MyObservation.joins(:card).
     select('DISTINCT observ_date, species_id').
     where('EXTRACT(year FROM observ_date)::integer = 2014').
     #where("observ_date <= '2013-01-19'").
-    order(:observ_date).map { |o| [o.observ_date, o.species_id] }.
+    order('observ_date').map { |o| [o.observ_date, o.species_id] }.
     group_by(&:first).values.map { |e| e.map(&:second) }
 
 # p obs.flatten.size
