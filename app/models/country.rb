156e7cf219707e028f9d74291358cdb63f3e93ca
Vitalii Khustochka <xyctka@gmail.com>
2014-01-13 11:16:07 +0000

    Cache country

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,9 @@
 class Country < Locus
   default_scope { where(parent_id: nil).order(:public_index) }
+
+  def self.quick_find(slug)
+    Rails.cache.fetch("records/loci/country/#{slug}") do
+      Country.find_by_slug!(slug)
+    end
+  end
 end
