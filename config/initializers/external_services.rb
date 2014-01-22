772e651c054581a666c343c57f4ab4c01efb1f2e
Vitalii Khustochka <xyctka@gmail.com>
2014-01-22 10:16:32 +0000

    Simplify external services config

--- config/initializers/external_services.rb
+++ config/initializers/external_services.rb
@@ -3,10 +3,10 @@ require 'service_code/google_analytics'
 require 'service_code/facebook_sdk'
 require 'service_code/vkontakte_sdk'
 
-GoogleAnalytics.configure(ENV['quails_ga_code'])
-GoogleSearch.configure(ENV['quails_google_cse'])
-FacebookSdk.configure(ENV['quails_facebook_app_id'])
-VkontakteSdk.configure(ENV['quails_vkontakte_app_id'])
+GoogleAnalytics.configure
+GoogleSearch.configure
+FacebookSdk.configure
+VkontakteSdk.configure
 
 
 
--- lib/service_code/google_search.rb
+++ lib/service_code/google_search.rb
@@ -2,6 +2,8 @@ require 'service_code/service_code'
 
 class GoogleSearch < ServiceCode
 
+  CODE_ENV_VAR = 'quails_google_cse'.freeze
+
   def render
     unless Rails.env.test? || config.code.blank?
       @view.render partial: 'partials/search', object: config.code, as: :code
