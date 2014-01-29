06564e660671b6c03347f773d90a2262e6a6e46c
Vitalii Khustochka <xyctka@gmail.com>
2014-01-29 11:32:53 +0200

    fix errbit config

--- lib/configurator.rb
+++ lib/configurator.rb
@@ -16,7 +16,7 @@ module Configurator
     if ENV['errbit_api_key'] && ENV['errbit_host']
       Airbrake.configure do |config|
         config.api_key = ENV['errbit_api_key']
-        config.host = ENV['errbit_api_key']
+        config.host = ENV['errbit_host']
         config.port = 443 #80
         config.secure = config.port == 443
         config.ignore_only = []
