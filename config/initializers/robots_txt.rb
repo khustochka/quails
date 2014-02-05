e4a9ba16a5388bbf5f9ec919d44a20c2e0471ed9
Vitalii Khustochka <xyctka@gmail.com>
2014-02-05 19:56:12 +0200

    Fix some ruby deprecations

--- config/initializers/robots_txt.rb
+++ config/initializers/robots_txt.rb
@@ -2,5 +2,5 @@ require "quails/env"
 
 # Rename restrictive robots.txt if this is REAL production
 if Rails.env.production? && Quails.env.real_prod?
-  FileUtils.mv('public/robots.txt', 'public/robots.txt.disallow',) if File.exists?('public/robots.txt')
+  FileUtils.mv('public/robots.txt', 'public/robots.txt.disallow',) if File.exist?('public/robots.txt')
 end
