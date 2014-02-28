4b8a72f3bfa806ff5c9b0eeace1cedc307e0f591
Vitalii Khustochka <xyctka@gmail.com>
2014-02-28 19:24:31 +0200

    Add pending tests

---
+++ test/unit/libraries/ebird_converter_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class EbirdConverterTest < ActiveSupport::TestCase
+
+  test "that the most correct latin name is used (Clements 6)" do
+    skip
+  end
+
+  test "that distance in miles is used" do
+    skip
+  end
+
+  test "that date is properly formatted (American format)" do
+    skip
+  end
+
+  test "that Feral Pigeon is marked properly" do
+    skip
+  end
+
+  test "that Hirundo rustica is marked properly when seen in Ukraine" do
+    skip
+  end
+
+  test "that Hirundo rustica is marked properly when seen in USA" do
+    skip
+  end
+
+  test "that Larus argentatus is marked properly when seen in Ukraine" do
+    skip
+  end
+
+  test "that Larus argentatus is marked properly when seen in USA" do
+    skip
+  end
+
+  test "that Larus heuglini is marked properly when seen in UK" do
+    skip
+  end
+
+  test "that Motacilla feldegg is marked properly" do
+    skip
+  end
+
+end
