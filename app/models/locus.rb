a44ca59ee20368884fe7ef35bd80dce1427377bd
Vitalii Khustochka <xyctka@gmail.com>
2014-03-20 23:07:32 +0200

    Fix some of the tests

--- app/models/locus.rb
+++ app/models/locus.rb
@@ -8,9 +8,6 @@ class Locus < ActiveRecord::Base
   validates :slug, :format => /\A[a-z_]+\Z/i, :uniqueness => true, :presence => true, :length => {:maximum => 32}
   validates :name_en, :name_ru, :name_uk, :uniqueness => true
 
-  belongs_to :parent, :class_name => 'Locus'
-  has_many :children, :class_name => 'Locus', :foreign_key => 'parent_id', :dependent => :restrict_with_exception
-
   has_many :cards, dependent: :restrict_with_exception
   has_many :observations, through: :cards
 
@@ -41,20 +38,21 @@ class Locus < ActiveRecord::Base
   scope :list_order, lambda { order(:parent_id, :slug) }
 
   def self.suggestion_order
-    Rails.cache.fetch("records/loci/suggestion_order") do
-      query = <<-SQL
-      WITH RECURSIVE subregions(id) AS (
-        SELECT *, 0 as level
-        FROM loci
-        WHERE parent_id IS NULL
-          UNION ALL
-        SELECT loci.*, (subregions.level + 1) as level
-        FROM loci JOIN subregions ON loci.parent_id = subregions.id
-      )
-      SELECT * FROM subregions ORDER BY level DESC, parent_id, id
-      SQL
-      find_by_sql(query)
-    end
+    sort_by_ancestry(Locus.all)
+    # Rails.cache.fetch("records/loci/suggestion_order") do
+    #   query = <<-SQL
+    #   WITH RECURSIVE subregions(id) AS (
+    #     SELECT *, 0 as level
+    #     FROM loci
+    #     WHERE parent_id IS NULL
+    #       UNION ALL
+    #     SELECT loci.*, (subregions.level + 1) as level
+    #     FROM loci JOIN subregions ON loci.parent_id = subregions.id
+    #   )
+    #   SELECT * FROM subregions ORDER BY level DESC, parent_id, id
+    #   SQL
+    #   find_by_sql(query)
+    # end
   end
 
   scope :public, lambda { where('public_index IS NOT NULL').order(:public_index) }
@@ -72,67 +70,70 @@ class Locus < ActiveRecord::Base
   end
 
   def subregion_ids
-    Rails.cache.fetch("records/loci/#{self.slug}/subregion_ids") do
-      # Hack for Arabat Spit
-      if self.slug == 'arabat_spit'
-        Locus.where("slug LIKE 'arabat%'").pluck(:id)
-      else
-        # WARNING: PostgreSQL specific syntax
-        # Learnt from: http://blog.hashrocket.com/posts/recursive-sql-in-activerecord
-        query = <<-SQL
-            WITH RECURSIVE subregions(id) AS (
-              SELECT id
-              FROM loci
-              WHERE parent_id = #{self.id}
-                UNION ALL
-              SELECT loci.id
-              FROM loci JOIN subregions ON loci.parent_id = subregions.id
-            )
-            SELECT * FROM subregions
-        SQL
-
-        Locus.connection.select_rows(query, "Locus Load").map! { |a| a[0].to_i }.push(self.id)
-      end
-    end
+    subtree_ids
+    # Rails.cache.fetch("records/loci/#{self.slug}/subregion_ids") do
+    #   # Hack for Arabat Spit
+    #   if self.slug == 'arabat_spit'
+    #     Locus.where("slug LIKE 'arabat%'").pluck(:id)
+    #   else
+    #     # WARNING: PostgreSQL specific syntax
+    #     # Learnt from: http://blog.hashrocket.com/posts/recursive-sql-in-activerecord
+    #     query = <<-SQL
+    #         WITH RECURSIVE subregions(id) AS (
+    #           SELECT id
+    #           FROM loci
+    #           WHERE parent_id = #{self.id}
+    #             UNION ALL
+    #           SELECT loci.id
+    #           FROM loci JOIN subregions ON loci.parent_id = subregions.id
+    #         )
+    #         SELECT * FROM subregions
+    #     SQL
+    #
+    #     Locus.connection.select_rows(query, "Locus Load").map! { |a| a[0].to_i }.push(self.id)
+    #   end
+    # end
   end
 
   def country
-    @country ||= if parent_id.nil?
-                   self
-                 else
-                   query = <<-SQL
-                      WITH RECURSIVE parents AS (
-                        SELECT *
-                        FROM loci
-                        WHERE id = #{self.parent_id}
-                          UNION ALL
-                        SELECT loci.*
-                        FROM loci JOIN parents ON loci.id = parents.parent_id
-                      )
-                      SELECT * FROM parents WHERE parent_id IS NULL LIMIT 1
-                   SQL
-                   Locus.find_by_sql(query).first
-                 end
+    ancestors.where(loc_type: 'country').first
+    # @country ||= if parent_id.nil?
+    #                self
+    #              else
+    #                query = <<-SQL
+    #                   WITH RECURSIVE parents AS (
+    #                     SELECT *
+    #                     FROM loci
+    #                     WHERE id = #{self.parent_id}
+    #                       UNION ALL
+    #                     SELECT loci.*
+    #                     FROM loci JOIN parents ON loci.id = parents.parent_id
+    #                   )
+    #                   SELECT * FROM parents WHERE parent_id IS NULL LIMIT 1
+    #                SQL
+    #                Locus.find_by_sql(query).first
+    #              end
   end
 
   def public_locus
-    @public_locus ||= if !private_loc?
-                        self
-                      else
-                        query = <<-SQL
-                      WITH RECURSIVE parents AS (
-                        SELECT *
-                        FROM loci
-                        WHERE id = #{self.parent_id}
-                          UNION ALL
-                        SELECT loci.*
-                        FROM loci JOIN parents ON loci.id = parents.parent_id
-                        WHERE parents.private_loc = 't'
-                      )
-                      SELECT * FROM parents WHERE private_loc = 'f' LIMIT 1
-                        SQL
-                        Locus.find_by_sql(query).first
-                      end
+    ancestors.where(private_loc: false).last
+    # @public_locus ||= if !private_loc?
+    #                     self
+    #                   else
+    #                     query = <<-SQL
+    #                   WITH RECURSIVE parents AS (
+    #                     SELECT *
+    #                     FROM loci
+    #                     WHERE id = #{self.parent_id}
+    #                       UNION ALL
+    #                     SELECT loci.*
+    #                     FROM loci JOIN parents ON loci.id = parents.parent_id
+    #                     WHERE parents.private_loc = 't'
+    #                   )
+    #                   SELECT * FROM parents WHERE private_loc = 'f' LIMIT 1
+    #                     SQL
+    #                     Locus.find_by_sql(query).first
+    #                   end
   end
 
 end
