bbe668f221c4cf2ff6cd8d667e76b3ce1688c127
Vitalii Khustochka <xyctka@gmail.com>
2014-03-28 12:24:23 +0200

    Fix whitespace

--- db/migrate/20140320054653_add_ancestry_to_loci.rb
+++ db/migrate/20140320054653_add_ancestry_to_loci.rb
@@ -3,7 +3,7 @@ class AddAncestryToLoci < ActiveRecord::Migration
     add_column :loci, :ancestry, :string
     add_index :loci, :ancestry
 
-    Locus.build_ancestry_from_parent_ids!    
+    Locus.build_ancestry_from_parent_ids!
     Locus.check_ancestry_integrity!
   end
 end
