88ae5b262d42c2ab1484aa53f92ee1258d5ac505
Vitalii Khustochka <xyctka@gmail.com>
2014-04-13 18:56:45 +0300

    Always set locale, even if default

--- app/controllers/mixins/locale_controller.rb
+++ app/controllers/mixins/locale_controller.rb
@@ -8,12 +8,14 @@ module LocaleController
     locale_from_path = request.symbolized_path_parameters[:locale].try(:to_sym)
     if locale_from_path && ALL_LOCALES.include?(locale_from_path)
       @locale_set = I18n.locale = locale_from_path
+    else
+      I18n.locale = I18n.default_locale
     end
-    I18n.reload!
   end
 
   def default_url_options(options={})
-    locale_option = @locale_set ? {locale: @locale_set} : {locale: nil}
+    options.delete(:locale)
+    locale_option = @locale_set ? {locale: @locale_set} : {}
     options.merge(locale_option)
   end
 end
