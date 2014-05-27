2602bb5493122d55997742a070ada5ae0dbf52c8
Vitalii Khustochka <xyctka@gmail.com>
2014-05-27 16:52:53 +0300

    Update jquery

--- config/initializers/jquery.rb
+++ config/initializers/jquery.rb
@@ -1,5 +1,5 @@
 # Jquery series 2.x does not support IE7 properly (e.g .the Map page)
-JQUERY_CDN = "//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"
+JQUERY_CDN = "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"
 JQUERYUI_CDN = "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"
 
 JQUERY_URL = Rails.env.production? ? JQUERY_CDN : "jquery.min.js"
