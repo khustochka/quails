85cd711d10a16b96ed6d2f886ba2a002deaf247a
Vitalii Khustochka <xyctka@gmail.com>
2014-06-30 13:29:26 -0500

    Refactor

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -3,16 +3,15 @@ class CountriesController < ApplicationController
   def gallery
     @country = Country.quick_find(params[:country])
 
-    case @country.slug
+    @thumbs = case @country.slug
       when 'ukraine'
-        @species = @country.checklist([:image, :species])
+        @country.checklist([:image, :species])
       when 'usa'
-        country_obs = Observation.joins(:card).select(:species_id).where("cards.locus_id" => @country.subregion_ids)
-        @species = Species.ordered_by_taxonomy.where("species.id" => country_obs).joins(:image).includes(:image)
+        country_sps = Observation.joins(:card).select(:species_id).where("cards.locus_id" => @country.subregion_ids)
+        Species.ordered_by_taxonomy.where("species.id" => country_sps).joins(:image).includes(:image)
       when 'united_kingdom'
-        country_obs = Observation.joins(:card).select(:id).where("cards.locus_id" => @country.subregion_ids)
-        @images = Image.joins(:observations => :card).
-            merge(Observation.where(id: country_obs)).
+        Image.joins(:observations => :card).
+            where("cards.locus_id" => @country.subregion_ids).
             preload(:species).
             order('cards.observ_date').basic_order
       else
