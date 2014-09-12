8caa6f44d3dc4b9eb3c0d6e4e63ceef2d050ace7
Vitalii Khustochka <xyctka@gmail.com>
2014-09-12 12:28:34 -0500

    Do not hit youtube in test env.

--- app/models/youtube_video.rb
+++ app/models/youtube_video.rb
@@ -1,8 +1,12 @@
 class YoutubeVideo < Struct.new(:video_id, :width, :height)
+
+  YOUTUBE_HOST = Rails.env.test? ? "http://localhost" : "//www.youtube.com"
+
   def to_partial_path
     'videos/youtube_embed'
   end
+
   def url
-    "//www.youtube.com/embed/#{video_id}/?vq=hd720"
+    "#{YOUTUBE_HOST}/embed/#{video_id}/?vq=hd720"
   end
 end
