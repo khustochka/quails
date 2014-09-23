d0e20b4c25d0da1ad42c3cc8a709ee559f306abd
Vitalii Khustochka <xyctka@gmail.com>
2014-09-23 18:51:02 -0500

    Set video quality with JS API (species & videos page).

--- app/assets/javascripts/video_quality.js
+++ app/assets/javascripts/video_quality.js
@@ -4,14 +4,15 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player;
-
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('species_video', {
-    events: {
-      'onReady': onPlayerReady
-    }
-  });
+  $('.video-container iframe').each(
+      function () {
+        new YT.Player(this, {
+          events: {
+            'onReady': onPlayerReady
+          }
+        });
+      });
 }
 
 function onPlayerReady(event) {
