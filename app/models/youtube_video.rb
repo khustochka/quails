e454266c79733ec36d4c9bd4d452d9980711f088
Vitalii Khustochka <xyctka@gmail.com>
2014-12-06 23:09:53 -0600

    Fix video tests if no web server on localhost.

--- app/models/youtube_video.rb
+++ app/models/youtube_video.rb
@@ -1,12 +1,19 @@
 class YoutubeVideo < Struct.new(:video_id, :width, :height)
 
-  YOUTUBE_HOST = Rails.env.test? ? "http://localhost" : "//www.youtube.com"
+  YOUTUBE_HOST = "//www.youtube.com"
 
   def to_partial_path
     'videos/youtube_embed'
   end
 
-  def url
-    "#{YOUTUBE_HOST}/embed/#{video_id}?enablejsapi=1&rel=0&vq=hd720"
+  if Rails.env.test?
+    def url
+      "#{YOUTUBE_HOST}/embed/#{video_id}?enablejsapi=1&rel=0&vq=hd720"
+    end
+  else
+    def url;
+      '';
+    end
+
   end
 end
