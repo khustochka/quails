5b6924a2f673e677c2995a080278858665f247d9
Vitalii Khustochka <xyctka@gmail.com>
2014-12-11 18:47:29 -0600

    Remove pjax from images.
    It is not updating canonical, etc.

--- app/assets/javascripts/image_show.js
+++ app/assets/javascripts/image_show.js
@@ -1,15 +1,15 @@
 $(function () {
 
   // Not used for now
-  function resizeTable() {
-    $('.image_nav').width($('.image_canvas').width());
-  }
+//  function resizeTable() {
+//    $('.image_nav').width($('.image_canvas').width());
+//  }
 
   // default timeout is causing page reload on heavy pages, like lifelist
-  $('.main').pjax('a.img_prev_next');
+//  $('.main').pjax('a.img_prev_next');
 
-  Quails.features.pjaxSpinner.define(function (event) {
-    var link = $(event.relatedTarget);
-    link.replaceWith("<img src='/img/loading_small.gif'>");
-  });
+//  Quails.features.pjaxSpinner.define(function (event) {
+//    var link = $(event.relatedTarget);
+//    link.replaceWith("<img src='/img/loading_small.gif'>");
+//  });
 });
