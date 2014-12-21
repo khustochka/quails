6ba0dd0bcf5c7db5436cdfef177676e838d5044e
Vitalii Khustochka <xyctka@gmail.com>
2014-12-21 15:39:05 -0600

    Hide Birdingtop banner from map.
    Take into account that footer is cached.

--- lib/service_code/birdingtop.rb
+++ lib/service_code/birdingtop.rb
@@ -5,8 +5,8 @@ class Birdingtop < ServiceCode
   CODE_ENV_VAR = 'quails_birdingtop_code'.freeze
 
   def render
-    cache [:birdingtop_code, admin: is_admin?] do
-      unless is_admin? || config.code.blank?
+    cache [:birdingtop_code, show: is_admin? || map?] do
+      unless is_admin? || config.code.blank? || map?
         if Rails.env.production? && Quails.env.real_prod?
           @view.render partial: 'partials/birdingtop_code', object: config.code, as: :code
         else
@@ -14,6 +14,12 @@ class Birdingtop < ServiceCode
         end
       end
     end
+  end
+
+  private
+
+  def map?
+    @view.controller_name == 'maps'
   end
 
 end
