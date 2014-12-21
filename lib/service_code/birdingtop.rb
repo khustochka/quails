af6e8336f4c9a0a5922479e74ae2ac06bb49800e
Vitalii Khustochka <xyctka@gmail.com>
2014-12-21 15:23:47 -0600

    Hide Birdingtop banner from map.
    Show span in dev mode.

--- lib/service_code/birdingtop.rb
+++ lib/service_code/birdingtop.rb
@@ -6,8 +6,12 @@ class Birdingtop < ServiceCode
 
   def render
     cache [:birdingtop_code, admin: is_admin?] do
-      unless !(Rails.env.production? && Quails.env.real_prod?) || is_admin? || config.code.blank?
-        @view.render partial: 'partials/birdingtop_code', object: config.code, as: :code
+      unless is_admin? || config.code.blank?
+        if Rails.env.production? && Quails.env.real_prod?
+          @view.render partial: 'partials/birdingtop_code', object: config.code, as: :code
+        else
+          @view.content_tag(:span, "Here be BirdingTop banner.")
+        end
       end
     end
   end
