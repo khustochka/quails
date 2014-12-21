895f411d1d03799ee8ec7ea9cb94d793925cf588
Vitalii Khustochka <xyctka@gmail.com>
2014-12-21 10:42:14 -0600

    Fix the migration.

--- db/migrate/20141220222009_migrate_images_and_videos_to_media.rb
+++ db/migrate/20141220222009_migrate_images_and_videos_to_media.rb
@@ -1,23 +1,35 @@
 class MigrateImagesAndVideosToMedia < ActiveRecord::Migration
+
+  class OldImage < ActiveRecord::Base
+    self.table_name = 'images'
+    serialize :assets_cache, ImageAssetsArray
+    has_and_belongs_to_many :observations, join_table: 'images_observations', foreign_key: 'image_id'
+  end
+
+  class OldVideo < ActiveRecord::Base
+    self.table_name = 'videos'
+    has_and_belongs_to_many :observations, join_table: 'videos_observations', foreign_key: 'video_id'
+  end
+
+
   def up
 
     img_cache = {}
 
-    Image.find_each do |im|
+    OldImage.find_each do |im|
       attrs = im.attributes.to_hash.with_indifferent_access
 
       attrs.delete(:id)
       attrs[:external_id] = attrs.delete(:flickr_id)
       attrs[:media_type] = 'photo'
 
-      m = Media.create!(attrs)
-
-      img_cache[im.id] = m.id
+      m = Media.new(attrs)
 
       m.observations = im.observations
       m.created_at = im.created_at
       m.save
 
+      img_cache[im.id] = m.id
     end
 
     Media.where('parent_id IS NOT NULL').find_each do |im|
@@ -30,14 +42,14 @@ class MigrateImagesAndVideosToMedia < ActiveRecord::Migration
       sp.update_attribute(:image_id, img_cache[old_img])
     end
 
-    Video.find_each do |vi|
+    OldVideo.find_each do |vi|
       attrs = vi.attributes.to_hash.with_indifferent_access
 
       attrs.delete(:id)
       attrs[:external_id] = attrs.delete(:youtube_id)
       attrs[:media_type] = 'video'
 
-      m = Media.create!(attrs)
+      m = Media.new(attrs)
 
       m.observations = vi.observations
       m.created_at = vi.created_at
