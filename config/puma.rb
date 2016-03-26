863a72babc3af8f833a51ad632c6e869327aff07
Vitalii Khustochka <xyctka@gmail.com>
2016-03-25 21:10:40 -0500

    Update puma config.

--- config/puma.rb
+++ config/puma.rb
@@ -1,13 +1,30 @@
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
 
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development'
+app_port = ENV['PORT'] || 3000
+rails_env = ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development'
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+port app_port
+environment rails_env
+
+# Run on socket in production if port is not specified
+if ENV['PORT'].nil? && rails_env == "production"
+  app_dir = File.expand_path("../..", __FILE__)
+  shared_dir = "#{app_dir}/shared"
+
+  bind "unix://#{shared_dir}/sockets/puma.sock"
+
+# Logging
+  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+# Set master PID and state locations
+  pidfile "#{shared_dir}/pids/puma.pid"
+  state_path "#{shared_dir}/pids/puma.state"
+end
 
 on_worker_boot do
   if defined?(::ActiveRecord) && defined?(::ActiveRecord::Base)
