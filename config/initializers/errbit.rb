3802ff279c54da07aa5cd53fb9b5da94813d51b3
Vitalii Khustochka <xyctka@gmail.com>
2016-05-04 19:40:33 -0500

    Workaround for unconfigured Airbrake.

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,9 +1,11 @@
-if ENV['errbit_api_key'] && ENV['errbit_host']
-  Airbrake.configure do |config|
-    config.project_id = true # Not needed for errbit
-    config.project_key = ENV['errbit_api_key']
-    config.host = "https://#{ENV['errbit_host']}"
-    config.environment = Rails.env
-    config.ignore_environments = %w(development test)
-  end
+Airbrake.configure do |config|
+  config.project_id = true # Not needed for errbit
+  config.project_key = ENV['errbit_api_key'] || "dummy"
+  config.host = "https://#{ENV['errbit_host']}"
+  config.environment = Rails.env
+  config.ignore_environments = %w(test)
+end
+
+unless ENV['errbit_api_key'] && ENV['errbit_host']
+  Airbrake.add_filter(&:ignore!)
 end
