23d139eda4005572c14c02912e565e1d3e9f1313
Vitalii Khustochka <xyctka@gmail.com>
2016-06-12 22:34:50 -0500

    Fix for airbrake.

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,6 @@
 if ENV['errbit_api_key'] && ENV['errbit_host']
   Airbrake.configure do |config|
-    config.project_id = true # Not needed for errbit
+    config.project_id = "1" # Not needed for errbit
     config.project_key = ENV['errbit_api_key']
     config.host = "https://#{ENV['errbit_host']}"
     config.environment = Rails.env
