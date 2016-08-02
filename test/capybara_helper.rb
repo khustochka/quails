40b8a88a60d70f4e67334d7b083474d01babb9e3
Vitalii Khustochka <xyctka@gmail.com>
2016-08-01 23:06:34 -0500

    SLightly increase timeout for autosuggest selection in tests.

--- test/capybara_helper.rb
+++ test/capybara_helper.rb
@@ -60,7 +60,7 @@ module JavaScriptTestCase
       def select_suggestion(value, hash)
         selector = ".ui-menu-item a:contains(\"#{value}\"):first"
         fill_in hash[:from], with: value
-        sleep(0.01)
+        sleep(0.05)
         #raise "No element '#{value}' in the field #{hash[:from]}" unless page.has_selector?(:xpath, "//*[@class=\"ui-menu-item\"]//a[contains(text(), \"#{value}\")]")
         page.execute_script " $('#{selector}').trigger('mouseenter').click();"
       end
