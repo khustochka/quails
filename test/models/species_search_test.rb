d4cf63db4a02a7257e119d5e6bede41ce485a144
Vitalii Khustochka <xyctka@gmail.com>
2016-10-19 21:22:30 -0500

    Add a test for search by legacy name.

--- test/models/species_search_test.rb
+++ test/models/species_search_test.rb
@@ -4,8 +4,6 @@ class SpeciesSearchTest < ActionView::TestCase
 
   test 'simple search by scientific name for user should not include unseen species' do
     create(:observation, taxon: taxa(:gargla))
-    #create(:observation, taxon: taxa(:corgar))
-    #create(:observation, taxon: taxa(:corgar))
 
     result = SpeciesSearch.new(User.from_session(controller.request).searchable_species, 'garrul').find
     assert_equal ["Garrulus glandarius"], result.map(&:name_sci)
@@ -13,8 +11,6 @@ class SpeciesSearchTest < ActionView::TestCase
 
   test 'simple search by scientific name for admin should include unseen species' do
     create(:observation, taxon: taxa(:gargla))
-    #create(:observation, taxon: taxa(:corgar))
-    #create(:observation, taxon: taxa(:corgar))
     user = User.from_session(controller.request).extend(Role::Admin)
     result = SpeciesSearch.new(user.searchable_species, 'garrul').find
     assert_equal ["Garrulus glandarius", "Bombycilla garrulus"], result.map(&:name_sci)
@@ -24,6 +20,20 @@ class SpeciesSearchTest < ActionView::TestCase
     user = User.from_session(controller.request).extend(Role::Admin)
     result = SpeciesSearch.new(user.searchable_species, "Bohemian").find
     assert_equal ["Bombycilla garrulus"], result.map(&:name_sci)
+  end
+
+  test 'search by legacy scientific name' do
+    create(:observation, taxon: taxa(:saxola))
+
+    result = SpeciesSearch.new(User.from_session(controller.request).searchable_species, 'Saxicola torquata').find
+    assert_equal ["Saxicola torquata"], result.map(&:name_sci)
+  end
+
+  test 'search by the new species name shuld not include legacy name' do
+    create(:observation, taxon: taxa(:saxola))
+
+    result = SpeciesSearch.new(User.from_session(controller.request).searchable_species, 'Saxicola rubicola').find
+    assert_equal ["Saxicola rubicola"], result.map(&:name_sci)
   end
 
 end
