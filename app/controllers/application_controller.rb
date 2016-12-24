cfd202f6f0c9ef854b7d42c09159ecefdbde5726
Vitalii Khustochka <xyctka@gmail.com>
2016-12-23 18:43:42 -0600

    No need in protected methods, private will do.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,8 +32,6 @@ class ApplicationController < ActionController::Base
     expire_page controller: :feeds, action: :photos, format: 'xml', locale: 'en'
   end
 
-  protected
-
   def pjax_layout
     "pjax"
   end
