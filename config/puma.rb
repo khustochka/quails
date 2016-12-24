5ad34a36ac9d88270df7bc7fe51a64d0b69c79a6
Vitalii Khustochka <xyctka@gmail.com>
2016-12-24 16:27:25 -0600

    Puma - locations for new capistrano.

--- config/puma.rb
+++ config/puma.rb
@@ -15,14 +15,14 @@ if app_port.nil? && rails_env == "production"
   app_dir = File.expand_path("../../../..", __FILE__)
   shared_dir = "#{app_dir}/shared"
 
-  bind "unix://#{shared_dir}/sockets/puma.sock"
+  bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
 
 # Logging
   stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-  pidfile "#{shared_dir}/pids/puma.pid"
-  state_path "#{shared_dir}/pids/puma.state"
+  pidfile "#{shared_dir}/tmp/pids/puma.pid"
+  state_path "#{shared_dir}/tmp/pids/puma.state"
 else
   port app_port || 3000
 end
