3029c9899873e6b1df3bd1ee944c503b390e0dc1
Vitalii Khustochka <xyctka@gmail.com>
2016-12-28 19:14:08 -0600

    Replace PJAX spinner with simulated progress bar.

--- app/assets/javascripts/lifelist.js
+++ app/assets/javascripts/lifelist.js
@@ -2,12 +2,7 @@ $(function () {
   // default timeout is causing page reload on heavy pages, like lifelist
   $('.main').pjax('.filter-list a:not(.advanced), table#lifelist th a');
 
-  Quails.features.pjaxSpinner.define(function (event) {
-    var link = $(event.relatedTarget);
-    var row = $(link).parents('.filter-list');
-    if (row.length < 1) row = $(link).parents('table#lifelist tr');
-    row.append("<img src='/img/loading_small.gif' class='spinner-img'>");
-  });
+  Quails.features.pjaxSpinner.init();
 
   $(document).tooltip({
     items: ".llc",
