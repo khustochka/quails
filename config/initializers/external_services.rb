b6811697873b3f7f1bdc2984e6ab79b3de28e660
Vitalii Khustochka <xyctka@gmail.com>
2017-01-03 18:51:13 -0600

    Remove Vkontakte and GPlus SDKs. Facebook SDK remains.

--- config/initializers/external_services.rb
+++ config/initializers/external_services.rb
@@ -1,13 +1,11 @@
 require 'service_code/google_search'
 require 'service_code/google_analytics'
 require 'service_code/facebook_sdk'
-require 'service_code/vkontakte_sdk'
 require 'service_code/birdingtop'
 
 GoogleAnalytics.configure
 GoogleSearch.configure
 FacebookSdk.configure
-VkontakteSdk.configure
 Birdingtop.configure
 
 
