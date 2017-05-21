241df1c56e445435044395f629a99661161022de
Vitalii Khustochka <xyctka@gmail.com>
2017-05-21 18:58:29 -0500

    Fix for heroku (less work).

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = Quails.env.nil? || ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || Quails.env.nil?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
