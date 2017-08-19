6da7a5ab02f023a8323c82193d9a91ac9d06d79d
Vitalii Khustochka <xyctka@gmail.com>
2017-08-19 11:51:45 -0500

    Prepare for the taxonomy update 2017.

---
+++ db/migrate/20170819164200_alter_ebird_taxa_for_update.rb
@@ -0,0 +1,7 @@
+class AlterEbirdTaxaForUpdate < ActiveRecord::Migration[5.1]
+  def change
+    change_column :ebird_taxa, :name_ioc_en, :string, null: true
+    add_column :ebird_taxa, :ebird_version, :smallint, null: false, default: 2016
+    change_column :ebird_taxa, :ebird_version, :smallint, null: false, default: nil
+  end
+end
