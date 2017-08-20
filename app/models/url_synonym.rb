233b9f223d353be3fb2522e8ccde3858b41f7e26
Vitalii Khustochka <xyctka@gmail.com>
2017-08-20 08:21:31 -0500

    Add lump as a reason for redirect.

--- app/models/url_synonym.rb
+++ app/models/url_synonym.rb
@@ -1,10 +1,11 @@
 class UrlSynonym < ApplicationRecord
 
-  REASONS = ["split"]
+  REASONS = ["split", "lump"]
 
   belongs_to :species
 
   scope :different_name, -> { where(reason: nil) }
   scope :splits, -> { where(reason: "split") }
+  scope :lumps, -> { where(reason: "lump") }
 
 end
