7b9c93a7e9cf748cc55dd7f10bc946333a5f34d6
Vitalii Khustochka <xyctka@gmail.com>
2017-09-07 22:30:58 -0500

    Fix seed restore code.

--- lib/seeds/table.rb
+++ lib/seeds/table.rb
@@ -15,7 +15,7 @@ module Seeds
       quoted_column_names = column_names.map { |column| ActiveRecord::Base.connection.quote_column_name(column) }.join(',')
 
       records.each_slice(1000) do |bunch|
-        quoted_values = bunch.map { |rec| "(#{rec.each_with_index.map { |v, i| ActiveRecord::Base.connection.quote(v, columns[i]) }.join(',')})" }.join(',')
+        quoted_values = bunch.map { |rec| "(#{rec.map { |v| ActiveRecord::Base.connection.quote(v) }.join(',')})" }.join(',')
         ActiveRecord::Base.connection.execute("INSERT INTO #@quoted_table_name (#{quoted_column_names}) VALUES #{quoted_values}")
       end
     end
