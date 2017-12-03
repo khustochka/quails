f43d6123c24ca0e4e0e5172665123ed09d3db182
Vitalii Khustochka <xyctka@gmail.com>
2017-12-03 10:36:21 -0600

    Do not cache in test.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # No need for caching in test env
+  config.cache_store = :null_store
+
 end
