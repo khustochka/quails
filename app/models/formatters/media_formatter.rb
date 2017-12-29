36fb6343df305b471c5896cde70aaa2e48d262c1
Vitalii Khustochka <xyctka@gmail.com>
2017-12-28 20:52:55 -0600

    Longer descriptions, fix wording.

--- app/models/formatters/media_formatter.rb
+++ app/models/formatters/media_formatter.rb
@@ -16,7 +16,7 @@ class MediaFormatter < ModelFormatter
   end
 
   def meta_description
-    descr = "#{I18n.t("images.picture_taken")} #{l(@model.observ_date, format: :long)}, #{@model.locus.name}."
+    descr = "#{I18n.t("images.picture_taken", title: title)} #{l(@model.observ_date, format: :long)}, #{@model.locus.name}."
     if I18n.russian_locale? && description.present?
       descr << "\n"
       descr << strip_tags(description)
