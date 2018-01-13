0e06f66c6c2e87bbb4bc7b0ba1774f11efe5a1b9
Vitalii Khustochka <xyctka@gmail.com>
2018-01-12 22:23:29 -0600

    Disable seemingly unnecessary (since rails 5.1) DB hacks in tests.

--- test/capybara_helper.rb
+++ test/capybara_helper.rb
@@ -3,26 +3,27 @@ require 'capybara_drivers'
 
 #Capybara.default_wait_time = 5
 
+# Seems like all the commented out stuff below is NOT VALID since Rails 5.1
+
 # There are problems with transactional fixtures in Selenium tests, because
 # Capybara uses a separate server thread.
 # BUT: I found out that we do not need DatabaseCleaner etc, we just need to run factories
 # before opening the browser (using visit or login_as_admin)
-# TODO: override factory's create method to check if browser is open!
 
 # To enable FactoryBot (and probably transactions) for tests run via selenium
 # https://groups.google.com/forum/#!msg/ruby-capybara/JI6JrirL9gM/R6YiXj4gi_UJ
-class ActiveRecord::Base
-  mattr_accessor :shared_connection
-  @@shared_connection = nil
-
-  def self.connection
-    @@shared_connection || retrieve_connection
-  end
-end
+# class ActiveRecord::Base
+#   mattr_accessor :shared_connection
+#   @@shared_connection = nil
+# 
+#   def self.connection
+#     @@shared_connection || retrieve_connection
+#   end
+# end
 
 # Forces all threads to share the same connection. This works on
 # Capybara because it starts the web server in a thread.
-ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
+#ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
 
 module CapybaraTestCase
   include Capybara::DSL
