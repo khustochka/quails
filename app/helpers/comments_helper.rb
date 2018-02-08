b479c51a3436e67465f7bde5a99e79b601276cd2
Vitalii Khustochka <xyctka@gmail.com>
2018-02-07 21:16:30 -0600

    Change the real name field.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,6 @@
 module CommentsHelper
 
-  REAL_NAME_FIELD = -"febfea3515909c4a"
+  REAL_NAME_FIELD = -"3e6f2809d67ef117"
 
   def admin_commenter_id
     @admin_commenter_id ||= Commenter.where(is_admin: true).pluck(:id).first
