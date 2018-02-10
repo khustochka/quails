163b72011d1e6d2ee1989947d97434c7544fe411
Vitalii Khustochka <xyctka@gmail.com>
2018-02-09 22:39:32 -0600

    Fix comment form on reply page not redirecting to post page.
    Because the form was remote and reply page should submit regular request.

--- test/integration/js_comments_test.rb
+++ test/integration/js_comments_test.rb
@@ -21,4 +21,19 @@ class JSCommentsTest < ActionDispatch::IntegrationTest
     assert_equal 1, comment.subcomments.size
   end
 
+  test "Reply from reply page (JS on)" do
+    comment = create(:comment)
+    blogpost = comment.post
+    visit reply_comment_path(comment)
+    within("form#new_comment") do
+      fill_in(CommentsHelper::REAL_NAME_FIELD, with: 'Vasya')
+      fill_in('comment_text', with: 'Some text')
+      click_button("save_button")
+    end
+
+    assert_current_path show_post_path(blogpost.to_url_params)
+    assert_content "Vasya"
+    assert_equal 1, comment.subcomments.size
+  end
+
 end
