559229cb40a8ddc281c29b64d976f17e0e54c6f8
Vitalii Khustochka <xyctka@gmail.com>
2018-02-21 12:06:42 -0600

    Remove json3.
    It was used to show mapped images in IE7 but now Google Maps does not support IE7.

--- app/assets/javascripts/map_show.js
+++ app/assets/javascripts/map_show.js
@@ -10,8 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-// TODO: move json3 into ie_fix with html5shiv ?
-//= require json3
 //= require map_init
 
 $(function () {
