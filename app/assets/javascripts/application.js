63b4417494c937630f012fc5133f13dc797f58f2
Vitalii Khustochka <xyctka@gmail.com>
2018-02-23 20:22:53 -0600

    Less coffee is good.

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,21 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui.user
+//= require search
+//= require base
+
+$(function () {
+    var $body = $("body"),
+        jsController = $body.data("js-controller"),
+        jsFeatures = $body.data("js-features");
+
+    if (jsController)
+        Quails.pages[jsController].init();
+
+
+    if (jsFeatures)
+        for (var i = 0, _len = jsFeatures.length; i < _len; i++) {
+            Quails.features[jsFeatures[i]].init()
+        }
+});
+
--- lib/assets/javascripts/admin_base.js.coffee
+++
@@ -1,2 +0,0 @@
-#= require_tree ./admin/features
-#= require_tree ./admin/pages
