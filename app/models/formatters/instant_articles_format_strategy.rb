fcab4e83457b8baf3b682993a482154989eee27c
Vitalii Khustochka <xyctka@gmail.com>
2018-03-06 18:52:37 -0600

    Instant articles only supports h1, h2. Replace h3 with h2, rest with strong.

--- app/models/formatters/instant_articles_format_strategy.rb
+++ app/models/formatters/instant_articles_format_strategy.rb
@@ -1,5 +1,15 @@
 class InstantArticlesFormatStrategy < FeedFormatStrategy
 
+  private
 
+  def preprocess(text)
+    new_text = super(text)
+    # Replace h3 with h2, h4-6 with strong
+    new_text.
+        gsub(/^h3\./i, "h2.").
+        gsub(/<(\/?)h3/i, '<\1h2').
+        gsub(/^h[4-6]\.\s+(.*)$/i, '*\1*').
+        gsub(/<(\/?)h[4-6]/i, '<\1strong')
+  end
 
 end
