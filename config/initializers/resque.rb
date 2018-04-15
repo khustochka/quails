e6fc007c0468b3cc99ebcf1bd00f273181661c99
Vitalii Khustochka <xyctka@gmail.com>
2018-04-14 21:36:50 -0500

    Report resque errors to Airbrake.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,3 +3,12 @@ if ENV["REDISTOGO_URL"]
 else
   Resque.redis = {:host => 'localhost', :port => 6379, :db => 0}
 end
+
+if ENV['errbit_api_key'] && ENV['errbit_host']
+  require 'resque/failure/multiple'
+  require 'resque/failure/redis'
+  require 'airbrake/resque' # use failure backend from airbrake, not from resque
+  
+  Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Airbrake]
+  Resque::Failure.backend = Resque::Failure::Multiple
+end
