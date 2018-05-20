52856a075863f757a2b5a54289eb8583bdfafed4
Vitalii Khustochka <xyctka@gmail.com>
2018-05-20 11:08:01 -0500

    Mark and exclude already imported checklists.

--- lib/ebird/ebird_client.rb
+++ lib/ebird/ebird_client.rb
@@ -21,12 +21,14 @@ class EbirdClient
     page = @agent.get("https://ebird.org/eBirdReports?cmd=SubReport&currentRow=1&rowsPerPage=1000&sortBy=date&order=desc")
     list_rows = page.xpath("//tr[td[1][starts-with(text(), '#{date}')]]")
     list_rows.map do |row|
+      ebird_id = row.xpath("td/a[text()='View or edit']").first[:href].scan(/checklist\/(.*)\/?$/)[0][0]
       {
           time: row.xpath("td[1]").text,
           location: row.xpath("td[2]").text,
           county: row.xpath("td[3]").text,
           state_prov: row.xpath("td[4]").text,
-          ebird_id: row.xpath("td/a[text()='View or edit']").first[:href].scan(/checklist\/(.*)\/?$/)[0][0],
+          ebird_id: ebird_id,
+          card: Card.find_by_ebird_id(ebird_id)
       }
     end
   end
