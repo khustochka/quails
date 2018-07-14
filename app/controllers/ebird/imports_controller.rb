7c4555ed4b5cb2cc14e8d30294986a6363e1c449
Vitalii Khustochka <xyctka@gmail.com>
2018-07-13 22:27:43 -0500

    Allow fetching all ebird checklist not yet imported.

--- app/controllers/ebird/imports_controller.rb
+++ app/controllers/ebird/imports_controller.rb
@@ -6,12 +6,13 @@ class Ebird::ImportsController < ApplicationController
 
   def index
     date = params[:date]
+    client = EbirdClient.new
+    client.authenticate
     if date.present?
-      client = EbirdClient.new
-      client.authenticate
       @checklists = client.get_checklists_for_date(date)
-    elsif date == ""
-      flash.now.notice = "Select date."
+    else
+      last_date = Card.maximum(:observ_date) || Time.now.to_date
+      @checklists = client.get_checklists_after_date(last_date)
     end
 
   end
