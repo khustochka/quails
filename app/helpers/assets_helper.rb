14cb4240a11153410e0b40b3c52efe997824c9c8
Vitalii Khustochka <xyctka@gmail.com>
2018-10-19 19:48:48 -0500

    Google Map version was force switched already.

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,7 +1,6 @@
 module AssetsHelper
 
-  # Stick to this API version. 3.32.2 has problems: not clickable labels on mobile and inconsistent zoom.
-  GMAP_API_URL = -"//maps.googleapis.com/maps/api/js?key=#{ENV["quails_google_maps_api_key"]}&v=3.31.0"
+  GMAP_API_URL = -"//maps.googleapis.com/maps/api/js?key=#{ENV["quails_google_maps_api_key"]}&v=3.34.14"
 
   def stylesheet(*args)
     @stylesheets ||= []
