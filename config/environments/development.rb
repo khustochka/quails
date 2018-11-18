4534f8da0cb9a201c7b5a43d2e1a3e498ab315ab
Vitalii Khustochka <xyctka@gmail.com>
2018-11-17 21:42:36 -0600

    Use local storage for dev (with ability to set env var to use S3).

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,8 +32,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  # config.active_storage.service = :local
-  config.active_storage.service = :"amazon-test"
+  config.active_storage.service = ENV["DEV_S3"] == "true" ? :"amazon-test" : :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
