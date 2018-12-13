642840a50759f99e293d8d071f79323c9db62fd6
Vitalii Khustochka <xyctka@gmail.com>
2018-12-12 18:44:43 -0600

    Upgrade gems and add comment about i18n.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,8 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = false
+  # To enable callbacks do:
+  # config.i18n.fallbacks = [I18n.default_locale]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
