9886fd3f811e00babd5ab3c719cb55ba1a2f2d54
Vitalii Khustochka <xyctka@gmail.com>
2019-01-28 20:25:51 -0600

    Packs path fix.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,13 @@
 const environment = require('./environment')
 
+const path = require('path')
+const context = "../.."
+
+// prevent exposing full path:
+// https://github.com/webpack/webpack/issues/3603
+environment.config.output.devtoolModuleFilenameTemplate = function(info) {
+        const rel = path.relative(context, info.absoluteResourcePath)
+        return `webpack:///${rel}`
+}
+
 module.exports = environment.toWebpackConfig()
