496c0f90b7305fa0c3696fbf539e515da9678f5c
Vitalii Khustochka <xyctka@gmail.com>
2019-05-07 21:03:12 -0500

    Fix youtube link (nocookie only works for embedding).

--- app/models/youtube_video.rb
+++ app/models/youtube_video.rb
@@ -1,17 +1,15 @@
 class YoutubeVideo < Struct.new(:video_id, :width, :height)
 
-  YOUTUBE_HOST = "https://www.youtube-nocookie.com"
-
   def to_partial_path
     'videos/youtube_embed'
   end
 
   def url
-    "#{YOUTUBE_HOST}/embed/#{video_id}?enablejsapi=1&rel=0&vq=hd720"
+    "https://www.youtube-nocookie.com/embed/#{video_id}?enablejsapi=1&rel=0&vq=hd720"
   end
 
   def direct_url
-    "#{YOUTUBE_HOST}/watch?v=#{video_id}"
+    "https://www.youtube.com/watch?v=#{video_id}"
   end
 
 end
