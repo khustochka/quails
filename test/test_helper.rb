555c5ffdcc92adee2a5fb0846847c7dbed4345b4
Vitalii Khustochka <xyctka@gmail.com>
2019-06-28 20:25:56 -0500

    Add minitest-reporters.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,10 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'minitest/reporters'
+
+MiniTest::Reporters.use!
+
 # Capybara::Webkit.configure do |config|
 #   config.block_unknown_urls
 #   # Don't load images
