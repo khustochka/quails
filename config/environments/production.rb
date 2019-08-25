6bd0053dc3d093544cafc0c8108204d764a98002
Vitalii Khustochka <xyctka@gmail.com>
2019-08-24 22:55:04 -0500

    Do not set nginx-specific header on heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,8 +38,10 @@ Rails.application.configure do
   # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  if !Quails.env.heroku?
+    # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
+    config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :amazon
