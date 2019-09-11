02eba3ddcfb0645f559ce020cc763cea3c3e3676
Vitalii Khustochka <xyctka@gmail.com>
2019-09-10 21:05:49 -0500

    Fully switch to Rails 6.0 defaults.

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ require "haml/filters/textile"
 module Quails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
