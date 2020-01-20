0235ec2e36041c100790f48bc4e1e1a7c839094b
Vitalii Khustochka <xyctka@gmail.com>
2020-01-19 19:59:55 -0600

    Remove unnecessary default_url_options (?)

--- app/controllers/concerns/localization_concern.rb
+++ app/controllers/concerns/localization_concern.rb
@@ -13,12 +13,5 @@ module LocalizationConcern
                     I18n.default_locale
                   end
   end
-
-  def default_url_options(options = {})
-    new_opts = options
-    new_opts.delete(:locale)
-    new_opts[:locale] = I18n.locale unless I18n.default_locale?
-    new_opts
-  end
 end
 
