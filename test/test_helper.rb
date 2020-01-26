46a5906b6def71b77a1c9fdcfdf9270dfabb99c8
Vitalii Khustochka <xyctka@gmail.com>
2020-01-25 20:33:46 -0600

    Disable reporters in RubyMine (currently broken).

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,9 +18,10 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-require 'minitest/reporters'
-
-MiniTest::Reporters.use!
+unless ENV["RM_INFO"]
+  require 'minitest/reporters'
+  MiniTest::Reporters.use!
+end
 
 class ActiveSupport::TestCase
 
