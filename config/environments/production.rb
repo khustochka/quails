1539e16e86b22482235ef86b4a0eeb8509058892
Vitalii Khustochka <xyctka@gmail.com>
2020-03-21 14:34:38 -0500

    Distinct queue names for real production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter     = :resque
-  config.active_job.queue_name_prefix = "quails_#{Rails.env}"
+  config.active_job.queue_name_prefix = "quails_#{Quails.env.real_prod? ? "REAL_" : ""}#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
