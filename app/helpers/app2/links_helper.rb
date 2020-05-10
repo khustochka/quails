716e111de406aa45dff3b644dd9d3b0007278831
Vitalii Khustochka <xyctka@gmail.com>
2020-05-09 20:54:20 -0500

    External links.

---
+++ app/helpers/app2/links_helper.rb
@@ -0,0 +1,26 @@
+module App2::LinksHelper
+
+  #FIXME: This method is not exactly compatible with link_to. No block argument!
+  def external_link(name = nil, options = nil, html_options = nil)
+    html_options2 = (html_options.dup || {}).stringify_keys!
+    klass = html_options2["class"]
+    case klass
+    when String
+      html_options2["class"] += -" external"
+    when Array
+      html_options2["class"] << -"external"
+    when NilClass
+      html_options2["class"] = -"external"
+    end
+
+    nofollow = true
+    if html_options2.has_key?(-"nofollow")
+      nofollow = html_options2.delete(-"nofollow")
+    end
+    if nofollow
+      html_options2.merge!(rel: -"nofollow")
+    end
+    link_to(name, options, html_options2)
+  end
+
+end
