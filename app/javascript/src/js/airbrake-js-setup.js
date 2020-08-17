6f33f1bcdefd8148544ba5b8b2789eeb2c7f2ee1
Vitalii Khustochka <xyctka@gmail.com>
2020-08-16 19:02:34 -0500

    Fix airbrake loading.

--- app/javascript/src/js/airbrake-js-setup.js
+++ app/javascript/src/js/airbrake-js-setup.js
@@ -1,9 +1,9 @@
 if (ERRBIT_CONFIGURED) {
-    const Notifier = require('@airbrake/browser')
+    const Airbrake = require('@airbrake/browser')
 
     const railsEnv = process.env.NODE_ENV || "development"
 
-    const airbrake = new Notifier({
+    const airbrake = new Airbrake.Notifier({
         projectId: 1,
         projectKey: process.env.errbit_api_key,
         environment: railsEnv,
