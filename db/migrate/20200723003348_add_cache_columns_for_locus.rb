ff34067785de10086afb94225a60fb7302f8dd98
Vitalii Khustochka <xyctka@gmail.com>
2020-09-18 16:29:35 -0500

    Fix migration

--- db/migrate/20200723003348_add_cache_columns_for_locus.rb
+++ db/migrate/20200723003348_add_cache_columns_for_locus.rb
@@ -16,9 +16,9 @@ class AddCacheColumnsForLocus < ActiveRecord::Migration[6.1]
 
   def change
     add_column :loci, :cached_parent_id, :bigint, null: true
-    add_column :loci, :city_id, :bigint, null: true
-    add_column :loci, :subdivision_id, :bigint, null: true
-    add_column :loci, :country_id, :bigint, null: true
+    add_column :loci, :cached_city_id, :bigint, null: true
+    add_column :loci, :cached_subdivision_id, :bigint, null: true
+    add_column :loci, :cached_country_id, :bigint, null: true
 
     Locus.find_each(&:cache_parent_loci)
 
