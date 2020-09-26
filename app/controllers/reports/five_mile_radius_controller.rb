5f2dc17edcbf1c9fff7b9236fb05016cb413ffb3
Vitalii Khustochka <xyctka@gmail.com>
2020-09-25 23:23:02 -0500

    Update 5MR status from review page.

--- app/controllers/reports/five_mile_radius_controller.rb
+++ app/controllers/reports/five_mile_radius_controller.rb
@@ -7,7 +7,27 @@ class Reports::FiveMileRadiusController < ApplicationController
     candidates_removal = Rails.cache.read("5mr/removal") || []
     loci_ids = (candidates_5mr + candidates_removal).map {|record| record[:locus_id]}
     loci = Locus.where(id: loci_ids).index_by(&:id)
-    @candidates_5mr = candidates_5mr.each {|rec| rec[:locus] = loci[rec[:locus_id]]}.sort_by {|e| e[:distance]}
-    @candidates_removal = candidates_removal.each {|rec| rec[:locus] = loci[rec[:locus_id]]}.sort_by {|e| e[:distance]}
+    @candidates_5mr = candidates_5mr.
+        select {|rec| !loci[rec[:locus_id]].five_mile_radius}.
+        each {|rec| rec[:locus] = loci[rec[:locus_id]]}.
+        sort_by {|e| e[:distance]}
+    @candidates_removal = candidates_removal.
+        select {|rec| loci[rec[:locus_id]].five_mile_radius}.
+        each {|rec| rec[:locus] = loci[rec[:locus_id]]}.
+        sort_by {|e| e[:distance]}
+  end
+
+  def update
+    locs = params[:locus_id]
+    approve = if params[:commit] == "Confirm"
+                true
+              elsif params[:commit] == "Remove"
+                false
+              else
+                raise "Invalid commit action"
+              end
+    Locus.where(id: locs).update_all(five_mile_radius: approve)
+    CacheKey.lifelist.invalidate
+    redirect_to reports_five_mile_radius_path
   end
 end
