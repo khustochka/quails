29944236bc97f3687f08465df5150ee80c141d46
Vitalii Khustochka <xyctka@gmail.com>
2021-03-03 18:53:58 -0600

    Fix open-uri.

--- app/jobs/flickr_to_storage_job.rb
+++ app/jobs/flickr_to_storage_job.rb
@@ -7,7 +7,7 @@ class FlickrToStorageJob < ApplicationJob
 
   def perform(image)
     asset = image.assets_cache.externals.find {|a| a.url =~ /_o.jpg$/i}&.url
-    image.stored_image.attach(io: open(asset), filename: "#{image.slug}.jpg")
+    image.stored_image.attach(io: URI.open(asset), filename: "#{image.slug}.jpg")
     Deflicker::Service.new.match_to_site
   end
 end
