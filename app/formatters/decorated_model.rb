e268b5bc58cd53e7758a1950766d63e8d701d0e6
Vitalii Khustochka <xyctka@gmail.com>
2021-03-06 18:33:06 -0600

    Add main image title.

--- app/formatters/decorated_model.rb
+++ app/formatters/decorated_model.rb
@@ -3,6 +3,6 @@
 module DecoratedModel
 
   def decorated(metadata = {})
-    ("#{self.class.name}Formatter").constantize.new(self, metadata)
+    @decorated ||= ("#{self.class.name}Formatter").constantize.new(self, metadata)
   end
 end
