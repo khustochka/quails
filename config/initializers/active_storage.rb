26740705d0ba599b17fa0ca86c2ca86f42132089
Vitalii Khustochka <xyctka@gmail.com>
2021-03-06 22:33:53 -0600

    Introduce separate queue for purging.

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -4,3 +4,5 @@ require 'core_ext/active_storage/exif_date_image_analyzer'
 
 Rails.application.config.active_storage.analyzers = [ActiveStorage::Analyzer::ExifDateImageAnalyzer]
 Rails.application.config.active_storage.service_urls_expire_in = 30.days
+Rails.application.config.active_storage.queues.purge = :storage_low_priority
+Rails.application.config.active_storage.queues.mirror = :storage_low_priority
