101ecd9dbc9819749a96e3374665404da1953db6
Vitalii Khustochka <xyctka@gmail.com>
2021-03-08 23:00:17 -0600

    Fix host-less images.

--- test/helpers/images_helper_test.rb
+++ test/helpers/images_helper_test.rb
@@ -3,18 +3,5 @@
 require "test_helper"
 
 class ImagesHelperTest < ActionView::TestCase
-  #test 'remote image host' do
-  #  Setting.create!(key: 'image_host', value: 'http://birdwatch.org.ua')
-  #  assert_equal "http://birdwatch.org.ua/aves/photos/booted_eagle.jpg", legacy_image_url('booted_eagle.jpg')
-  #end
-  #
-  #test 'local image host' do
-  #  Setting.create!(key: 'image_host', value: '/')
-  #  assert_equal "/aves/photos/booted_eagle.jpg", legacy_image_url('booted_eagle.jpg')
-  #end
-  #
-  #test 'no image host' do
-  #  Setting.where(key: 'image_host').delete_all
-  #  assert_equal "/aves/photos/booted_eagle.jpg", legacy_image_url('booted_eagle.jpg')
-  #end
+
 end
