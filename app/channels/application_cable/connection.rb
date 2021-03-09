afdcd9c109320a2dabc976b98c9af81447c2be8d
Vitalii Khustochka <xyctka@gmail.com>
2021-03-09 16:34:47 -0600

    Add some Action cable files.

---
+++ app/channels/application_cable/connection.rb
@@ -0,0 +1,19 @@
+module ApplicationCable
+  class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    protected
+    def find_verified_user
+      session = request.session
+      if session[:admin] == true
+        "admin"
+      else
+        reject_unauthorized_connection
+      end
+    end
+  end
+end
