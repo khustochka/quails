3ced977c5e9349a93fb82b9ec408ee6b5c3b7196
Vitalii Khustochka <xyctka@gmail.com>
2021-03-11 19:00:17 -0600

    Move over formatters and decorators.

--- app/formatters/lj_format_strategy.rb
+++ app/formatters/lj_format_strategy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class LJFormatStrategy < FormattingStrategy
-  include VideoEmbedder
+  include VideoEmbedderHelper
 
   include SpeciesHelper
   include ImagesHelper
