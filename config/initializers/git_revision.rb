4281cf229c91809dd0bbec53255e8f6af053b802
Vitalii Khustochka <xyctka@gmail.com>
2021-03-13 18:50:47 -0600

    Prevent command injection: check that rev file contains sha1.

--- config/initializers/git_revision.rb
+++ config/initializers/git_revision.rb
@@ -1,19 +1,24 @@
 # frozen_string_literal: true
 
-begin
-  revision_file = File.join(Rails.root, "REVISION")
-  if File.exist?(revision_file)
-    $revision = File.read(revision_file)
-    repo_folder = File.join(Rails.root, "../../repo")
-    if File.directory?(repo_folder)
-      Dir.chdir(repo_folder) do
-        $commit = `git show -s --format=%B #{$revision}`
+if Rails.env.production?
+  begin
+    revision_file = File.join(Rails.root, "REVISION")
+    if File.exist?(revision_file)
+      rev = File.read(revision_file)
+      if rev =~ /\A\h{40}\Z/
+        $revision = rev.strip
       end
+      repo_folder = File.join(Rails.root, "../../repo")
+      if File.directory?(repo_folder)
+        Dir.chdir(repo_folder) do
+          $commit = `git show -s --format=%B #{$revision}`
+        end
+      end
+    elsif File.directory?(File.join(Rails.root, ".git"))
+      $revision, $commit = `git show -s --format=%H%n%n%B`.split("\n\n", 2)
     end
-  elsif File.directory?(File.join(Rails.root, ".git"))
-    $revision, $commit = `git show -s --format=%H%n%n%B`.split("\n\n", 2)
+  rescue
   end
-rescue
 end
 
 # FIXME: used by instant articles development feed
