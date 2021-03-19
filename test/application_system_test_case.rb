60d26804b327c8e447f9bb812a98f523e7af209c
Vitalii Khustochka <xyctka@gmail.com>
2021-03-18 22:34:42 -0500

    Rescue all google errors in map edit JS.
    It was causing test failures with webkit driver.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -7,13 +7,12 @@ DEFAULT_JS_DRIVER = :webkit
 
 env_js_driver = ENV["JS_DRIVER"]&.to_sym || DEFAULT_JS_DRIVER
 
-if env_js_driver == :webkit
-  require "capybara/webkit"
-  require "core_ext/capybara/webkit/node"
+if env_js_driver =~ /\Awebkit/
   Capybara::Webkit.configure do |config|
     config.block_unknown_urls
     # Don't load images
     config.skip_image_loading
+    #config.raise_javascript_errors = true
   end
 end
 
