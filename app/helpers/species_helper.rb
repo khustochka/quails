66ee5edb3db3fc0aef2c5e5faa41a580031b759a
Vitalii Khustochka <xyctka@gmail.com>
2021-03-19 21:35:42 -0500

    Species page minor tweaks.

--- app/helpers/species_helper.rb
+++ app/helpers/species_helper.rb
@@ -69,8 +69,8 @@ module SpeciesHelper
       # if distance is too far rely on automatic zoom
       zoom = nil if (lats.max - lats.min).abs > 10 || (lons.max - lons.min).abs > 12
     end
-    image_tag("//maps.googleapis.com/maps/api/staticmap?key=#{ENV["quails_google_maps_api_key"]}&zoom=#{zoom}&size=443x300&#{center}&markers=#{markers}",
-              alt: "#{country} map")
+    image_tag("//maps.googleapis.com/maps/api/staticmap?key=#{ENV["quails_google_maps_api_key"]}&zoom=#{zoom}&size=421x285&#{center}&markers=#{markers}",
+              alt: "#{country} map", size: "421x285")
   end
 
   def term_highlight(string, term)
