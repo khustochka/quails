f530c69a2efc704ae113e0ce507c7e2d144ab881
Vitalii Khustochka <xyctka@gmail.com>
2021-04-03 22:50:33 -0500

    Remove global vars for revision.

--- config/initializers/git_revision.rb
+++ config/initializers/git_revision.rb
@@ -6,16 +6,18 @@ if Rails.env.production?
     if File.exist?(revision_file)
       rev = File.read(revision_file)
       if rev =~ /\A\h{40}\Z/
-        $revision = rev.strip
+        Rails.application.config.x.revision.sha = rev.strip
       end
       repo_folder = File.join(Rails.root, "../../repo")
       if File.directory?(repo_folder)
         Dir.chdir(repo_folder) do
-          $commit = `git show -s --format=%B #{$revision}`
+          Rails.application.config.x.revision.message = `git show -s --format=%B #{$revision}`
         end
       end
     elsif File.directory?(File.join(Rails.root, ".git"))
-      $revision, $commit = `git show -s --format=%H%n%n%B`.split("\n\n", 2)
+      sha, message = `git show -s --format=%H%n%n%B`.split("\n\n", 2)
+      Rails.application.config.x.revision.sha = sha
+      Rails.application.config.x.revision.message = message
     end
   rescue
   end
