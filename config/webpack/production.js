d9caac5c850b5031525984d819753934ecd44445
Vitalii Khustochka <xyctka@gmail.com>
2021-04-11 18:45:59 -0500

    Remove unnecessary.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,12 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const webpackConfig = require('./base')
 
-// Check if it is still needed
-// prevent exposing full path:
-// https://github.com/webpack/webpack/issues/3603
-// webpackConfig.config.output.devtoolModuleFilenameTemplate = function(info) {
-//     const rel = path.relative(context, info.absoluteResourcePath)
-//     return `webpack:///${rel}`
-// }
-
 module.exports = webpackConfig
