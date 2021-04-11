0fdcf270101f25cf03d40136d3e90938fddc2479
Vitalii Khustochka <xyctka@gmail.com>
2021-04-11 18:22:47 -0500

    Test in production.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -5,9 +5,9 @@ const webpackConfig = require('./base')
 // Check if it is still needed
 // prevent exposing full path:
 // https://github.com/webpack/webpack/issues/3603
-webpackConfig.config.output.devtoolModuleFilenameTemplate = function(info) {
-    const rel = path.relative(context, info.absoluteResourcePath)
-    return `webpack:///${rel}`
-}
+// webpackConfig.config.output.devtoolModuleFilenameTemplate = function(info) {
+//     const rel = path.relative(context, info.absoluteResourcePath)
+//     return `webpack:///${rel}`
+// }
 
 module.exports = webpackConfig
