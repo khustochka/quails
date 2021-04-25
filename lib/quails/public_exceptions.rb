56611e4f9f4779047c35ad559dbe6ebc3aef38e5
Vitalii Khustochka <xyctka@gmail.com>
2021-04-25 10:49:51 -0500

    Public Exceptions app - cosmetic.

--- lib/quails/public_exceptions.rb
+++ lib/quails/public_exceptions.rb
@@ -12,38 +12,40 @@ module Quails
       @env = env
       request      = ActionDispatch::Request.new(env)
       status       = request.path_info[1..-1].to_i
-      content_type = request.formats.first
-      body         = { status: status, error: Rack::Utils::HTTP_STATUS_CODES.fetch(status, Rack::Utils::HTTP_STATUS_CODES[500]) }
+      begin
+        content_type = request.formats.first
+      rescue ActionDispatch::Http::MimeNegotiation::InvalidType
+        content_type = Mime[:text]
+      end
+      body = { status: status, error: Rack::Utils::HTTP_STATUS_CODES.fetch(status, Rack::Utils::HTTP_STATUS_CODES[500]) }
 
       render(status, content_type, body)
     end
 
     private
-
-    def render(status, content_type, body)
-      format = "to_#{content_type.to_sym}" if content_type
-      if format && body.respond_to?(format)
-        render_format(status, content_type, body.public_send(format))
-      else
-        render_html(status)
+      def render(status, content_type, body)
+        format = "to_#{content_type.to_sym}" if content_type
+        if format && body.respond_to?(format)
+          render_format(status, content_type, body.public_send(format))
+        else
+          render_html(status)
+        end
       end
-    end
 
-    def render_format(status, content_type, body)
-      [status, { "Content-Type" => "#{content_type}; charset=#{ActionDispatch::Response.default_charset}",
-                 "Content-Length" => body.bytesize.to_s }, [body]]
-    end
-
-    def render_html(status)
-      path = "#{public_path}/#{status}.#{I18n.locale}.html"
-      path = "#{public_path}/#{status}.html" unless (found = File.exist?(path))
-
-      if found || File.exist?(path)
-        render_format(status, "text/html", File.read(path))
-      else
-        ErrorsController.action(:show).call(@env)
+      def render_format(status, content_type, body)
+        [status, { "Content-Type" => "#{content_type}; charset=#{ActionDispatch::Response.default_charset}",
+                  "Content-Length" => body.bytesize.to_s }, [body]]
       end
-    end
 
+      def render_html(status)
+        path = "#{public_path}/#{status}.#{I18n.locale}.html"
+        path = "#{public_path}/#{status}.html" unless (found = File.exist?(path))
+
+        if found || File.exist?(path)
+          render_format(status, "text/html", File.read(path))
+        else
+          ErrorsController.action(:show).call(@env)
+        end
+      end
   end
 end
