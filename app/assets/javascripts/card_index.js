d6e5d399e43f10c0367dcd158036807116bf4917
Vitalii Khustochka <xyctka@gmail.com>
2021-08-05 22:27:28 -0500

    Link to instantly mark a card as Motorless.

--- app/assets/javascripts/card_index.js
+++ app/assets/javascripts/card_index.js
@@ -11,3 +11,9 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require suggest_over_combo
+
+$(function() {
+  $("a.mark_motorless").on("ajax:success", function() {
+    $(this).replaceWith($("<b>Motorless</b>"));
+  });
+});
