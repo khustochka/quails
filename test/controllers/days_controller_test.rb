751a2a4a8b2e3541744de8b25e51a438f7b03fa0
Vitalii Khustochka <xyctka@gmail.com>
2021-09-16 21:03:08 -0500

    Fix marking cards as motorless on Day page.

--- test/controllers/days_controller_test.rb
+++ test/controllers/days_controller_test.rb
@@ -5,14 +5,16 @@ require "test_helper"
 class DaysControllerTest < ActionController::TestCase
 
   test "should get index" do
+    create(:card)
     login_as_admin
     get :index
     assert_response :success
   end
 
   test "should get show" do
+    card = create(:card)
     login_as_admin
-    get :show, params: {id: "2017-03-06"}
+    get :show, params: {id: card.observ_date.iso8601}
     assert_response :success
   end
 
