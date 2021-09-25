4b2d9a346ae5cb1c2ef818404d54edb1b3c6d42c
Vitalii Khustochka <xyctka@gmail.com>
2021-09-25 17:03:45 -0500

    Indent fixes.

--- app/models/post.rb
+++ app/models/post.rb
@@ -51,7 +51,7 @@ class Post < ApplicationRecord
 
   # Convert "timezone-less" face_date to local time zone because AR treats it as UTC (especially necessary for feed updated time)
   def face_date
-   Time.zone.parse(self.read_attribute(:face_date).strftime("%F %T"))
+    Time.zone.parse(self.read_attribute(:face_date).strftime("%F %T"))
   end
 
   # Parameters
