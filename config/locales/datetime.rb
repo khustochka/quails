540d126f099dad1c7e300c7f51b52d84f1fbc2e3
Vitalii Khustochka <xyctka@gmail.com>
2022-04-12 19:45:04 -0500

    Make Ukrainian version the default (unfinished).

--- config/locales/datetime.rb
+++ config/locales/datetime.rb
@@ -11,6 +11,39 @@ LOCALIZE_STANDALONE_ABBR_DAY_NAMES_MATCH = /^%a/
 LOCALIZE_STANDALONE_DAY_NAMES_MATCH = /^%A/
 
 {
+  :uk => {
+    :date => {
+      :abbr_day_names => lambda { |key, **options|
+        if options[:format] && options[:format] =~ LOCALIZE_STANDALONE_ABBR_DAY_NAMES_MATCH
+          :'date.common_abbr_day_names'
+        else
+          :'date.standalone_abbr_day_names'
+        end
+      },
+      :day_names => lambda { |key, **options|
+        if options[:format] && options[:format] =~ LOCALIZE_STANDALONE_DAY_NAMES_MATCH
+          :'date.standalone_day_names'
+        else
+          :'date.common_day_names'
+        end
+      },
+      :abbr_month_names => lambda { |key, **options|
+        if options[:format] && options[:format] =~ LOCALIZE_ABBR_MONTH_NAMES_MATCH
+          :'date.common_abbr_month_names'
+        else
+          :'date.standalone_abbr_month_names'
+        end
+      },
+      :month_names => lambda { |key, **options|
+        if options[:format] && options[:format] =~ LOCALIZE_MONTH_NAMES_MATCH
+          :'date.common_month_names'
+        else
+          :'date.standalone_month_names'
+        end
+      }
+    }
+  },
+
   :ru => {
     :date => {
       :abbr_day_names => lambda { |key, **options|
