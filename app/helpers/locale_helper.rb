2adb95c302acbebb5a1fc9d1581422f78701c9a7
Vitalii Khustochka <xyctka@gmail.com>
2022-04-14 19:08:31 -0500

    Make Ukrainian version the default (continued).

--- app/helpers/locale_helper.rb
+++ app/helpers/locale_helper.rb
@@ -8,4 +8,12 @@ module LocaleHelper
   def russian_locale?
     I18n.locale == :ru
   end
+
+  def cyrillic_locale?
+    I18n.locale.in?(%w(uk ru))
+  end
+
+  def blogless_locale?
+    I18n.locale == :en
+  end
 end
