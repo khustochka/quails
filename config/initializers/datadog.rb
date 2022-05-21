0bd0884da11c6affe20294160f8256507204358a
Vitalii Khustochka <xyctka@gmail.com>
2022-05-20 21:49:54 -0500

    More instrumentations

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,7 +1,12 @@
+require 'ddtrace'
+
 Datadog.configure do |c|
   c.env = Rails.env
   c.tracing.instrument :rails
   c.tracing.instrument :resque
+  c.tracing.instrument :action_cable
+  c.tracing.instrument :aws
+  c.tracing.instrument :mongo
 
   c.tracing.report_hostname = true
 end
