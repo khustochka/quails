215ed6337ecbb6834a3b27861c4862c35b0346d9
Vitalii Khustochka <xyctka@gmail.com>
2022-05-22 20:33:07 -0500

    Add a proper service name.

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -5,7 +5,7 @@ Datadog.configure do |c|
   if Dir.pwd.match(/releases\/(\d{14})$/)
     c.version = $1
   end
-  c.tracing.instrument :rails
+  c.tracing.instrument :rails, service_name: "quails"
   c.tracing.instrument :resque
   c.tracing.instrument :action_cable
   c.tracing.instrument :aws
