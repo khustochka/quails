cd51a01650f261dfbaa63a2e6988904eea3b06e5
Vitalii Khustochka <xyctka@gmail.com>
2022-05-25 15:55:21 -0500

    Enable on prod.

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -7,7 +7,9 @@ Datadog.configure do |c|
   c.tracing.instrument :resque
   c.tracing.instrument :aws
   c.tracing.instrument :mongo
-  #c.diagnostics.startup_logs.enabled = false
+  c.diagnostics.startup_logs.enabled = false
   c.tracing.report_hostname = true
-  c.tracing.enabled = Datadog::Core::Environment::VariableHelpers.env_to_bool("DD_TRACE_ENABLED")
+  if !Rails.env.production?
+    c.tracing.enabled = Datadog::Core::Environment::VariableHelpers.env_to_bool("DD_TRACE_ENABLED")
+  end
 end
