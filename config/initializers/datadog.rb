0dda8e317e4cff004e30a1d867a15dbb72e41401
Vitalii Khustochka <xyctka@gmail.com>
2022-05-24 19:07:37 -0500

    Proper enable/disable.
    Dotenv does not work with datadog env vars.
    (Datadog seems to read them too early).

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,5 +1,3 @@
-require 'ddtrace'
-
 Datadog.configure do |c|
   c.env = Rails.env
   if Dir.pwd.match(/releases\/(\d{14})$/)
@@ -7,9 +5,9 @@ Datadog.configure do |c|
   end
   c.use :rails, service_name: "quails"
   c.use :resque
-  c.use :action_cable
   c.use :aws
   c.use :mongo
   #c.diagnostics.startup_logs.enabled = false
   c.report_hostname = true
+  c.tracer.enabled = Datadog::Core::Environment::VariableHelpers.env_to_bool("DD_TRACE_ENABLED")
 end
