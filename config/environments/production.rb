7b1efd38f29bda3c5b40f07ae304f396df39d14a
Vitalii Khustochka <xyctka@gmail.com>
2022-06-22 22:42:22 -0500

    Change queue prefix.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter     = :resque
-  config.active_job.queue_name_prefix = "quails_#{Quails.env.live?.presence && "REAL_"}#{Rails.env}"
+  config.active_job.queue_name_prefix = "quails_#{Quails.env.live? ? "LIVE" : Rails.env}"
 
   config.action_mailer.perform_caching = false
 
