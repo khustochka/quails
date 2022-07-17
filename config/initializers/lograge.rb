e6004cb442edc41cde32609988edda7c9136ff9b
Vitalii Khustochka <xyctka@gmail.com>
2022-07-17 00:10:42 -0500

    Fix logging when params not present (actioncable).

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.lograge.custom_options = lambda do |event|
     exceptions = %w(controller action format id)
     {
-      params: event.payload[:params].except(*exceptions).to_json,
+      params: event.payload[:params].yield_self {|params| params.except(*exceptions).to_json if params},
       time: Time.zone.now,
     }
   end
