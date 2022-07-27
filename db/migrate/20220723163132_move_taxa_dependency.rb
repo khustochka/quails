a218b0a91fd3c1c400516fff75edae3f4c7d5ffa
Vitalii Khustochka <xyctka@gmail.com>
2022-07-26 20:26:15 -0500

    Fix EbirdTaxon class name in migration.

--- db/migrate/20220723163132_move_taxa_dependency.rb
+++ db/migrate/20220723163132_move_taxa_dependency.rb
@@ -1,7 +1,7 @@
 class MoveTaxaDependency < ActiveRecord::Migration[7.0]
   def change
     add_reference :ebird_taxa, :taxon, foreign_key: { on_delete: :nullify }
-    ebird_taxa = EbirdTaxon.all.index_by(&:id)
+    ebird_taxa = EBirdTaxon.all.index_by(&:id)
     Taxon.find_each do |taxon|
       ebird_taxa[taxon.ebird_taxon_id].update!(taxon_id: taxon.id) if taxon.ebird_taxon_id
     end
