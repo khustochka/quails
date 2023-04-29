30864ab0d13b0bfd55098b9d9c70d4337912f1d0
Vitalii Khustochka <xyctka@gmail.com>
2023-04-28 20:36:50 -0500

    Extract common i18n rules.

--- config/locales/datetime.rb
+++ config/locales/datetime.rb
@@ -10,69 +10,40 @@ LOCALIZE_MONTH_NAMES_MATCH = /(%\-?d|%e)(.*)(%B)/
 LOCALIZE_STANDALONE_ABBR_DAY_NAMES_MATCH = /^%a/
 LOCALIZE_STANDALONE_DAY_NAMES_MATCH = /^%A/
 
+RULES = {
+  date: {
+    abbr_day_names: lambda { |_key, **options|
+      if options[:format] && options[:format] =~ LOCALIZE_STANDALONE_ABBR_DAY_NAMES_MATCH
+        :"date.common_abbr_day_names"
+      else
+        :"date.standalone_abbr_day_names"
+      end
+    },
+    day_names: lambda { |_key, **options|
+      if options[:format] && options[:format] =~ LOCALIZE_STANDALONE_DAY_NAMES_MATCH
+        :"date.standalone_day_names"
+      else
+        :"date.common_day_names"
+      end
+    },
+    abbr_month_names: lambda { |_key, **options|
+      if options[:format] && options[:format] =~ LOCALIZE_ABBR_MONTH_NAMES_MATCH
+        :"date.common_abbr_month_names"
+      else
+        :"date.standalone_abbr_month_names"
+      end
+    },
+    month_names: lambda { |_key, **options|
+      if options[:format] && options[:format] =~ LOCALIZE_MONTH_NAMES_MATCH
+        :"date.common_month_names"
+      else
+        :"date.standalone_month_names"
+      end
+    },
+  },
+}
+
 {
-  uk: {
-    date: {
-      abbr_day_names: lambda { |_key, **options|
-        if options[:format] && options[:format] =~ LOCALIZE_STANDALONE_ABBR_DAY_NAMES_MATCH
-          :"date.common_abbr_day_names"
-        else
-          :"date.standalone_abbr_day_names"
-        end
-      },
-      day_names: lambda { |_key, **options|
-        if options[:format] && options[:format] =~ LOCALIZE_STANDALONE_DAY_NAMES_MATCH
-          :"date.standalone_day_names"
-        else
-          :"date.common_day_names"
-        end
-      },
-      abbr_month_names: lambda { |_key, **options|
-        if options[:format] && options[:format] =~ LOCALIZE_ABBR_MONTH_NAMES_MATCH
-          :"date.common_abbr_month_names"
-        else
-          :"date.standalone_abbr_month_names"
-        end
-      },
-      month_names: lambda { |_key, **options|
-        if options[:format] && options[:format] =~ LOCALIZE_MONTH_NAMES_MATCH
-          :"date.common_month_names"
-        else
-          :"date.standalone_month_names"
-        end
-      },
-    },
-  },
-  ru: {
-    date: {
-      abbr_day_names: lambda { |_key, **options|
-        if options[:format] && options[:format] =~ LOCALIZE_STANDALONE_ABBR_DAY_NAMES_MATCH
-          :"date.common_abbr_day_names"
-        else
-          :"date.standalone_abbr_day_names"
-        end
-      },
-      day_names: lambda { |_key, **options|
-        if options[:format] && options[:format] =~ LOCALIZE_STANDALONE_DAY_NAMES_MATCH
-          :"date.standalone_day_names"
-        else
-          :"date.common_day_names"
-        end
-      },
-      abbr_month_names: lambda { |_key, **options|
-        if options[:format] && options[:format] =~ LOCALIZE_ABBR_MONTH_NAMES_MATCH
-          :"date.common_abbr_month_names"
-        else
-          :"date.standalone_abbr_month_names"
-        end
-      },
-      month_names: lambda { |_key, **options|
-        if options[:format] && options[:format] =~ LOCALIZE_MONTH_NAMES_MATCH
-          :"date.common_month_names"
-        else
-          :"date.standalone_month_names"
-        end
-      },
-    },
-  },
+  uk: RULES,
+  ru: RULES,
 }
