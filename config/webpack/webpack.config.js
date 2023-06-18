7d4f655555191cfd03e8af27a17c6b7a1527acdc
Vitalii Khustochka <xyctka@gmail.com>
2023-06-17 22:20:09 -0500

    Cleanup webpacker.config.

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,8 +1,12 @@
 const { webpackConfig, merge } = require('shakapacker')
-const customConfig = {
-  resolve: {
-    extensions: ['.css']
-  }
-}
+// const customConfig = {
+//   resolve: {
+//     extensions: ['.css']
+//   }
+// }
+//
+// module.exports = merge(webpackConfig, customConfig)
 
-module.exports = merge(webpackConfig, customConfig)
+// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
+
+module.exports = webpackConfig
