bb8770e894373b2ac67ef8e70d896202ba4ce1c2
Vitalii Khustochka <xyctka@gmail.com>
2024-04-05 21:18:21 -0500

    Tracing config.

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -9,8 +9,8 @@ Datadog.configure do |c|
   end
 
   c.tracing.instrument :rails, service_name: "quails"
-  c.tracing.instrument :pg, service_name: "quails-postgres"
-  c.tracing.instrument :active_support, cache_service: "quails-cache"
+  c.tracing.instrument :active_record, service_name: "quails-postgres"
+  c.tracing.instrument :active_support, cache_service: "quails-rails-cache"
   c.tracing.instrument :resque, service_name: "quails-resque"
   c.tracing.instrument :aws, service_name: "quails-aws"
   c.tracing.instrument :mongo, service_name: "quails-mongo"
