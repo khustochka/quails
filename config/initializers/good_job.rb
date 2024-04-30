18bdfffdbd2307b21d03841cfb7f6631bc8c3431
Vitalii Khustochka <xyctka@gmail.com>
2024-04-29 21:21:05 -0500

    Add example cron jobs.

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -3,6 +3,9 @@
 Rails.application.configure do
   config.good_job = {
     smaller_number_is_higher_priority: true,
+
+    # Put cron jobs in config/cron_jobs.yml
+    # Nore they will not run without setting GOOD_JOB_ENABLE_CRON=true
     cron: config_for(:cron_jobs),
   }
 end
