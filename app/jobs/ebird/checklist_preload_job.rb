d96ba1646b9f02194ce51e2af814aa7060bad023
Vitalii Khustochka <xyctka@gmail.com>
2024-05-05 13:51:29 -0500

    Put ebird jobs into a dedicated queue.

--- app/jobs/ebird/checklist_preload_job.rb
+++ app/jobs/ebird/checklist_preload_job.rb
@@ -8,7 +8,7 @@ module EBird
 
     # good_job_control_concurrency_with perform_limit: 1, key: "ebird-task"
 
-    queue_as :low
+    queue_as :ebird
 
     def perform
       time, checklists = EBird::Service.preload_checklists
