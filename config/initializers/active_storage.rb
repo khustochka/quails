a8377c5f4b65b600a24be0aa8eec9c0a017821a7
Vitalii Khustochka <xyctka@gmail.com>
2024-05-05 15:44:34 -0500

    Dedicated storage queue.
    To reduce load during mass varinats generation. Todo: limit number of threads for this queue.

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -5,6 +5,8 @@ require "quails/exif_date_image_analyzer"
 Rails.application.configure do
   config.active_storage.analyzers = [Quails::ExifDateImageAnalyzer]
   config.active_storage.service_urls_expire_in = 30.days
-  config.active_storage.queues.purge = :storage
+  config.active_storage.queues.analysis = :storage
   config.active_storage.queues.mirror = :storage
+  config.active_storage.queues.transform = :storage
+  config.active_storage.queues.purge = :storage
 end
