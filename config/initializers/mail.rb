93e9bc0b1f6a25f3bcbe99ceff8313bf3d391a9c
Vitalii Khustochka <xyctka@gmail.com>
2025-07-29 19:26:35 -0500

    Mailtrap SMTP.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,25 +1,15 @@
 # frozen_string_literal: true
 
 if ENV["MAILTRAP_API_TOKEN"]
-  # require "json"
-  # require "net/http"
-
-  uri = URI.parse("https://mailtrap.io/api/v1/inboxes.json?api_token=#{ENV["MAILTRAP_API_TOKEN"]}")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
-
-  first_inbox = JSON.parse(response.body)[0]
-
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    user_name: first_inbox["username"],
-    password: first_inbox["password"],
-    address: first_inbox["domain"],
-    domain: first_inbox["domain"],
-    port: first_inbox["smtp_ports"][0],
+    user_name: "smtp@mailtrap.io",
+    password: ENV["MAILTRAP_API_TOKEN"],
+    domain: "birdwatch.org.ua",
+    address: "live.smtp.mailtrap.io",
+    port: 587,
     authentication: :plain,
+    enable_starttls_auto: true,
   }
 elsif ENV["SENDGRID_API_KEY"]
   ActionMailer::Base.delivery_method = :smtp
