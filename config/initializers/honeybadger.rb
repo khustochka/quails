e26f7e9efb00ef3b019665c3b202fd8a9a59f3f5
Vitalii Khustochka <xyctka@gmail.com>
2025-10-23 20:19:48 -0500

    Enable honeybadger insights.

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Honeybadger.configure do |config|
-  config.insights.enabled = false
+  config.insights.enabled = true
   config.api_key = ENV["HONEYBADGER_API_KEY"]
   config.env = ENV["DD_ENV"] || Rails.env
 end
