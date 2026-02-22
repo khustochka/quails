1363dc66b0a34ddc315df5d899d8cf4c57f9ac26
Vitalii Khustochka <xyctka@gmail.com>
2026-02-21 22:39:37 -0600

    Remove obsolete comment.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,5 @@
 //= link_tree ../../../lib/assets/images
 //= link_tree ../../../vendor/assets/images
-// For some reason with shakapacker stylesheets should be above javascripts
 //= link_directory ../stylesheets .css
 //= link_directory ../javascripts .js
 //= link_tree ../builds
