c786e394cabeabad45932ebfdeda9d08b3dc95cc
Vitalii Khustochka <xyctka@gmail.com>
2026-02-26 19:01:50 -0600

    Refactor locus_edit.

--- app/assets/javascripts/locus_edit.js
+++ app/assets/javascripts/locus_edit.js
@@ -2,26 +2,25 @@
 
 $(function () {
 
-  function panToLocus(loc_id) {
-    $.get('/loci/' + loc_id + '.json', function (data) {
-      var lat = data['lat'], lon = data['lon'];
-      if (lat != null && lon != null) {
-        theMap.gmap3("get").setCenter(new google.maps.LatLng(lat, lon));
-        theMap.gmap3("get").setZoom(13);
-      }
-    });
+  function panToLocus(map, loc_id) {
+    fetch('/loci/' + loc_id + '.json')
+      .then(function (r) { return r.json(); })
+      .then(function (data) {
+        var lat = data['lat'], lon = data['lon'];
+        if (lat != null && lon != null) {
+          $(map).gmap3("get").setCenter(new google.maps.LatLng(lat, lon));
+          $(map).gmap3("get").setZoom(13);
+        }
+      });
   }
-
-  var lat = $("#locus_lat").val(),
-    lng = $("#locus_lon").val();
 
   function updateGeoFields(latLng) {
-    $("#locus_lat").val(latLng.lat());
-    $("#locus_lon").val(latLng.lng());
+    document.getElementById('locus_lat').value = latLng.lat();
+    document.getElementById('locus_lon').value = latLng.lng();
   }
 
-  function addMarker(latLng) {
-    theMap.gmap3({
+  function addMarker(map, latLng) {
+    $(map).gmap3({
       marker: {
         latLng: latLng,
         options: {
@@ -36,19 +35,22 @@ $(function () {
     });
   }
 
-  var theMap = $('#googleMap');
 
-  theMap.height("250px");
+  var lat = document.getElementById('locus_lat').value,
+    lng = document.getElementById('locus_lon').value,
+    theMap = document.getElementById('googleMap');
+
+  theMap.style.height = '250px';
 
   if (window.mapEnabled) {
-    theMap.gmap3({
+    $(theMap).gmap3({
       map: {
         events: {
           click: function (map, event) {
             var latLng = event.latLng,
-              marker = theMap.gmap3({get: "marker"});
+              marker = $(theMap).gmap3({ get: "marker" });
             if (typeof (marker) != 'undefined') marker.setMap(null);
-            addMarker(latLng);
+            addMarker(map, latLng);
             updateGeoFields(latLng);
           }
         }
@@ -56,18 +58,18 @@ $(function () {
     });
 
     if (lat && lng) {
-      addMarker(new google.maps.LatLng(lat, lng));
-      theMap.gmap3({
-        autofit: {maxZoom: 13}
+      addMarker(theMap, new google.maps.LatLng(lat, lng));
+      $(theMap).gmap3({
+        autofit: { maxZoom: 13 }
       });
     } else {
       var parentLocId = document.querySelector("select[name='locus[parent_id]']").value;
-      if (parentLocId) panToLocus(parentLocId);
+      if (parentLocId) panToLocus(theMap, parentLocId);
     }
 
     document.querySelector("select[name='locus[parent_id]']").addEventListener("change", function () {
       var loc_id = this.value;
-      if (loc_id.length > 0) panToLocus(loc_id);
+      if (loc_id.length > 0) panToLocus(theMap, loc_id);
     });
   }
 });
