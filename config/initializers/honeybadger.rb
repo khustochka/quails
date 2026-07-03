8d6011046b38e04b2b1109f77f038c83d2f0dcb8
Vitalii Khustochka <xyctka@gmail.com>
2026-07-03 12:01:04 -0500

    Only configure honeybadger in API key present.

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-Honeybadger.configure do |config|
-  config.insights.enabled = true
-  config.api_key = ENV["HONEYBADGER_API_KEY"]
-  config.env = ENV["DD_ENV"] || Rails.env
+if ENV["HONEYBADGER_API_KEY"].present?
+  Honeybadger.configure do |config|
+    config.insights.enabled = true
+    config.api_key = ENV["HONEYBADGER_API_KEY"]
+    config.env = ENV["DD_ENV"] || Rails.env
+  end
 end
