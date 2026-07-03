a9f81b330438ff209d0d4f4ecddf49721134e863
Vitalii Khustochka <xyctka@gmail.com>
2026-07-02 23:10:11 -0500

    Rename env var to distinguish from standard DD_ env vars.
    Also remove tracing enabled config, should obey the default env var.

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if ENV["DD_ENABLED"].in?(["true", "1"])
+if ENV["QUAILS_DD_ENABLED"].in?(["true", "1"])
   require "datadog/auto_instrument"
 
   TRACED_TASKS = ["quails:five_mr:refresh", "quails:ebird:checklists_preload", "quails:email:test"]
@@ -22,8 +22,6 @@ if ENV["DD_ENABLED"].in?(["true", "1"])
     # Use DD_TRACE_STARTUP_LOGS for startup logs
     # c.diagnostics.startup_logs.enabled = true
     c.tracing.report_hostname = true
-
-    c.tracing.enabled = Datadog::Core::Environment::VariableHelpers.env_to_bool("DD_TRACE_ENABLED")
   end
 
   Datadog::Tracing.before_flush(
