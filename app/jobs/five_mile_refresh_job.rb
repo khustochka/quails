611027911789eb23f7f8bbc764019db5c32151f9
Vitalii Khustochka <xyctka@gmail.com>
2026-07-03 22:22:09 -0500

    Allow running 5MR detection without arguments.
    It will fallback to MYLOC env var.

--- app/jobs/five_mile_refresh_job.rb
+++ app/jobs/five_mile_refresh_job.rb
@@ -5,7 +5,8 @@ require "quails/five_mile_radius"
 class FiveMileRefreshJob < ApplicationJob
   queue_as :low
 
-  def perform(lat, lon)
+  def perform(lat = nil, lon = nil)
+    lat, lon = ENV["MYLOC"].split(",").map { |n| n.strip.to_f } if lat.nil? || lon.nil?
     Quails::FiveMileRadius.new(lat, lon).refresh
   end
 end
