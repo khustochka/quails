f5e6be18c545b68879ac41235d1434681f3be20a
Vitalii Khustochka <xyctka@gmail.com>
2026-07-07 09:50:26 -0500

    Disable insights reporting to Honeybadger.

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -2,7 +2,7 @@
 
 if ENV["HONEYBADGER_API_KEY"].present?
   Honeybadger.configure do |config|
-    config.insights.enabled = true
+    config.insights.enabled = false
     config.api_key = ENV["HONEYBADGER_API_KEY"]
     config.env = ENV["DD_ENV"] || Rails.env
   end
